17b1b5f586fe88b7a113fce5ce31febb00890d37
Tobias Powalowski <tobias@T-POWA-LX.(none)>
2009-01-26 15:44:45 +0100

    'fixed typo in script'

--- usr/bin/archboot-find-changes.sh
+++ usr/bin/archboot-find-changes.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 TEMPFILE=$(mktemp /tmp/archboot-change-XXX)
-for i in /usr/share/archboot/{base,capi4k,cpufreq,grub,iptables,isdn,kexec,lilo,net,openvpn,pacman,pam,ppp pppoe,pptpclient,remote,shadow,udev,vpnc,wireless}; do
+for i in /usr/share/archboot/{base,capi4k,cpufreq,grub,iptables,isdn,kexec,lilo,net,openvpn,pacman,pam,ppp,pppoe,pptpclient,remote,shadow,udev,vpnc,wireless}; do
 	cd $i
 	for k in $(find ! -type d); do 
 		diff -u $k /$k >>$TEMPFILE || (echo Changes found $i: $k)
