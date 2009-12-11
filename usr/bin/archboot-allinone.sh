56afe5c440a5a779e3a8fa93014ea595593e8719
Tobias Powalowski <tobias@T-POWA-LX.(none)>
2009-12-11 20:17:01 +0100

    changed release name to 2k10-R1

--- usr/bin/archboot-allinone.sh
+++ usr/bin/archboot-allinone.sh
@@ -60,7 +60,7 @@ fi
 
 # set defaults, if nothing given
 [ "${KERNEL}" = "" ] && KERNEL=$(uname -r)
-[ "${RELEASENAME}" = "" ] && RELEASENAME="Schepperle"
+[ "${RELEASENAME}" = "" ] && RELEASENAME="2k10-R1"
 [ "${IMAGENAME}" = "" ] && IMAGENAME="Archlinux-allinone-$(date +%Y.%m)"
 
 # generate temp directories
