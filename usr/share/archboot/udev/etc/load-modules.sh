92297ed8495305543e2d26f252e0bc67c8df2fa4
Tobias Powalowski <tobias@T-POWA-LX.(none)>
2010-01-26 19:46:16 +0100

    changed to use load-modules.sh for correct blacklisting

--- usr/share/archboot/udev/etc/load-modules.sh
+++ usr/share/archboot/udev/etc/load-modules.sh
@@ -1,22 +1,29 @@
 #! /bin/sh
 # Implement blacklisting for udev-loaded modules
 #   Includes module checking
-# - Aaron Griffin & Tobias Powalowski for Archlinux
+# - Aaron Griffin, Tobias Powalowski and Thomas Bächler for Archlinux
 [ $# -ne 1 ] && exit 1
 
+MODPROBE="/sbin/modprobe"
+RESOLVEALIAS="${MODPROBE} --resolve-alias"
+
 if [ -f /proc/cmdline ]; then 
-	for cmd in $(cat /proc/cmdline); do
-    		case $cmd in
-        		*=*) eval $cmd ;;
-    		esac
-	done
+    for cmd in $(cat /proc/cmdline); do
+        case $cmd in
+            disablemodules=*) eval $cmd ;;
+            load_modules=off) exit ;;
+        esac
+    done
+    #parse cmdline entries of the form "disablemodules=x,y,z"
+    if [ -n "$disablemodules" ]; then
+        BLACKLIST="$BLACKLIST $(echo $disablemodules | sed 's|,| |g')"
+    fi
 fi
 
 # get the real names from modaliases
-i="$(/bin/moddeps $1)"
+i="$($RESOLVEALIAS $1)"
 # add disablemodules= from commandline to blacklist
-k="$(/bin/replace "${disablemodules}" ',')"
-j="$(/bin/replace "${k}" '-' '_')"
+j="$(echo ${BLACKLIST} | sed  's|-|_|g')"
 # add blacklisted modules from /tmp/.ide-blacklist
 if [ -s /tmp/.ide-blacklist ]; then
 	for l in $(sort -u /tmp/.ide-blacklist); do
