1a9bd7c33019a6ef9d736f2eb0b4a7943552f30e
Tobias Powalowski <tobias@T-POWA-LX.(none)>
2011-08-11 15:37:05 +0200

    add new version

--- usr/bin/archboot-allinone.sh
+++ usr/bin/archboot-allinone.sh
@@ -71,7 +71,7 @@ fi
 # set defaults, if nothing given
 [ "${KERNEL}" = "" ] && KERNEL="$(uname -r)"
 [ "${LTS_KERNEL}" = "" ] && LTS_KERNEL="2.6.32-lts"
-[ "${RELEASENAME}" = "" ] && RELEASENAME="2k11-R5"
+[ "${RELEASENAME}" = "" ] && RELEASENAME="2k11-R6"
 [ "${IMAGENAME}" = "" ] && IMAGENAME="Archlinux-allinone-$(date +%Y.%m)"
 
 ALLINONE="$(mktemp -d /tmp/allinone.XXX)"
