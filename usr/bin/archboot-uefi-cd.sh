749ded9bbf0255839eb6c950bf66edd3fa6b66e0
Tobias Powalowski <tpowa@archlinux.org>
2013-06-05 11:13:04 +0200

    don't hardcode tmp paths in uefi script

--- usr/bin/archboot-uefi-cd.sh
+++ usr/bin/archboot-uefi-cd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ## Script for creating a UEFI CD bootable ISO from existing Archboot ISO
+## by Tobias Powalowski <tpowa@archlinux.org>
 ## Contributed by "Keshav Padram" <the ddoott ridikulus ddoott rat aatt geemmayil ddoott ccoomm>
 ## ${1} == PATH to ISO
 
@@ -9,32 +10,39 @@ if ! [[ ${UID} -eq 0 ]]; then
     exit 1
 fi
 
+FSIMG=$(mktemp -d)
+ISOIMG=$(mktemp -d)
+MOUNT_FSIMG=$(mktemp -d)
+
 ## Extract old ISO
-mkdir -p /tmp/ARCHBOOTISO/
-bsdtar -C /tmp/ARCHBOOTISO/ -xf "${1}"
+bsdtar -C ${ISOIMG} -xf "${1}"
 # 7z x -o /tmp/ARCHBOOTISO/ "${1}"
 
+## get size of boot x86_64 files
+BOOTSIZE=$(LANG=EN_US du -bc ${ISOIMG}/boot/*x86_64* | grep total | cut -f1)
+IMGSZ=$(( (${BOOTSIZE}*102)/100/1024 + 1)) # image size in sectors
+
 ## Create efiboot.img
-dd if=/dev/zero of=/tmp/efiboot.img bs=120000 count=1024 
-mkfs.vfat /tmp/efiboot.img
-LOOPDEV="$(losetup --find --show /tmp/efiboot.img)"
+dd if=/dev/zero of="${FSIMG}"/efiboot.img bs="${IMGSZ}" count=1024 
+mkfs.vfat "${FSIMG}"/efiboot.img
+LOOPDEV="$(losetup --find --show "${FSIMG}"/efiboot.img)"
 
 ## Mount efiboot.img
-mkdir -p /tmp/EFIBOOT/
-mount -t vfat -o rw,users "${LOOPDEV}" /tmp/EFIBOOT
+mount -t vfat -o rw,users "${LOOPDEV}" "${MOUNT_FSIMG}"
 
 ## Copy UEFI files fo efiboot.img
-cp -r /tmp/ARCHBOOTISO/{EFI,loader,boot} /tmp/EFIBOOT/
-rm -rf /tmp/EFIBOOT/boot/syslinux
-rm /tmp/EFIBOOT/boot/memtest
+mkdir "${MOUNT_FSIMG}"/boot
+cp -r "${ISOIMG}"/{EFI,loader} "${MOUNT_FSIMG}"
+cp "${ISOIMG}"/boot/*x86_64* "${MOUNT_FSIMG}"/boot
 
 ## Unmount efiboot.img
 umount "${LOOPDEV}"
 losetup --detach "${LOOPDEV}"
-rm -rf /tmp/EFIBOOT/
+rm -rf "${MOUNT_FSIMG}"
 
 ## Move updated efiboot.img to old ISO extracted dir
-mv /tmp/efiboot.img /tmp/ARCHBOOTISO/EFI/efiboot.img
+mv "${FSIMG}"/efiboot.img "${ISOIMG}"/EFI/efiboot.img
+rm -rf "${FSIMG}"
 
 ## Create new ISO with BIOS, ISOHYBRID and UEFI support
 xorriso -as mkisofs \
@@ -48,15 +56,15 @@ xorriso -as mkisofs \
         -eltorito-catalog boot/syslinux/boot.cat \
         -no-emul-boot -boot-load-size 4 -boot-info-table \
         -eltorito-alt-boot --efi-boot EFI/efiboot.img -no-emul-boot \
-        -isohybrid-mbr /tmp/ARCHBOOTISO/boot/syslinux/isohdpfx.bin \
-        -output /tmp/ARCHBOOT.iso /tmp/ARCHBOOTISO/ &> /tmp/xorriso.log
+        -isohybrid-mbr "${ISOIMG}"/boot/syslinux/isohdpfx.bin \
+        -output ARCHBOOT.iso "${ISOIMG}"/ &> /tmp/xorriso.log
 
 ## Updated ISO at /tmp/ARCHBOOT.iso
-if [[ -e "/tmp/ARCHBOOT.iso" ]]; then
-    echo "Updated ISO at /tmp/ARCHBOOT.iso"
+if [[ -e "ARCHBOOT.iso" ]]; then
+    echo "Updated ISO at ARCHBOOT.iso"
 else
     echo "ISO generation failed. See /tmp/xorriso.log for more info."
 fi
 
 ## Delete old ISO extracted files
-rm -rf /tmp/ARCHBOOTISO/
+rm -rf "${ISOIMG}"
