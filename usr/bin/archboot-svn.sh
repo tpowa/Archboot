a9b269aabb1861d44a8c6730ac0e75ac995ac5b9
Tobias Powalowski <tpowa@archlinux.org>
2013-09-16 14:15:34 +0200

    remove efivar from svn checkout

--- usr/bin/archboot-svn.sh
+++ usr/bin/archboot-svn.sh
@@ -4,7 +4,7 @@ SVNSETUP="svn://svn.archlinux.org/packages/"
 BASE=""
 DEVEL=""
 SUPPORT=""
-SUPPORT_ADDITION="ntfs-3g_ntfsprogs fuse dhcp efivar f2fs-tools prebootloader lockdown-ms"
+SUPPORT_ADDITION="ntfs-3g_ntfsprogs fuse dhcp f2fs-tools prebootloader lockdown-ms"
 # generate base
 for i in $(pacman -Sg base | sed -e "s/base//g"); do 
 	BASE="$BASE $(echo $i)"
