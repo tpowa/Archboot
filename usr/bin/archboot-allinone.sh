1fb94b5043dcddffe7e88ddbee196b0242d7d70f
Tobias Powalowski <tpowa@archlinux.org>
2015-03-08 11:36:25 +0100

    bump release name

--- usr/bin/archboot-allinone.sh
+++ usr/bin/archboot-allinone.sh
@@ -83,7 +83,7 @@ fi
 
 # set defaults, if nothing given
 [[ -z "${KERNEL}" ]] && KERNEL="$(uname -r)"
-[[ -z "${RELEASENAME}" ]] && RELEASENAME="2k15-R1"
+[[ -z "${RELEASENAME}" ]] && RELEASENAME="2k15-R2"
 [[ -z "${IMAGENAME}" ]] && IMAGENAME="Archlinux-allinone-$(date +%Y.%m)"
 
 IMAGENAME_OLD="${IMAGENAME}"
