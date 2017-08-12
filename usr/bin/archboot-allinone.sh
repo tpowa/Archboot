686deb396bd95d75b250088b1f8ebbc405f62de7
Tobias Powalowski <tpowa@archlinux.org>
2017-08-12 09:14:00 +0200

    bump to 2k17 release

--- usr/bin/archboot-allinone.sh
+++ usr/bin/archboot-allinone.sh
@@ -83,7 +83,7 @@ fi
 
 # set defaults, if nothing given
 [[ -z "${KERNEL}" ]] && KERNEL="$(uname -r)"
-[[ -z "${RELEASENAME}" ]] && RELEASENAME="2k16-R3"
+[[ -z "${RELEASENAME}" ]] && RELEASENAME="2k17-R1"
 [[ -z "${IMAGENAME}" ]] && IMAGENAME="Archlinux-allinone-$(date +%Y.%m)"
 
 IMAGENAME_OLD="${IMAGENAME}"
