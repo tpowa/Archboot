d9722011b6ef66b8aacbf5e7d0f85aee10d77aaa
Tobias Powalowski <tpowa@archlinux.org>
2021-09-21 12:19:24 +0200

    add tz and km script to updater

--- usr/bin/archboot-update-installer.sh
+++ usr/bin/archboot-update-installer.sh
@@ -3,8 +3,9 @@
 
 # Download latest setup and quickinst script from git repository
 
-echo 'Downloading latest quickinst and setup script...'
+echo 'Downloading latest km, tz, quickinst and setup script...'
 INSTALLER_SOURCE="https://gitlab.archlinux.org/tpowa/archboot/-/raw/master/usr/bin"
-[[ -e /arch/quickinst ]] && wget -q "$INSTALLER_SOURCE/archboot-quickinst.sh?inline=false" -O /usr/bin/quickinst
-[[ -e /arch/setup ]] && wget -q "$INSTALLER_SOURCE/archboot-setup.sh?inline=false" -O /usr/bin/setup
-
+[[ -e /usr/bin/quickinst ]] && wget -q "$INSTALLER_SOURCE/archboot-quickinst.sh?inline=false" -O /usr/bin/quickinst
+[[ -e /usr/bin/setup ]] && wget -q "$INSTALLER_SOURCE/archboot-setup.sh?inline=false" -O /usr/bin/setup
+[[ -e /usr/bin/km ]] && wget -q "$INSTALLER_SOURCE/archboot-km.sh?inline=false" -O /usr/bin/km
+[[ -e /usr/bin/tz ]] && wget -q "$INSTALLER_SOURCE/archboot-tz.sh?inline=false" -O /usr/bin/tz
