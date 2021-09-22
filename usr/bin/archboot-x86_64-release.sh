6f00787ddef65d1111a556f14d69eda48a9ac9cd
Tobias Powalowski <tpowa@archlinux.org>
2021-09-22 21:03:16 +0200

    fix Release.txt

--- usr/bin/archboot-x86_64-release.sh
+++ usr/bin/archboot-x86_64-release.sh
@@ -26,6 +26,18 @@ archboot-create-container.sh archboot-release
 systemd-nspawn -D archboot-release /bin/bash -c "umount /tmp;archboot-x86_64-iso.sh -t -i=archrelease"
 # generate iso in container
 systemd-nspawn -D archboot-release /bin/bash -c "umount /tmp;archboot-x86_64-iso.sh -g -T=archrelease.tar"
+# create Release.txt with included main archlinux packages
+echo "Welcome to ARCHBOOT INSTALLATION / RESCUEBOOT SYSTEM" >>Release.txt
+echo "Creation Tool: 'archboot' Tobias Powalowski <tpowa@archlinux.org>" >>Release.txt
+echo "Homepage: https://wiki.archlinux.org/title/Archboot" >>Release.txt
+echo "Architecture: x86_64" >>Release.txt
+echo "RAM requirement to boot: 1024 MB or greater" >>Release.txt
+echo "Kernel:$(systemd-nspawn -D archboot-release pacman -Qi linux | grep Version | cut -d ":" -f2)" >>Release.txt
+echo "Pacman:$(systemd-nspawn -D archboot-release pacman -Qi pacman | grep Version | cut -d ":" -f2)" >>Release.txt
+echo "Systemd:$(systemd-nspawn -D archboot-release pacman -Qi systemd | grep Version | cut -d ":" -f2)" >>Release.txt
+echo "Have fun" >>Release.txt
+echo "Tobias Powalowski" >>Release.txt
+echo "tpowa@archlinux.org" >>Release.txt
 # move iso out of container
 mv archboot-release/*.iso ./
 # remove container
@@ -42,18 +54,6 @@ cp /usr/share/licenses/amd-ucode/* boot/licenses/amd-ucode/
 cp /usr/share/licenses/intel-ucode/* boot/licenses/intel-ucode/
 # create torrent file
 archboot-mktorrent.sh archboot/$1 *.iso
-# create Release.txt with included main archlinux packages
-echo "Welcome to ARCHBOOT INSTALLATION / RESCUEBOOT SYSTEM" >>Release.txt
-echo "Creation Tool: 'archboot' Tobias Powalowski <tpowa@archlinux.org>" >>Release.txt
-echo "Homepage: https://wiki.archlinux.org/title/Archboot" >>Release.txt
-echo "Architecture: x86_64" >>Release.txt
-echo "RAM requirement to boot: 1024 MB or greater" >>Release.txt
-echo "Kernel:$(systemd-nspawn -D archboot-release pacman -Qi linux | grep Version | cut -d ":" -f2)" >>Release.txt
-echo "Pacman:$(systemd-nspawn -D archboot-release pacman -Qi pacman | grep Version | cut -d ":" -f2)" >>Release.txt
-echo "Systemd:$(systemd-nspawn -D archboot-release pacman -Qi systemd | grep Version | cut -d ":" -f2)" >>Release.txt
-echo "Have fun" >>Release.txt
-echo "Tobias Powalowski" >>Release.txt
-echo "tpowa@archlinux.org" >>Release.txt
 # create sha256sums
 sha256sum boot/* >> boot/sha256sum.txt
 sha256sum * >> sha256sum.txt
