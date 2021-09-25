dde669a8a166345691fa99c4caa815d1d9c8af8e
Tobias Powalowski <tpowa@archlinux.org>
2021-09-25 14:02:02 +0200

    fix options loop

--- usr/bin/archboot-create-container.sh
+++ usr/bin/archboot-create-container.sh
@@ -8,6 +8,7 @@ _CLEANUP_CACHE=""
 _MOVE_CACHE=""
 _SAVE_RAM=""
 _LINUX_FIRMWARE=""
+_DIR=""
 
 usage () {
 	echo "CREATE ARCHBOOT CONTAINER"
@@ -24,7 +25,9 @@ usage () {
 
 [[ -z "${1}" ]] && usage
 
-while [ $# -gt 1 ]; do
+_DIR="$1"
+
+while [ $# -gt 0 ]; do
 	case ${2} in
 		-cc|--cc) _SAVE_RAM="1" ;;
 		-cp|--cp) _CLEANUP_CACHE="1" ;;
@@ -42,34 +45,34 @@ fi
 
 
 # prepare pacman dirs
-mkdir -p "$1"/var/lib/pacman
+mkdir -p "${_DIR}"/var/lib/pacman
 mkdir -p "${_CACHEDIR}"
 # install archboot
-pacman --root "$1" -Sy base archboot --noconfirm --cachedir "${_PWD}"/"${_CACHEDIR}"
+pacman --root "${_DIR}" -Sy base archboot --noconfirm --cachedir "${_PWD}"/"${_CACHEDIR}"
 # generate locales
-systemd-nspawn -D "$1" /bin/bash -c "echo 'en_US ISO-8859-1' >> /etc/locale.gen"
-systemd-nspawn -D "$1" /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen"
-systemd-nspawn -D "$1" locale-gen
+systemd-nspawn -D "${_DIR}" /bin/bash -c "echo 'en_US ISO-8859-1' >> /etc/locale.gen"
+systemd-nspawn -D "${_DIR}" /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen"
+systemd-nspawn -D "${_DIR}" locale-gen
 # generate pacman keyring
-systemd-nspawn -D "$1" pacman-key --init
-systemd-nspawn -D "$1" pacman-key --populate archlinux
+systemd-nspawn -D "${_DIR}" pacman-key --init
+systemd-nspawn -D "${_DIR}" pacman-key --populate archlinux
 # add genneral mirror
-systemd-nspawn -D "$1" /bin/bash -c "echo 'Server = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch' >> /etc/pacman.d/mirrorlist"
+systemd-nspawn -D "${_DIR}" /bin/bash -c "echo 'Server = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch' >> /etc/pacman.d/mirrorlist"
 # disable checkspace option in pacman.conf, to allow to install packages in environment
-systemd-nspawn -D "$1" /bin/bash -c "sed -i -e 's:^CheckSpace:#CheckSpace:g' /etc/pacman.conf"
+systemd-nspawn -D "${_DIR}" /bin/bash -c "sed -i -e 's:^CheckSpace:#CheckSpace:g' /etc/pacman.conf"
 # enable parallel downloads
-systemd-nspawn -D "$1" /bin/bash -c "sed -i -e 's:^#ParallelDownloads:ParallelDownloads:g' /etc/pacman.conf"
+systemd-nspawn -D "${_DIR}" /bin/bash -c "sed -i -e 's:^#ParallelDownloads:ParallelDownloads:g' /etc/pacman.conf"
 # reinstall kernel to get files in /boot and firmware package
-systemd-nspawn -D "$1" pacman -Sy linux --noconfirm
-! [[ ${_LINUX_FIRMWARE} == "" ]] && systemd-nspawn -D "$1" pacman -Sy "${_LINUX_FIRMWARE}" --noconfirm
+systemd-nspawn -D "${_DIR}" pacman -Sy linux --noconfirm
+! [[ ${_LINUX_FIRMWARE} == "" ]] && systemd-nspawn -D "${_DIR}" pacman -Sy "${_LINUX_FIRMWARE}" --noconfirm
 
 if [[ "${SAVE_RAM}" ==  "1" ]]; then
     # clean container from not needed files
-    rm -r "$1"/usr/include
-    rm -r "$1"/usr/share/{man,doc}
+    rm -r "${_DIR}"/usr/include
+    rm -r "${_DIR}"/usr/share/{man,doc}
 fi
 
 if [[ "${_CLEANUP_CACHE}" ==  "1" ]]; then
     # clean cache
-    systemd-nspawn -D "$1" pacman -Scc --noconfirm
+    systemd-nspawn -D "${_DIR}" pacman -Scc --noconfirm
 fi
