eb10ff6f49ba23c4c475765f328670d5283e064a
Tobias Powalowski <tpowa@archlinux.org>
2021-10-07 17:45:59 +0200

    change sleep on latest image and message

--- usr/share/archboot/installer_latest/etc/profile.d/latest-installer.sh
+++ usr/share/archboot/installer_latest/etc/profile.d/latest-installer.sh
@@ -1,17 +1,14 @@
 cd /
 echo "Welcome to Arch Linux (archboot environment):"
 echo "----------------------------------------------------------------"
-echo "Go get a cup of coffee, on a fast internet connection (100Mbit),"
+echo "Go and get a cup of coffee, on a fast internet connection (100Mbit),"
 echo "you can start in 5 minutes with your tasks..."
 echo ""
-echo "Have fun."
-echo "Tobias Powalowski <tpowa@archlinux.org>"
-echo ""
-echo "5 seconds time to hit CTRL-C to stop the process now..."
-sleep 5
+echo "10 seconds time to hit CTRL-C to stop the process now..."
+sleep 10
 echo "Starting assembling of latest archboot environment with package cache..."
 echo ""
-echo "Waiting 10 seconds for getting an internet connection through dhcpcd..."
+echo "Waiting 5 seconds for getting an internet connection through dhcpcd..."
 echo ""
-sleep 10
+sleep 5
 update-installer.sh -latest-install
