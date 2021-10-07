48c5c5e739aeba24938d695fa258b4547a6feba1
Tobias Powalowski <tpowa@archlinux.org>
2021-10-07 21:28:24 +0200

    change login message on latest image

--- usr/share/archboot/installer_latest/etc/profile.d/latest-installer.sh
+++ usr/share/archboot/installer_latest/etc/profile.d/latest-installer.sh
@@ -1,14 +1,13 @@
 cd /
 echo "Welcome to Arch Linux (archboot environment):"
-echo "----------------------------------------------------------------"
+echo "--------------------------------------------------------------------"
 echo "Go and get a cup of coffee, on a fast internet connection (100Mbit),"
 echo "you can start in 5 minutes with your tasks..."
 echo ""
 echo "10 seconds time to hit CTRL-C to stop the process now..."
 sleep 10
+echo ""
 echo "Starting assembling of latest archboot environment with package cache..."
 echo ""
-echo "Waiting 5 seconds for getting an internet connection through dhcpcd..."
-echo ""
-sleep 5
+echo "Running now: update-installer.sh -latest-install"
 update-installer.sh -latest-install
