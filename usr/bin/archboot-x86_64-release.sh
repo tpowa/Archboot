f6ae5ce37d492315eee34ea9c6696f5fc994b579
Tobias Powalowski <tpowa@archlinux.org>
2021-10-09 11:38:57 +0200

    change Release.txt

--- usr/bin/archboot-x86_64-release.sh
+++ usr/bin/archboot-x86_64-release.sh
@@ -41,13 +41,10 @@ echo "Creation Tool: 'archboot' Tobias Powalowski <tpowa@archlinux.org>" >>Relea
 echo "Homepage: https://wiki.archlinux.org/title/Archboot" >>Release.txt
 echo "Architecture: x86_64" >>Release.txt
 echo "RAM requirement to boot: 1024 MB or greater" >>Release.txt
-echo "Archboot: $(systemd-nspawn -D "${W_DIR}" pacman -Qi archboot | grep Version | cut -d ":" -f2 | sed -e "s/\r//g")" >>Release.txt
+echo "Archboot:$(systemd-nspawn -D "${W_DIR}" pacman -Qi archboot | grep Version | cut -d ":" -f2 | sed -e "s/\r//g")" >>Release.txt
 echo "Kernel:$(systemd-nspawn -D "${W_DIR}" pacman -Qi linux | grep Version | cut -d ":" -f2 | sed -e "s/\r//g")" >>Release.txt
 echo "Pacman:$(systemd-nspawn -D "${W_DIR}" pacman -Qi pacman | grep Version | cut -d ":" -f2 | sed -e "s/\r//g")" >>Release.txt
 echo "Systemd:$(systemd-nspawn -D "${W_DIR}" pacman -Qi systemd | grep Version | cut -d ":" -f2 | sed -e "s/\r//g")" >>Release.txt
-echo "Have fun" >>Release.txt
-echo "Tobias Powalowski" >>Release.txt
-echo "tpowa@archlinux.org" >>Release.txt
 # move iso out of container
 mv "${W_DIR}"/*.iso ./
 # remove container
