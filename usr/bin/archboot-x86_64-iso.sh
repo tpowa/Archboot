96ea012df4d21358cc9e77c4e988bf1418f6f0b0
Tobias Powalowski <tpowa@archlinux.org>
2021-11-30 17:36:48 +0100

    add fix for mkinitcpio 31 to still use compressed modules

--- usr/bin/archboot-x86_64-iso.sh
+++ usr/bin/archboot-x86_64-iso.sh
@@ -91,6 +91,11 @@ kver() {
 [[ -z "${RELEASENAME}" ]] && RELEASENAME="$(date +%Y.%m.%d-%H.%M)"
 [[ -z "${IMAGENAME}" ]] && IMAGENAME="archlinux-archboot-${RELEASENAME}-x86_64"
 
+# fix for mkinitcpio 31
+# https://bugs.archlinux.org/task/72882
+# remove on mkinitcpio 32 release
+[[ -f "/usr/share/archboot/patches/31-initcpio.functions.fixed" ]] && cp "/usr/share/archboot/patches/31-initcpio.functions.fixed" "/usr/lib/initcpio/functions"
+
 if [[ "${TARBALL}" == "1" ]]; then
 	"${TARBALL_HELPER}" -c="${PRESET}" -t="${IMAGENAME}.tar"
 	exit 0
