38acec50e03dd6efdab78ae7500656f712660046
Tobias Powalowski <tpowa@archlinux.org>
2022-01-02 20:36:48 +0100

    only offer rebuild install system on x86_64

--- usr/bin/archboot-setup.sh
+++ usr/bin/archboot-setup.sh
@@ -4417,7 +4417,7 @@ select_source() {
     else
         detect_uefi_boot
         UPDATE_ENVIRONMENT=""
-        if [[ -e "/usr/bin/update-installer.sh" && "${_DETECTED_UEFI_SECURE_BOOT}" == "0" ]];then
+        if [[ -e "/usr/bin/update-installer.sh" && "${_DETECTED_UEFI_SECURE_BOOT}" == "0" && "${RUNNING_ARCH}" ==  "x86_64" ]];then
             DIALOG --defaultno --yesno "${_DETECTED_UEFI_SECURE_BOOT} Do you want to update the archboot environment to latest packages with caching packages for installation?\n\nATTENTION:\nRequires at least 4GB RAM and will reboot the system using kexec!" 0 0 && UPDATE_ENVIRONMENT="1"
             if [[ "${UPDATE_ENVIRONMENT}" == "1" ]]; then
                 DIALOG --infobox "Now setting up new archboot environment and dowloading latest packages.\n\nRunning at the moment: update-installer.sh -latest-install\nCheck "${LOG}" for progress...\n\nGet a cup of coffee ...\nThis needs approx. 5 minutes on a fast internet connection (100Mbit)." 0 0
