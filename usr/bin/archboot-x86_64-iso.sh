9475cc60203c9cb080b8402717fd2037da10c468
Tobias Powalowski <tpowa@archlinux.org>
2022-01-21 10:40:18 +0100

    code cleanup

--- usr/bin/archboot-x86_64-iso.sh
+++ usr/bin/archboot-x86_64-iso.sh
@@ -38,9 +38,9 @@ export LANG="en_US"
 while [ $# -gt 0 ]; do
     case ${1} in
         -g|--g) _GENERATE="1" ;;
-        -p=*|--p=*) _PRESET="$(echo ${1} | awk -F= '{print $2;}')" ;;
-        -i=*|--i=*) _IMAGENAME="$(echo ${1} | awk -F= '{print $2;}')" ;;
-        -r=*|--r=*) _RELEASENAME="$(echo ${1} | awk -F= '{print $2;}')" ;;
+        -p=*|--p=*) _PRESET="$(echo "${1}" | awk -F= '{print $2;}')" ;;
+        -i=*|--i=*) _IMAGENAME="$(echo "${1}" | awk -F= '{print $2;}')" ;;
+        -r=*|--r=*) _RELEASENAME="$(echo "${1}" | awk -F= '{print $2;}')" ;;
         -h|--h|?) usage ;; 
         *) usage ;;
         esac
@@ -63,7 +63,7 @@ fi
 
 #set PRESET
 [[ -z "${_PRESET}" ]] && _PRESET="x86_64"
-_PRESET=""${_PRESET_DIR}"/"${_PRESET}""
+_PRESET="${_PRESET_DIR}/${_PRESET}"
 
 # set defaults, if nothing given
 [[ -z "${_RELEASENAME}" ]] && _RELEASENAME="$(date +%Y.%m.%d-%H.%M)"
@@ -112,7 +112,7 @@ _prepare_fedora_shim_bootloaders () {
 
 _prepare_uefi_image() {
     ## get size of boot x86_64 files
-    BOOTSIZE=$(du -bc ${_X86_64}/EFI | grep total | cut -f1)
+    BOOTSIZE=$(du -bc "$_X86_64"/EFI | grep total | cut -f1)
     IMGSZ=$(( (${BOOTSIZE}*102)/100/1024 + 1)) # image size in sectors
     ## Create cdefiboot.img
     dd if=/dev/zero of="${_X86_64}"/efi.img bs="${IMGSZ}" count=1024
@@ -175,7 +175,7 @@ grub-mkrescue --compress="xz" --fonts="unicode" --locales="" --themes="" -o "${_
 ## create sha256sums.txt
 echo "Generating sha256sum ..."
 rm -f "sha256sums.txt" || true
-cksum -a sha256 *.iso > "sha256sums.txt"
+cksum -a sha256 ./*.iso > "sha256sums.txt"
 
 # cleanup
 echo "Cleanup remove ${_X86_64}, ${_SHIM} and ${_SHIM32} ..."
