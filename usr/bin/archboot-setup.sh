30db481634dd24eb0aa770b5b5a0beb618fe6392
Tobias Powalowski <tpowa@archlinux.org>
2022-01-22 15:39:31 +0100

    code cleanup efisys

--- usr/bin/archboot-setup.sh
+++ usr/bin/archboot-setup.sh
@@ -1595,41 +1595,41 @@ check_efisys_part() {
     
     detect_DISC
     
-    if [[ "$(${_BLKID} -p -i -o value -s PTTYPE ${DISC})" != "gpt" ]]; then
+    if [[ "$(${_BLKID} -p -i -o value -s PTTYPE "${DISC}")" != "gpt" ]]; then
         GUID_DETECTED=""
         DIALOG --defaultno --yesno "Setup detected no GUID (gpt) partition table on ${DISC}.\nUEFI boot requires ${DISC} to be partitioned as GPT.\n\nDo you want to convert the existing MBR table in ${DISC} to a GUID (gpt) partition table?" 0 0 || return 1
         DIALOG --msgbox "Setup will now try to non-destructively convert ${DISC} to GPT using sgdisk." 0 0
-        sgdisk --mbrtogpt ${DISC} > ${LOG} && GUID_DETECTED="1"
-        partprobe ${DISC} > ${LOG}
+        sgdisk --mbrtogpt "${DISC}" > ${LOG} && GUID_DETECTED="1"
+        partprobe "${DISC}" > ${LOG}
         if [[ "${GUID_DETECTED}" == "" ]]; then
             DIALOG --msgbox "Conversion failed on ${DISC}.\nSetup detected no GUID (gpt) partition table on ${DISC}.\n\n You need to fix your partition table first, before setup can proceed." 0 0
             return 1
         fi
     fi
     
-    if [[ ! "$(sgdisk -p ${DISC} | grep 'EF00')" ]]; then
+    if ! sgdisk -p "${DISC}" | grep -q 'EF00'; then
         # Windows 10 recommends a minimum of 260MB Efi Systen Partition
         DIALOG --msgbox "Setup detected no EFI System partition in ${DISC}. You will now be put into cfdisk. Please create a >= 260 MB partition with cfdisk type EFI System .\nWhen prompted (later) to format as FAT32, say YES.\nIf you already have a >=260 MB FAT32 EFI System partition, check whether that partition has EFI System cfdisk type code." 0 0
         clear && cfdisk "${DISC}"
         RUN_CFDISK=""
     fi
     
-    if [[ "$(sgdisk -p ${DISC} | grep 'EF00')" ]]; then
+    if sgdisk -p "${DISC}" | grep -q 'EF00'; then
         # check on unique PARTTYPE c12a7328-f81f-11d2-ba4b-00a0c93ec93b for EFI System Partition type UUID
-        UEFISYS_PART="$(${_LSBLK} NAME,PARTTYPE ${DISC} | grep 'c12a7328-f81f-11d2-ba4b-00a0c93ec93b' | cut -d " " -f1)"
+        UEFISYS_PART="$(${_LSBLK} NAME,PARTTYPE "${DISC}" | grep 'c12a7328-f81f-11d2-ba4b-00a0c93ec93b' | cut -d " " -f1)"
         
-        if [[ "$(${_LSBLK} FSTYPE ${UEFISYS_PART})" != "vfat" ]]; then
+        if [[ "$(${_LSBLK} FSTYPE "${UEFISYS_PART}")" != "vfat" ]]; then
             ## Check whether EFISYS is FAT, otherwise inform the user and offer to format the partition as FAT32.
             DIALOG --defaultno --yesno "Detected EFI System partition ${UEFISYS_PART} does not appear to be FAT formatted. UEFI Specification requires EFI System partition to be FAT32 formatted. Do you want to format ${UEFISYS_PART} as FAT32?\nNote: Setup will proceed even if you select NO. Some systems like Apple Macs may work with Non-FAT EFI System partition. However the installed system is not in conformance with UEFI Spec., and MAY NOT boot properly." 0 0 && _FORMAT_UEFISYS_FAT32="1"
         fi
         
-        if [[ "$(${_LSBLK} FSTYPE ${UEFISYS_PART})" == "vfat" ]] && [[ "$(${_BLKID} -p -i -o value -s VERSION ${UEFISYS_PART})" != "FAT32" ]]; then
+        if [[ "$(${_LSBLK} FSTYPE "${UEFISYS_PART}")" == "vfat" ]] && [[ "$(${_BLKID} -p -i -o value -s VERSION "${UEFISYS_PART}")" != "FAT32" ]]; then
             ## Check whether EFISYS is FAT32 (specifically), otherwise warn the user about compatibility issues with UEFI Spec.
             DIALOG --defaultno --yesno "Detected EFI System partition ${UEFISYS_PART} does not appear to be FAT32 formatted. Do you want to format ${UEFISYS_PART} as FAT32?\nNote: Setup will proceed even if you select NO. Most systems will boot fine even with FAT16 or FAT12 EFI System partition, however some firmwares may refuse to boot with a non-FAT32 EFI System partition. It is recommended to use FAT32 for maximum compatibility with UEFI Spec." 0 0 && _FORMAT_UEFISYS_FAT32="1"
         fi
         
         #autodetect efisys mountpoint, on fail ask for mountpoint
-        UEFISYS_MOUNTPOINT="/$(basename $(mount | grep "${UEFISYS_PART}" | cut -d " " -f 3))"
+        UEFISYS_MOUNTPOINT="/$(basename "$(mount | grep "${UEFISYS_PART}" | cut -d " " -f 3)")"
         if [[ "${UEFISYS_MOUNTPOINT}" == "/" ]]; then
             DIALOG --inputbox "Enter the mountpoint of your EFI System partition (Default is /boot): " 0 0 "/boot" 2>${ANSWER} || return 1
             UEFISYS_MOUNTPOINT="$(cat ${ANSWER})"
