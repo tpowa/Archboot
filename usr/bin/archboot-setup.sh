5a267097c5bb9287d64b79e82c66e6900e47f75f
Tobias Powalowski <tpowa@archlinux.org>
2022-01-26 21:29:00 +0100

    fix blockdevices

--- usr/bin/archboot-setup.sh
+++ usr/bin/archboot-setup.sh
@@ -264,7 +264,7 @@ blockdevices_partitions() {
         #  "echo ${part} | grep "[a-z]$(parted -s $(${_LSBLK} PKNAME ${part}) print 2>/dev/null | grep bios_grub | cut -d " " -f 2)$"
         #- iso9660 devices
         #  "${_LSBLK} FSTYPE -s ${part} | grep "iso9660"
-        if ! (${_LSBLK} FSTYPE "${part}" | grep -q "linux_raid_member" || ${_LSBLK} FSTYPE "${part}" | grep -q "LVM2_member" || ${_LSBLK} FSTYPE "${part}" | grep -q "crypto_LUKS" || sfdisk -l 2>/dev/null | grep "${part}" | grep -q "Extended$" || sfdisk -l 2>/dev/null | grep "${part}" | grep -q "(LBA)$" || echo "${part}" | grep "[a-z]$(parted -s "$(${_LSBLK} PKNAME "${part}")" print 2>/dev/null | grep bios_grub | cut -d " " -f 2)$" || ${_LSBLK} FSTYPE -s "${part}" | grep "iso9660"); then
+        if ! (${_LSBLK} FSTYPE "${part}" | grep -q "linux_raid_member" || ${_LSBLK} FSTYPE "${part}" | grep -q "LVM2_member" || ${_LSBLK} FSTYPE "${part}" | grep -q "crypto_LUKS" || sfdisk -l 2>/dev/null | grep "${part}" | grep -q "Extended$" || sfdisk -l 2>/dev/null | grep "${part}" | grep -q "(LBA)$" || echo "${part}" | grep -q "[a-z]$(parted -s "$(${_LSBLK} PKNAME "${part}")" print 2>/dev/null | grep bios_grub | cut -d " " -f 2)$" || ${_LSBLK} FSTYPE -s "${part}" | grep -q "iso9660"); then
             echo "${part}"
             [[ "${1}" ]] && echo "${1}"
         fi           
