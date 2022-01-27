d31140587ac33150f871158479f48813d9c5fa8b
Tobias Powalowski <tpowa@archlinux.org>
2022-01-27 15:35:53 +0100

    fix check

--- usr/bin/archboot-setup.sh
+++ usr/bin/archboot-setup.sh
@@ -2317,17 +2317,17 @@ _mkfs() {
             local ret
             #shellcheck disable=SC2086
             case ${_fstype} in
-                xfs)      mkfs.xfs ${_fsoptions} -L "${_labelname}" -f "${_device}" >${LOG} 2>&1; ret=$? ;;
-                jfs)      yes | mkfs.jfs ${_fsoptions} -L "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
-                reiserfs) yes | mkreiserfs ${_fsoptions} -l "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
-                ext2)     mkfs.ext2 -F -L ${_fsoptions} "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
-                ext3)     mke2fs -F ${_fsoptions} -L "${_labelname}" -t ext3 "${_device}" >${LOG} 2>&1; ret=$? ;;
-                ext4)     mke2fs -F ${_fsoptions} -L "${_labelname}" -t ext4 "${_device}" >${LOG} 2>&1; ret=$? ;;
-                f2fs)     mkfs.f2fs ${_fsoptions} -l "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
+                xfs)      mkfs.xfs ${_fsoptions} -L "${_labelname}" -f ${_device} >${LOG} 2>&1; ret=$? ;;
+                jfs)      yes | mkfs.jfs ${_fsoptions} -L "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
+                reiserfs) yes | mkreiserfs ${_fsoptions} -l "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
+                ext2)     mkfs.ext2 -F -L ${_fsoptions} "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
+                ext3)     mke2fs -F ${_fsoptions} -L "${_labelname}" -t ext3 ${_device} >${LOG} 2>&1; ret=$? ;;
+                ext4)     mke2fs -F ${_fsoptions} -L "${_labelname}" -t ext4 ${_device} >${LOG} 2>&1; ret=$? ;;
+                f2fs)     mkfs.f2fs ${_fsoptions} -l "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
                 btrfs)    mkfs.btrfs -f ${_fsoptions} -L "${_labelname}" ${_btrfsdevices} >${LOG} 2>&1; ret=$? ;;
-                nilfs2)   mkfs.nilfs2 -f ${_fsoptions} -L "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
-                ntfs3)    mkfs.ntfs ${_fsoptions} -L "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
-                vfat)     mkfs.vfat -F32 ${_fsoptions} -n "${_labelname}" "${_device}" >${LOG} 2>&1; ret=$? ;;
+                nilfs2)   mkfs.nilfs2 -f ${_fsoptions} -L "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
+                ntfs3)    mkfs.ntfs ${_fsoptions} -L "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
+                vfat)     mkfs.vfat -F32 ${_fsoptions} -n "${_labelname}" ${_device} >${LOG} 2>&1; ret=$? ;;
                 # don't handle anything else here, we will error later
             esac
             if [[ ${ret} != 0 ]]; then
