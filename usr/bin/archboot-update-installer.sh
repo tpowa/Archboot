954d2585376d164d22a2e48634a86703b4303f1a
Tobias Powalowski <tpowa@archlinux.org>
2022-02-03 09:09:19 +0100

    update initramfs creation in update script

--- usr/bin/archboot-update-installer.sh
+++ usr/bin/archboot-update-installer.sh
@@ -83,30 +83,21 @@ echo "Information: Logging is done on /dev/tty7 ..."
 # Generate new environment and launch it with kexec
 if [[ "${_L_COMPLETE}" == "1" || "${_L_INSTALL_COMPLETE}" == "1" ]]; then
     # remove everything not necessary
-    echo "Step 1/6: Removing not necessary files from / ..."
+    echo "Step 1/8: Removing not necessary files from / ..."
     [[ -d "/usr/lib/firmware" ]] && rm -r "/usr/lib/firmware"
     [[ -d "/usr/lib/modules" ]] && rm -r "/usr/lib/modules"
     _SHARE_DIRS="efitools file grub hwdata kbd licenses makepkg nmap openvpn pacman refind tc usb_modeswitch vim zoneinfo zsh"
     for i in "${_SHARE_DIRS}"; do
         [[ -d "/usr/share/${i}" ]] && rm -r "/usr/share/${i}"
     done
-    echo "Step 2/6: Generating archboot container in ${_W_DIR} ..."
+    echo "Step 2/8: Generating archboot container in ${_W_DIR} ..."
     echo "          This will need some time ..."
     # create container without package cache
     [[ "${_L_COMPLETE}" == "1" ]] && ("archboot-${_RUNNING_ARCH}-create-container.sh" "${_W_DIR}" -cc -cp >/dev/tty7 2>&1 || exit 1)
     # create container with package cache
     [[ "${_L_INSTALL_COMPLETE}" == "1" ]] && ("archboot-${_RUNNING_ARCH}-create-container.sh" "${_W_DIR}" -cc >/dev/tty7 2>&1 || exit 1)
     # generate initrd in container, remove archboot packages from cache, not needed in normal install, umount tmp before generating initrd
-    echo "Step 3/6: Generating initramfs in ${_W_DIR} ..."
-    echo "          This will need some time ..."
-    # add fix for mkinitcpio 31, remove when 32 is released
-    cp "${_W_DIR}"/usr/lib/initcpio/functions "${_W_DIR}"/usr/lib/initcpio/functions.old
-    cp "${_W_DIR}"/usr/share/archboot/patches/31-initcpio.functions.fixed "${_W_DIR}"/usr/lib/initcpio/functions
-    # switch compression
-    kver
-    systemd-nspawn -D "${_W_DIR}" /bin/bash -c "umount /tmp; mkinitcpio -k "${_HWKVER}" -c ${_CONFIG} -g /tmp/initrd.img; mv /tmp/initrd.img /" >/dev/tty7 2>&1 || exit 1
-    mv "${_W_DIR}"/usr/lib/initcpio/functions.old "${_W_DIR}"/usr/lib/initcpio/functions
-    echo "Step 4/6: Moving initramfs files from ${_W_DIR} to / ..."
+    echo "Step 3/8: Moving kernel from ${_W_DIR} to / ..."
     mv "${_W_DIR}"/initrd.img / || exit 1
     if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then 
         mv "${_W_DIR}"/boot/vmlinuz-linux / || exit 1
@@ -116,12 +107,28 @@ if [[ "${_L_COMPLETE}" == "1" || "${_L_INSTALL_COMPLETE}" == "1" ]]; then
     if [[ "${_RUNNING_ARCH}" == "aarch64" ]]; then
         mv "${_W_DIR}"/boot/Image / || exit 1
     fi
+    echo "Step 4/8: Collect initramfs files in ${_W_DIR} ..."
+    echo "          This will need some time ..."
+    # add fix for mkinitcpio 31, remove when 32 is released
+    cp "${_W_DIR}"/usr/lib/initcpio/functions "${_W_DIR}"/usr/lib/initcpio/functions.old
+    cp "${_W_DIR}"/usr/share/archboot/patches/31-initcpio.functions.fixed "${_W_DIR}"/usr/lib/initcpio/functions
+    kver
+    # write initramfs to /tmp
+    systemd-nspawn -D "${_W_DIR}" /bin/bash -c "umount /tmp; mkinitcpio -k "${_HWKVER}" -c ${_CONFIG} -d /tmp/ >/dev/tty7 2>&1 || exit 1"
+    # move initramgs to /
+    mv "${_W_DIR}/tmp" /initrd
+    echo "Step 5/8: Remove ${_W_DIR} ..."
+    rm -r "${_W_DIR}" || exit 1
+    echo "Step 6/8: Create initramfs /initrd.img ..."
+    find initrd/. -mindepth 1 -printf '%P\0' | sort -z | LANG=C bsdtar --uid 0 --gid 0 --null -cnf - -T - |\
+    LANG=C bsdtar --null -cf - --format=newc @- | zstd -T0 > /initrd.img
+    mv "${_W_DIR}"/usr/lib/initcpio/functions.old "${_W_DIR}"/usr/lib/initcpio/functions
+    echo "Step 7/8: Remove /initrd ..."
+    rm -r "/initrd" || exit 1
     ### not supported
     #mv "${_W_DIR}"/boot/amd-ucode.img / || exit 1
     # remove "${_W_DIR}"
-    echo "Step 5/6: Remove ${_W_DIR} ..."
-    rm -r "${_W_DIR}" || exit 1
-    echo "Step 6/6: Loading files to kexec now, reboot in a few seconds ..."
+    echo "Step 6/8: Loading files to kexec now, reboot in a few seconds ..."
     # load kernel and initrds into running kernel
     if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then 
         kexec -l /vmlinuz-linux --initrd=/initrd.img --reuse-cmdline
