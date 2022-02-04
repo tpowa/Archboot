beda2ddf7bf1473f35780db72dcf12ec87fe3089
Tobias Powalowski <tpowa@archlinux.org>
2022-02-04 08:51:48 +0100

    only add archinstall on x86_64

--- usr/bin/archboot-archinstall.sh
+++ usr/bin/archboot-archinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 _ARCHINSTALL=$(mktemp -d archinstall.XXX)
+_AARCH64_W_DIR="archinstall-aarch64"
 _USER="tobias"
 _GROUP="users"
 _GPG="--detach-sign --no-armor --batch --passphrase-file /etc/archboot/gpg.passphrase --pinentry-mode loopback -u 7EDF681F"
@@ -18,20 +19,26 @@ fi
 chown "${_USER}:${_GROUP}" "${_ARCHINSTALL}"
 cd "${_ARCHINSTALL}" || exit 1
 # download packages from fedora server
-echo "Building archinstall C version..."
+echo "Building archinstall C version X86_64..."
+pacman -Sy nuitka gcc archinstall patchelf --noconfirm
 nuitka3 --standalone /usr/bin/archinstall || exit 1
-mv archinstall.dist/archinstall ./
+mv archinstall.dist/archinstall ./archinstall.x86_64
+#rm -r archinstall.{dist,build}
+#archboot-aarch64-create-container.sh "${_AARCH64_W_DIR}"
+#systemd-nspawn -q -D "${_AARCH64_W_DIR}" /bin/bash -c "pacman -Sy nuitka gcc archinstall patchelf --noconfirm"; cd /;nuitka3 --standalone /usr/bin/archinstall || exit 1
+#mv "${_AARCH64_W_DIR}/archinstall.dist/archinstall" ./archinstall.aarch64
+#rm -r "${_AARCH64_W_DIR}"
 # sign files
 echo "Sign file and upload ..."
-chmod 755 archinstall
-chown "${_USER}:${_GROUP}" archinstall
-for i in archinstall; do
+chmod 755 ./*
+chown "${_USER}:${_GROUP}" ./*
+for i in ./*; do
     #shellcheck disable=SC2086
     [[ -f "${i}" ]] && sudo -u "${_USER}" gpg ${_GPG} "${i}" || exit 1
     [[ -f "${i}" ]] && cksum -a sha256 "${i}" >> sha256sum.txt
     [[ -f "${i}.sig" ]] && cksum -a sha256 "${i}.sig" >> sha256sum.txt
 done
-sudo -u "${_USER}" scp archinstall "${_SERVER}:${_SHIM_ARCH_SERVERDIR}" || exit 1
+sudo -u "${_USER}" scp ./* "${_SERVER}:${_SHIM_ARCH_SERVERDIR}" || exit 1
 # cleanup
 echo "Remove ${_ARCHINSTALL} directory."
 cd ..
