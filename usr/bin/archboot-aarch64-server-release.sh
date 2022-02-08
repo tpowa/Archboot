eed6a2914b90d4e3eb9144bee6c9f1efef7dd000
Tobias Powalowski <tpowa@archlinux.org>
2022-02-08 20:22:02 +0100

    fix function names

--- usr/bin/archboot-aarch64-server-release.sh
+++ usr/bin/archboot-aarch64-server-release.sh
@@ -3,7 +3,7 @@ source /usr/lib/archboot/functions
 source /usr/lib/archboot/server_functions
 _ARCH="aarch64"
 _root_check
-_check_buildserver
+_buildserver_check
 _update_aarch64_pacman_chroot
 _server_release
 
--- usr/bin/archboot-x86_64-server-release.sh
+++ usr/bin/archboot-x86_64-server-release.sh
@@ -3,7 +3,7 @@ source /usr/lib/archboot/functions
 source /usr/lib/archboot/server_functions
 _ARCH="x86_64"
 _root_check
-_check_buildserver
+_buildserver_check
 _pacman_x86_64_use_default
 _server_release
 _pacman_x86_64_restore
