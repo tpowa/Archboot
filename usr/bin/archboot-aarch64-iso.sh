e8cc2d2ece34dc2d225e8dd1cb54e795ff8313b1
Tobias Powalowski <tpowa@archlinux.org>
2022-02-09 18:18:17 +0100

    remove exit 1 from iso creation

--- usr/bin/archboot-aarch64-iso.sh
+++ usr/bin/archboot-aarch64-iso.sh
@@ -9,7 +9,7 @@ _aarch64_check
 [[ "${_GENERATE}" == "1" ]] || _usage
 _config
 echo "Starting ISO creation ..."
-_prepare_kernel_initramfs_files || exit 1
+_prepare_kernel_initramfs_files
 _prepare_fedora_shim_bootloaders_aarch64 >/dev/null 2>&1 || exit 1
 _prepare_efitools_uefi >/dev/null 2>&1 || exit 1
 _prepare_uefi_AA64_GRUB_USB_files >/dev/null 2>&1 || exit 1
