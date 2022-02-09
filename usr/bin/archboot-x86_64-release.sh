4e6e7f10257a4c53d8cddd4f7e0500166a552101
Tobias Powalowski <tpowa@archlinux.org>
2022-02-09 21:32:54 +0100

    readd exit 1

--- usr/bin/archboot-x86_64-release.sh
+++ usr/bin/archboot-x86_64-release.sh
@@ -9,7 +9,7 @@ _root_check
 _x86_64_check
 echo "Start release creation in $1 ..."
 _create_iso "$@" || exit 1
-_create_boot
+_create_boot || exit 1
 _create_cksum || exit 1
 echo "Finished release creation in ${1} ."
 
