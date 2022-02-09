cd0bf4a45759317b9d04277e94ad1f2a6f23a34f
Tobias Powalowski <tpowa@archlinux.org>
2022-02-09 10:59:26 +0100

    remove export and unset

--- usr/bin/archboot-aarch64-server-release.sh
+++ usr/bin/archboot-aarch64-server-release.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 source /usr/lib/archboot/functions
 source /usr/lib/archboot/server_functions
-export _ARCH="aarch64"
+_ARCH="aarch64"
 _root_check
 _buildserver_check
 _update_aarch64_pacman_chroot
 _server_release
-unset _ARCH
--- usr/bin/archboot-x86_64-server-release.sh
+++ usr/bin/archboot-x86_64-server-release.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 source /usr/lib/archboot/functions
 source /usr/lib/archboot/server_functions
-export _ARCH="x86_64"
+_ARCH="x86_64"
 _root_check
 _buildserver_check
 _pacman_x86_64_use_default
 _server_release
 _pacman_x86_64_restore
-unset _ARCH
-
