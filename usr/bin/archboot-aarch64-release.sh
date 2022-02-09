804cda1628cf295301e4c9d46157fa46dacccb97
Tobias Powalowski <tpowa@archlinux.org>
2022-02-09 18:26:05 +0100

    remove an other exit 1

--- usr/bin/archboot-aarch64-release.sh
+++ usr/bin/archboot-aarch64-release.sh
@@ -8,6 +8,6 @@ source /usr/lib/archboot/release_functions
 _root_check
 echo "Start release creation in $1 ..."
 _create_iso "$@" || exit 1
-_create_boot || exit 1
+_create_boot
 _create_cksum || exit 1
 echo "Finished release creation in ${1} ."
