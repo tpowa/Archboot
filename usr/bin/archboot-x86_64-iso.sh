d5964e760e57fa664e9c3cf48b4a65bec90f41e4
Tobias Powalowski <tpowa@archlinux.org>
2022-02-09 20:32:52 +0100

    fix aarch64 iso creation

--- usr/bin/archboot-x86_64-iso.sh
+++ usr/bin/archboot-x86_64-iso.sh
@@ -10,12 +10,12 @@ _x86_64_check
 _config
 echo "Starting ISO creation ..."
 _prepare_kernel_initramfs_files
-_prepare_fedora_shim_bootloaders_x86_64 >/dev/null 2>&1 || exit 1
-_download_uefi_shell_tianocore >/dev/null 2>&1 || exit 1
-_prepare_efitools_uefi >/dev/null 2>&1 || exit 1
-_prepare_uefi_X64_GRUB_USB_files >/dev/null 2>&1 || exit 1
-_prepare_uefi_IA32_GRUB_USB_files >/dev/null 2>&1 || exit 1
-_prepare_uefi_image >/dev/null 2>&1 || exit 1
+_prepare_fedora_shim_bootloaders_x86_64 || exit 1
+_download_uefi_shell_tianocore || exit 1
+_prepare_efitools_uefi || exit 1
+_prepare_uefi_X64_GRUB_USB_files || exit 1
+_prepare_uefi_IA32_GRUB_USB_files || exit 1
+_prepare_uefi_image || exit 1
 _grub_mkrescue || exit 1
 _create_cksum || exit 1
 _cleanup_iso || exit 1
