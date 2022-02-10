cd9044f2c30c13a05daddd13d4f5e3a8433b5783
Tobias Powalowski <tpowa@archlinux.org>
2022-02-10 14:22:59 +0100

    always generate locales after archboot installation

--- usr/bin/archboot-aarch64-create-container.sh
+++ usr/bin/archboot-aarch64-create-container.sh
@@ -9,8 +9,7 @@ _DIR="$1"
 _parameters "$@"
 _root_check
 echo "Starting container creation ..."
-echo "Create directory ${_DIR} ..."
-mkdir "${_DIR}"
+[[ -d "${_DIR}" ]] || (echo "Create directory ${_DIR} ..."; mkdir "${_DIR}")
 if [[ "${_RUNNING_ARCH}" == "aarch64" ]]; then
     _prepare_pacman || exit 1
     _install_base_packages || exit 1
@@ -18,11 +17,11 @@ if [[ "${_RUNNING_ARCH}" == "aarch64" ]]; then
     _clean_cache || exit 1
     _install_archboot || exit 1
     _umount_special || exit 1
+    _generate_locales || exit 1
+    _clean_locale
     _clean_container || exit 1
     _clean_archboot_cache
     _generate_keyring || exit 1
-    _generate_locales || exit 1
-    _clean_locale
     _copy_mirrorlist_and_pacman_conf
     _change_pacman_conf || exit 1
 fi
@@ -34,9 +33,9 @@ if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
     _aarch64_install_archboot || exit 1
     _clean_mkinitcpio || exit 1
     _clean_cache || exit 1
-    _clean_container || exit 1
     _generate_locales || exit 1
     _clean_locale
+    _clean_container || exit 1
 fi
 _set_hostname || exit 1
 echo "Finished container setup in ${_DIR} ."
--- usr/bin/archboot-x86_64-create-container.sh
+++ usr/bin/archboot-x86_64-create-container.sh
@@ -10,17 +10,17 @@ _parameters "$@"
 _root_check
 _x86_64_check
 echo "Starting container creation ..."
-echo "Create directories in ${_DIR} ..."
+[[ -d "${_DIR}" ]] || (echo "Create directory ${_DIR} ..."; mkdir "${_DIR}")
 _prepare_pacman || exit 1
 _install_base_packages || exit 1
 _clean_mkinitcpio || exit 1
 _clean_cache || exit 1
 _install_archboot || exit 1
 _umount_special || exit 1
-_clean_container || exit 1
-_clean_archboot_cache
 _generate_locales || exit 1
 _clean_locale
+_clean_container || exit 1
+_clean_archboot_cache
 _generate_keyring || exit 1
 _copy_mirrorlist_and_pacman_conf
 _change_pacman_conf || exit 1
