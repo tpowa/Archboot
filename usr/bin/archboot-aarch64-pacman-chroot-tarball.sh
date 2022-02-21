54d075c34ef09d36c3c95dbf12c5a1c45416fd11
Tobias Powalowski <tpowa@archlinux.org>
2022-02-21 15:24:13 +0100

    allow pacman-chroot building for all users

--- usr/bin/archboot-aarch64-pacman-chroot-tarball.sh
+++ usr/bin/archboot-aarch64-pacman-chroot-tarball.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 # created by Tobias Powalowski <tpowa@archlinux.org>
+_ARCH="aarch64"
 source /usr/lib/archboot/functions
+source /usr/lib/archboot/container_functions
+source /etc/archboot/defaults
 _LATEST_ARM64="http://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz"
-_PACMAN_CHROOT="pacman-aarch64-chroot"
 _KEYRING="archlinuxarm"
 
 _usage () {
     echo "CREATE AARCH64 PACMAN CHROOT"
     echo "-----------------------------"
     echo "This will create an aarch64 pacman chroot tarball on x86_64"
-    echo "Usage: ${_BASENAME} <directory>"
+    echo "Usage: ${_BASENAME} <build-directory>"
     exit 0
 }
 
@@ -20,14 +22,14 @@ _x86_64_check
 
 echo "Starting container creation ..."
 echo "Create directory ${1} ..."
-mkdir -p "${1}"/"${_PACMAN_CHROOT}"
-echo "Downloading archlinuxarm aarch64..."
+mkdir -p "${1}"/"${_PACMAN_AARCH64}"
+echo "Downloading archlinuxarm aarch64 ..."
 ! [[ -f ArchLinuxARM-aarch64-latest.tar.gz ]] && wget "${_LATEST_ARM64}" >/dev/null 2>&1
 bsdtar -xf ArchLinuxARM-aarch64-latest.tar.gz -C "${1}"
 echo "Removing installation tarball ..."
 rm ArchLinuxARM-aarch64-latest.tar.gz
-_generate_locales "${1}"
-_generate_keyring "${1}"
+_generate_locales "${1}" || exit 1
+_generate_keyring "${1}" || exit 1
 # enable parallel downloads
 sed -i -e 's:^#ParallelDownloads:ParallelDownloads:g' "${1}"/etc/pacman.conf
 # fix network in container
@@ -35,26 +37,28 @@ rm "${1}/etc/resolv.conf"
 echo "nameserver 8.8.8.8" > "${1}/etc/resolv.conf"
 # update container to latest packages
 echo "Installing pacman to container ..."
-mkdir -p "${1}/${_PACMAN_CHROOT}/var/lib/pacman"
-sleep 1
-systemd-nspawn -D "${1}" pacman --root "/${_PACMAN_CHROOT}" -Sy awk pacman --ignore systemd-resolvconf --noconfirm >/dev/null 2>&1
-# generate pacman keyring
-echo "Generate pacman keyring in container ..."
-systemd-nspawn -D "${1}/${_PACMAN_CHROOT}" pacman-key --init >/dev/null 2>&1
-systemd-nspawn -D "${1}/${_PACMAN_CHROOT}" pacman-key --populate archlinuxarm >/dev/null 2>&1
-sed -i -e 's:^#ParallelDownloads:ParallelDownloads:g' "${1}/${_PACMAN_CHROOT}"/etc/pacman.conf
+mkdir -p "${1}/${_PACMAN_AARCH64}/var/lib/pacman"
+systemd-nspawn -D "${1}" pacman --root "/${_PACMAN_AARCH64}" -Sy awk pacman --ignore systemd-resolvconf --noconfirm >/dev/null 2>&1
 # copy locale
 echo "Copying locales to container ..."
-cp "${1}/usr/lib/locale/locale-archive" "${1}/${_PACMAN_CHROOT}/usr/lib/locale/locale-archive"
+cp "${1}/usr/lib/locale/locale-archive" "${1}/${_PACMAN_AARCH64}/usr/lib/locale/locale-archive" || exit 1
+# generate pacman keyring
+_generate_keyring "${1}/${_PACMAN_AARCH64}" || exit 1
+# enable parallel downloads
+sed -i -e 's:^#ParallelDownloads:ParallelDownloads:g' "${1}/${_PACMAN_AARCH64}"/etc/pacman.conf
 # fix network in container
-rm "${1}/${_PACMAN_CHROOT}/etc/resolv.conf"
-echo "nameserver 8.8.8.8" > "${1}/${_PACMAN_CHROOT}/etc/resolv.conf"
-echo "Clean container, delete not needed files from ${1}/${_PACMAN_CHROOT} ..."
-rm -r "${1}/${_PACMAN_CHROOT}"/usr/include
-rm -r "${1}/${_PACMAN_CHROOT}"/usr/share/{man,doc,info}
+rm "${1}/${_PACMAN_AARCH64}/etc/resolv.conf"
+echo "nameserver 8.8.8.8" > "${1}/${_PACMAN_AARCH64}/etc/resolv.conf"
+_CLEANUP_CONTAINER="1" _clean_container "${1}/${_PACMAN_AARCH64}" || exit 1
+_CLEANUP_CACHE="1" _clean_cache "${1}/${_PACMAN_AARCH64}" || exit 1
 echo "Generating tarball ..."
-tar -acf ${_PACMAN_CHROOT}-latest.tar.zst -C "${1}"/"${_PACMAN_CHROOT}" .
+tar -acf ${_PACMAN_AARCH64_CHROOT} -C "${1}"/"${_PACMAN_AARCH64}" . || exit 1
 echo " Removing ${1} ..."
 rm -r "${1}"
 echo "Finished container tarball."
+echo "Sign tarball ..."
+sudo -u "${_USER}" gpg ${_GPG} ${_PACMAN_AARCH64_CHROOT} || exit 1
+chown "${_USER}":${GROUP} ${_PACMAN_AARCH64_CHROOT}{,.sig} || exit 1
+echo "Uploading tarball to ${_SERVER}:${_SERVER_PACMAN_AARCH64} ..."
+sudo -u "${_USER}" scp ${_PACMAN_AARCH64_CHROOT}{,.sig} ${_SERVER}:${_SERVER_PACMAN_AARCH64} || exit 1
 
