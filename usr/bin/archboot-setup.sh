e5371511292b69443ec8b992b66eac0b5dcf2fdc
Tobias Powalowski <tpowa@archlinux.org>
2022-03-19 17:15:44 +0100

    add Local mode message

--- usr/bin/archboot-setup.sh
+++ usr/bin/archboot-setup.sh
@@ -2452,7 +2452,7 @@ getsource() {
     PACMAN_CONF=""
     if [[ -e "${LOCAL_DB}" ]]; then
         custom_pacman_conf
-        DIALOG --msgbox "Setup is running in Local mode. Local package cache is used.\n\nIf you want to switch to Online mode, you have to delete /var/cache/pacman/pkg/archboot.db and rerun this step." 8 60
+        DIALOG --msgbox "Setup is running in <Local mode>.\nOnly Local package database is used for package installation.\n\nIf you want to switch to <Online mode>, you have to delete /var/cache/pacman/pkg/archboot.db and rerun this step." 10 70
         S_SRC=1
     else
 
