aabbf99e1abd94611bf0af88a90f610f5504db27
Tobias Powalowski <tpowa@archlinux.org>
2022-03-21 11:19:03 +0100

    fix typo in pgrep

--- usr/bin/archboot-update-installer.sh
+++ usr/bin/archboot-update-installer.sh
@@ -100,7 +100,7 @@ if [[ "${_L_COMPLETE}" == "1" || "${_L_INSTALL_COMPLETE}" == "1" ]]; then
         [[ -d "/usr/share/${i}" ]] && rm -r "/usr/share/${i}"
     done
     echo "Step 2/9: Waiting for gpg pacman keyring import to finish ..."
-    while pgreg -x gpg > /dev/null 2>&1; do
+    while pgrep -x gpg > /dev/null 2>&1; do
         sleep 1
     done
     echo "Step 3/9: Generating archboot container in ${_W_DIR} ..."
