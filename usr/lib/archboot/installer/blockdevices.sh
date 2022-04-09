7d504aa991438f707ce9638cd91b78634275cbc2
Tobias Powalowski <tpowa@archlinux.org>
2022-04-09 20:07:05 +0200

    remove space

--- usr/lib/archboot/installer/blockdevices.sh
+++ usr/lib/archboot/installer/blockdevices.sh
@@ -761,7 +761,7 @@ _createpv()
         # select the first device to use
         DEVNUMBER=1
         #shellcheck disable=SC2086
-        DIALOG --menu "Select device number ${DEVNUMBER} for physical volume" 11 50 8 ${PARTS} 2>"${ANSWER}" || return 1
+        DIALOG --menu "Select device number ${DEVNUMBER} for physical volume" 8 50 5 ${PARTS} 2>"${ANSWER}" || return 1
         PART=$(cat "${ANSWER}")
         echo "${PART}" >>/tmp/.pvs-create
         while [[ "${PART}" != "DONE" ]]; do
