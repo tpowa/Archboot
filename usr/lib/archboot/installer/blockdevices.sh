d371496b15ee3e3c55e549801de4a743deef7400
Tobias Powalowski <tpowa@archlinux.org>
2022-04-11 18:09:49 +0200

    longer menu

--- usr/lib/archboot/installer/blockdevices.sh
+++ usr/lib/archboot/installer/blockdevices.sh
@@ -762,7 +762,7 @@ _createpv()
         # select the first device to use
         DEVNUMBER=1
         #shellcheck disable=SC2086
-        DIALOG --menu "Select device number ${DEVNUMBER} for physical volume" 13 50 10 ${PARTS} 2>"${ANSWER}" || return 1
+        DIALOG --menu "Select device number ${DEVNUMBER} for physical volume" 15 50 12 ${PARTS} 2>"${ANSWER}" || return 1
         PART=$(cat "${ANSWER}")
         echo "${PART}" >>/tmp/.pvs-create
         while [[ "${PART}" != "DONE" ]]; do
@@ -771,7 +771,7 @@ _createpv()
             PARTS="${PARTS//${PART}\ _/}"
             # add more devices
             #shellcheck disable=SC2086
-            DIALOG --menu "Select additional device number ${DEVNUMBER} for physical volume" 13 60 10 ${PARTS} DONE _ 2>"${ANSWER}" || return 1
+            DIALOG --menu "Select additional device number ${DEVNUMBER} for physical volume" 15 60 12 ${PARTS} DONE _ 2>"${ANSWER}" || return 1
             PART=$(cat "${ANSWER}")
             [[ "${PART}" = "DONE" ]] && break
             echo "${PART}" >>/tmp/.pvs-create
