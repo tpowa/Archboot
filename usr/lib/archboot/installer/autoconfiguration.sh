fe704146d59a62a5129588762db92f5ac8ef6d77
Tobias Powalowski <tpowa@archlinux.org>
2022-04-26 07:45:50 +0200

    fix kver detection on aarch64

--- usr/lib/archboot/installer/autoconfiguration.sh
+++ usr/lib/archboot/installer/autoconfiguration.sh
@@ -116,10 +116,10 @@ auto_mkinitcpio() {
         fi
     fi
     # get kernel version
-    if [[ ${RUNNING_ARCH} == "x86_64" ]]; then
+    if [[ "${RUNNING_ARCH}" == "x86_64" ]]; then
         offset=$(hexdump -s 526 -n 2 -e '"%0d"' "${DESTDIR}/boot/${VMLINUZ}")
         read -r HWKVER _ < <(dd if="${DESTDIR}/boot/${VMLINUZ}" bs=1 count=127 skip=$(( offset + 0x200 )) 2>/dev/null)
-    elif [[ ${RUNNING_ARCH} == "aarch64" ]]; then
+    elif [[ "${RUNNING_ARCH}" == "aarch64" ]]; then
             read _ _ HWKVER _ < <(grep -m1 -aoE 'Linux version .(\.[-[:alnum:]]+)+' "${DESTDIR}/boot/${VMLINUZ}")
             # try if the image is gzip compressed
             if [[ -z "${HWKVER}" ]]; then
