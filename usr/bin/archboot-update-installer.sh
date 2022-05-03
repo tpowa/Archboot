9eda1ebd561474d10b668d90227ffd78a2cddfaa
Tobias Powalowski <tpowa@archlinux.org>
2022-05-03 11:03:43 +0200

    add X support with XFCE desktop and VNC shar

--- usr/bin/archboot-update-installer.sh
+++ usr/bin/archboot-update-installer.sh
@@ -78,7 +78,7 @@ fi
 
 # Launch xfce
 if [[ "${_L_XFCE}" == "1" ]]; then
-    echo -e "\033[1mStep 1/1:\033[0m Preparing XFCE desktop ${_W_DIR} now ..."
+    echo -e "\033[1mStep 1/1:\033[0m Preparing XFCE desktop now ..."
     echo "          This will need some time ..."
     _launch_xfce >/dev/tty7 2>&1
 fi
