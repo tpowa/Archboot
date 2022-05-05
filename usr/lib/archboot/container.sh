fb9798fdae3e57b834d133bb9151a04e7056eebd
Tobias Powalowski <tpowa@archlinux.org>
2022-05-05 11:13:56 +0200

    more group fixes

--- usr/lib/archboot/container.sh
+++ usr/lib/archboot/container.sh
@@ -191,15 +191,16 @@ _set_hostname() {
 _fix_groups() {
     echo "Recreate system groups ..."
     rm "${1}"/etc/{group,gshadow}
-    systemd-nspawn -q -D "${1}" systemd-sysusers  >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" systemd-sysusers >/dev/null 2>&1
     # fix missing group in iwd FS#74646
-    systemd-nspawn -q -D "${1}" groupadd netdev  >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd netdev >/dev/null 2>&1
     # add missing groups
-    systemd-nspawn -q -D "${1}" groupadd polkitd  >/dev/null 2>&1
-    systemd-nspawn -q -D "${1}" groupadd colord  >/dev/null 2>&1
-    systemd-nspawn -q -D "${1}" groupadd usbmux  >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd -r -g 102 polkitd >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd -r -g 949 colord >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd -r -g 140 usbmux >/dev/null 2>&1
     # add missing groups on aarch64
-    systemd-nspawn -q -D "${1}" groupadd proc  >/dev/null 2>&1
-    systemd-nspawn -q -D "${1}" groupadd network  >/dev/null 2>&1
-    systemd-nspawn -q -D "${1}" groupadd tss  >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd -r -g 26 proc >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupmems -g proc -a polkitd >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd -r -g 90 network >/dev/null 2>&1
+    systemd-nspawn -q -D "${1}" groupadd -r -g 975 tss >/dev/null 2>&1
 }
