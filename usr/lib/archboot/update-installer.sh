ef802f077b46176c7d0e191717971028d398dedb
Tobias Powalowski <tpowa@archlinux.org>
2022-05-14 20:43:39 +0200

    fix pacman.log location

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -261,7 +261,7 @@ _prepare_xfce() {
             pacman -S ${i} --config ${_PACMAN_CONF} --noconfirm >/dev/null 2>&1 || exit 1
             _cleanup_install
             _cleanup_cache
-            rm /var/cache/pacman.log
+            rm -f /var/log/pacman.log
         done
     else
         echo "Updating environment to latest packages (ignoring packages: ${_X_IGNORE}) ..."
@@ -280,7 +280,7 @@ _prepare_xfce() {
             pacman -S ${i} --noconfirm >/dev/null 2>&1 || exit 1
             _cleanup_install
             _cleanup_cache
-            rm -f /var/cache/pacman.log
+            rm -f /var/log/pacman.log
         done
     fi
     echo "Removing not used icons ..."
