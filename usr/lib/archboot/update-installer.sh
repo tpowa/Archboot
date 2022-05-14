6e4ee6df8d974eda8edf9fa05d301ec35e2cfcee
Tobias Powalowski <tpowa@archlinux.org>
2022-05-14 21:25:05 +0200

    lower memory requirement for xfce launch to 3.2G

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -32,7 +32,7 @@ usage () {
     echo -e "                  This operation needs at least \033[1m3.9 GB RAM\033[0m."
     echo ""
     echo -e " \033[1m-launch-xfce\033[0m     Launch XFCE desktop with VNC sharing enabled."
-    echo -e "                  This operation needs at least \033[1m3.5 GB RAM\033[0m."
+    echo -e "                  This operation needs at least \033[1m3.2 GB RAM\033[0m."
     echo ""
     echo -e " \033[1m-h\033[0m               This message."
     exit 0
