2a654e36048f893d70b9e90e64e92d334c7ee01f
Tobias Powalowski <tpowa@archlinux.org>
2022-05-20 20:03:19 +0200

    add symbols to panel

--- usr/lib/archboot/kde.sh
+++ usr/lib/archboot/kde.sh
@@ -21,7 +21,7 @@ _start_kde() {
 
 _configure_kde() {
     echo "Configuring KDE ..."
-    sed -i -e 's#<default>applications:.*#<default>applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser,gparted.desktop,archboot.desktop</default>#g' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config
+    sed -i -e 's#<default>applications:.*#<default>applications:systemsettings.desktop,applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser,gparted.desktop,archboot.desktop</default>#g' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
     echo "Replacing wallpaper ..."
     for i in /usr/share/wallpapers/Next/contents/images/*; do
         cp /usr/share/archboot/grub/archboot-background.png $i
