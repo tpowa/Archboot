2ab2af01024fa08492a87e23976c2de662ed7356
Tobias Powalowski <tpowa@archlinux.org>
2022-05-29 19:18:17 +0200

    move message on kde before starting wayland

--- usr/lib/archboot/kde-wayland.sh
+++ usr/lib/archboot/kde-wayland.sh
@@ -74,6 +74,6 @@ EOF
 
 _start_kde_wayland() {
     echo -e "Launching KDE/Plasma Wayland now, logging is done on \033[1m/dev/tty7\033[0m ..."
+	echo -e "To relaunch KDE/Plasma Wayland use: \033[92mupdate-installer.sh -kde-wayland\033[0m"
     exec dbus-run-session startplasma-wayland >/dev/tty7 2>&1
-    echo -e "To relaunch KDE/Plasma Wayland use: \033[92mupdate-installer.sh -kde-wayland\033[0m"
 }
