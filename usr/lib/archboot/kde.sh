8965f25222c4e8a4261e94df061bde1be0edd3d0
Tobias Powalowski <tpowa@archlinux.org>
2022-05-29 18:42:04 +0200

    add Plasma to KDE

--- usr/lib/archboot/kde.sh
+++ usr/lib/archboot/kde.sh
@@ -3,7 +3,7 @@
 
 _install_kde() {
     if ! [[ -e /usr/bin/startplasma-x11 ]]; then
-        echo -e "\033[1mStep 3/5:\033[0m Installing KDE desktop now ..."
+        echo -e "\033[1mStep 3/5:\033[0m Installing KDE/Plasma desktop now ..."
         echo "          This will need some time ..."
         _prepare_graphic "${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_KDE_PACKAGES}" >/dev/tty7 2>&1
         echo -e "\033[1mStep 4/5:\033[0m Configuring KDE desktop ..."
@@ -70,7 +70,7 @@ EOF
 }
 
 _start_kde() {
-    echo -e "Launching KDE now, logging is done on \033[1m/dev/tty8\033[0m ..."
+    echo -e "Launching KDE/Plasma now, logging is done on \033[1m/dev/tty8\033[0m ..."
     echo "export DESKTOP_SESSION=plasma" > /root/.xinitrc
     echo "startplasma-x11" >> /root/.xinitrc
     startx >/dev/tty8 2>&1
