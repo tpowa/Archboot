1b2d9aa91f9f0ccda446cf01599dfe84838a3b08
Tobias Powalowski <tpowa@archlinux.org>
2022-05-29 19:09:35 +0200

    readd exec and move message on gnome before starting wayland

--- usr/lib/archboot/kde.sh
+++ usr/lib/archboot/kde.sh
@@ -75,7 +75,7 @@ EOF
 _start_kde() {
     echo -e "Launching KDE/Plasma now, logging is done on \033[1m/dev/tty8\033[0m ..."
     echo "export DESKTOP_SESSION=plasma" > /root/.xinitrc
-    echo "startplasma-x11" >> /root/.xinitrc
+    echo "exec startplasma-x11" >> /root/.xinitrc
     startx >/dev/tty8 2>&1
     echo -e "To relaunch KDE desktop use: \033[92mstartx\033[0m"
 }
