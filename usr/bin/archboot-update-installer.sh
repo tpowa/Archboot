672dbc10d8100095f326b75085242206fb562709
Tobias Powalowski <tpowa@archlinux.org>
2022-06-01 19:27:51 +0200

    add tar to copy /

--- usr/bin/archboot-update-installer.sh
+++ usr/bin/archboot-update-installer.sh
@@ -44,8 +44,8 @@ if [[ "${_L_COMPLETE}" == "1" || "${_L_INSTALL_COMPLETE}" == "1" ]]; then
         mkfs.btrfs -q --mixed /dev/zram0
         mount /dev/zram0 /new_root
         echo $0 $1 > /etc/profile.d/zz-01-archboot.sh
-        tar -C / --exclude="./dev/*" --exclude="./proc/*" --exclude="./sys/*" --exclude="/tmp/*" --exclude="/run/*"\
-        --exclude="/mnt/*" --exclude="/media/*" --exclude="/lost+found" --exclude="/new_root/*" -clpf - . | tar -C /new_root -vxlspf -
+        tar -C / --exclude="./dev/*" --exclude="./proc/*" --exclude="./sys/*" --exclude="./tmp/*" --exclude="./run/*"\
+        --exclude="./mnt/*" --exclude="./media/*" --exclude="./lost+found" --exclude="./new_root/*" -clpf - . | tar -C /new_root -vxlspf -
         systemctl switch-root /new_root
     fi
     _update_installer_check
