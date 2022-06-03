a0a1a010f3fa5b39d8a2db34edbf393a31f0fdcc
Tobias Powalowski <tpowa@archlinux.org>
2022-06-03 09:32:00 +0200

    add local mode to all logins

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -2,6 +2,10 @@
 _welcome () {
     echo -e "\033[1mWelcome to \033[36mArch Linux \033[34m(archboot environment)\033[0m"
     echo -e "\033[1m--------------------------------------------------------------------\033[0m"
+    _local_mode
+}
+
+_local_mode () {
     if [[ -e /var/cache/pacman/pkg/archboot.db ]]; then
         echo -e "You are running in \033[92m\033[1mLocal mode\033[0m, with \033[1mlocal package repository\033[0m enabled.\033[0m"
         echo -e "To \033[1mswitch\033[0m to \033[1mOnline mode\033[0m:# \033[1m\033[91mrm /var/cache/pacman/pkg/archboot.db\033[0m\033[1m"
@@ -42,6 +46,7 @@ _run_update_installer() {
 }
 
 if [[ -e /usr/bin/setup ]]; then
+    _local_mode
     _enter_shell
     if ! [[ -e /tmp/.setup ]]; then
         setup
