7d90f629eacf98a4b487c25812115b4569a87687
Tobias Powalowski <tpowa@archlinux.org>
2022-06-11 12:38:08 +0200

    remove old _generate_locales

--- usr/bin/archboot-x86_64-create-container.sh
+++ usr/bin/archboot-x86_64-create-container.sh
@@ -18,7 +18,6 @@ _clean_cache "${1}"
 _install_archboot "${1}" || exit 1
 _clean_cache "${1}"
 _umount_special "${1}" || exit 1
-_generate_locales "${1}"
 _fix_groups "${1}"
 _clean_container "${1}"
 _clean_archboot_cache
