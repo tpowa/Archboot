0237778505e3a6bef5e5660540f76da7378139be
Tobias Powalowski <tpowa@archlinux.org>
2022-06-12 18:50:40 +0200

    move archboot_repo to archboot with adress archboot-repository

--- usr/lib/archboot/container.sh
+++ usr/lib/archboot/container.sh
@@ -98,10 +98,10 @@ _create_pacman_conf() {
     if [[ -z "${_INSTALL_SOURCE}" ]]; then
         echo "Use default pacman.conf ..."
         [[ "${2}" == "use_container_config" ]] && _PACMAN_CONF="${1}${_PACMAN_CONF}"
-        if ! grep -q archboot_repo "${_PACMAN_CONF}"; then
+        if ! grep -qw archboot "${_PACMAN_CONF}"; then
             echo "Adding archboot repository to ${_PACMAN_CONF} ..."
-            echo "[archboot_repo]" >> "${_PACMAN_CONF}"
-            echo "Server = https://pkgbuild.com/~tpowa/archboot-repo" >> "${_PACMAN_CONF}"
+            echo "[archboot]" >> "${_PACMAN_CONF}"
+            echo "Server = https://pkgbuild.com/~tpowa/archboot-repository" >> "${_PACMAN_CONF}"
         fi
     else
         echo "Use custom pacman.conf ..."
