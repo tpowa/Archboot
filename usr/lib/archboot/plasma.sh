4f97bfb212dbb9b06b303f9d4a2e8ab18556f4df
Tobias Powalowski <tpowa@archlinux.org>
2022-06-12 14:45:45 +0200

    rename KDE_PACKAGES to PLASMA_PACKAGES

--- usr/lib/archboot/plasma.sh
+++ usr/lib/archboot/plasma.sh
@@ -5,7 +5,7 @@ _install_kde() {
     if ! [[ -e /usr/bin/startplasma-x11 ]]; then
         echo -e "\033[1mStep 3/5:\033[0m Installing KDE/Plasma desktop now ..."
         echo "          This will need some time ..."
-        _prepare_graphic "${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_KDE_PACKAGES}" >/dev/tty7 2>&1
+        _prepare_graphic "${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_PLASMA_PACKAGES}" >/dev/tty7 2>&1
         echo -e "\033[1mStep 4/5:\033[0m Configuring KDE desktop ..."
         _configure_kde >/dev/tty7 2>&1
 	else
