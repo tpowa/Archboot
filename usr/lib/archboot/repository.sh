a4ad68c24599b49b8d6b9bfd7ffdbff5780535ef
Tobias Powalowski <tpowa@archlinux.org>
2022-06-21 21:54:51 +0200

    remove double quotes

--- usr/lib/archboot/repository.sh
+++ usr/lib/archboot/repository.sh
@@ -29,7 +29,7 @@ _download_packages() {
 
 _aarch64_download_packages() {
     mkdir "${1}"/blankdb
-    echo "Adding "${_GPG_KEY_ID}" to trusted keys"
+    echo "Adding ${_GPG_KEY_ID} to trusted keys"
     [[ -d "${1}"/usr/share/archboot/gpg ]] || mkdir -p "${1}"/usr/share/archboot/gpg
     cp "${_GPG_KEY}" "${1}"/"${_GPG_KEY}"
     systemd-nspawn -q -D "${1}" pacman-key --add "${_GPG_KEY}" >/dev/null 2>&1
