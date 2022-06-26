b5316bb7c46e207054f073cb4a7979e95a75657a
Tobias Powalowski <tpowa@archlinux.org>
2022-06-26 17:26:57 +0200

    show how big ramdisk is

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -278,6 +278,7 @@ _kexec() {
         echo -e "Running \033[1m\033[92mkexec\033[0m with \033[1mnew\033[0m KEXEC_FILE_LOAD ..."
         kexec -s -f /"${VMLINUZ}" --initrd="/initrd.img" --reuse-cmdline &
     else
+        echo "$(($(stat -c %s /initrd.img)*425/100000))"
         echo -e "Running \033[1m\033[92mkexec\033[0m with \033[1mold\033[0m KEXEC_LOAD ..."
         kexec -c -f --mem-max=0xA0000000 /"${VMLINUZ}" --initrd="/initrd.img" --reuse-cmdline &
     fi
