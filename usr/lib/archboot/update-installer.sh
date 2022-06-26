d424446645075e02cc3ea438fb220ebe7cd9d1fd
Tobias Powalowski <tpowa@archlinux.org>
2022-06-26 16:33:10 +0200

    nex try arithmethics

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -273,8 +273,8 @@ _create_initramfs() {
 }
 
 _kexec() {
-    # you need approx. 3x size for KEXEC_FILE_LOAD
-    if [[ "$(($(stat -c %s /initrd.img)*305/100000))" -lt "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" ]]; then
+    # you need approx. 4.25x size for KEXEC_FILE_LOAD
+    if [[ "$(($(stat -c %s /initrd.img)*425/100000))" -lt "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" ]]; then
         echo -e "Running \033[1m\033[92mkexec\033[0m with \033[1mnew\033[0m KEXEC_FILE_LOAD ..."
         kexec -s -f /"${VMLINUZ}" --initrd="/initrd.img" --reuse-cmdline &
     else
