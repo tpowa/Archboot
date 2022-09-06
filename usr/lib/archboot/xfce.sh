e07edf5eb5e4d76515eabf0bc34dc348f74636bb
Tobias Powalowski <tpowa@archlinux.org>
2022-09-06 09:38:13 +0200

    stop xfce if not possible to install

--- usr/lib/archboot/xfce.sh
+++ usr/lib/archboot/xfce.sh
@@ -5,7 +5,7 @@ _install_xfce() {
     if ! [[ -e /usr/bin/startxfce4 ]]; then
         echo -e "\033[1mStep 3/5:\033[0m Installing XFCE desktop now ..."
         echo "          This will need some time ..."
-        _prepare_graphic "${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_XFCE_PACKAGES}" >/dev/tty7 2>&1
+        _prepare_graphic "${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_XFCE_PACKAGES}" >/dev/tty7 2>&1 || exit 1
         echo -e "\033[1mStep 4/5:\033[0m Configuring XFCE desktop ..."
         _configure_xfce >/dev/tty7 2>&1
     else
