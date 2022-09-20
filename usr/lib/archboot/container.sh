c0a52aa31d02b7e9af4350e5734f5da29e8f9d81
Tobias Powalowski <tpowa@archlinux.org>
2022-09-20 09:14:19 +0200

    don't add GRAPHICAL packages to riscv64

--- usr/lib/archboot/container.sh
+++ usr/lib/archboot/container.sh
@@ -200,8 +200,8 @@ _other_install_base_packages() {
 }
 
 _other_install_archboot() {
-    # riscv64 need other packages
-    [[ "$(echo $(systemd-nspawn -q -D "${1}" uname -m) | sed -e 's#\r##g')" == "riscv64" ]] && _GRAPHICAL_PACKAGES="${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_STANDARD_BROWSER} ${_XFCE_PACKAGES}"
+    # riscv64 need does not support local image at the moment
+    [[ "$(echo $(systemd-nspawn -q -D "${1}" uname -m) | sed -e 's#\r##g')" == "riscv64" ]] && _GRAPHICAL_PACKAGES=""
     [[ "${_CLEANUP_CACHE}" == "1" ]] && _GRAPHICAL_PACKAGES=""
     [[ -d "${1}"/blankdb ]] || mkdir "${1}"/blankdb
     if [[ -e "${1}/$(basename "${_PACMAN_CONF}")"  ]]; then
