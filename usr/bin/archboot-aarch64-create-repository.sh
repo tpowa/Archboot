15daa3ea3660e74092461d14967ba62be577f1ed
Tobias Powalowski <tpowa@archlinux.org>
2022-10-02 10:33:14 +0200

    remove empy lines

--- usr/bin/archboot-aarch64-create-repository.sh
+++ usr/bin/archboot-aarch64-create-repository.sh
@@ -14,16 +14,17 @@ if [[ "${_RUNNING_ARCH}" == "aarch64" ]]; then
     _cachedir_check
     _create_pacman_conf "${_REPODIR}"
     _prepare_pacman "${_REPODIR}" || exit 1
+    _pacman_parameters "${_REPODIR}"
     _download_packages "${_REPODIR}" || exit 1
     _umount_special "${_REPODIR}" || exit 1
 fi
 if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
     _pacman_chroot "${_REPODIR}" "${_ARCHBOOT_AARCH64_CHROOT_PUBLIC}" "${_PACMAN_AARCH64_CHROOT}" || exit 1
     _create_pacman_conf "${_REPODIR}" "use_container_config"
-    _other_download_packages "${_REPODIR}" || exit 1
+    _pacman_parameters "${_REPODIR}" "use_binfmt"
+    _download_packages "${_REPODIR}" || exit 1
 fi
 _move_packages "${_REPODIR}" "${1}" || exit 1
 _cleanup_repodir "${_REPODIR}" || exit 1
 _create_archboot_db "${1}" || exit 1
 echo "Finished repository creation in ${_REPODIR} ."
-
