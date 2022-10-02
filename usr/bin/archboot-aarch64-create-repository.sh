bdb059f42da9d10b10445d3d0fcc379740d564f4
Tobias Powalowski <tpowa@archlinux.org>
2022-10-02 21:26:50 +0200

    fix repository creation

--- usr/bin/archboot-aarch64-create-repository.sh
+++ usr/bin/archboot-aarch64-create-repository.sh
@@ -22,7 +22,7 @@ if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
     _pacman_chroot "${_REPODIR}" "${_ARCHBOOT_AARCH64_CHROOT_PUBLIC}" "${_PACMAN_AARCH64_CHROOT}" || exit 1
     _create_pacman_conf "${_REPODIR}" "use_container_config"
     _pacman_parameters "${_REPODIR}" "use_binfmt"
-    _download_packages "${_REPODIR}" || exit 1
+    _download_packages "${_REPODIR}" "use_binfmt" || exit 1
 fi
 _move_packages "${_REPODIR}" "${1}" || exit 1
 _cleanup_repodir "${_REPODIR}" || exit 1
