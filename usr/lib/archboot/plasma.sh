1ad53a1ab0f7a9b578751c246d83958674d1f760
Tobias Powalowski <tpowa@archlinux.org>
2022-10-04 10:00:47 +0200

    rename _kde funtions to _plasma

--- usr/lib/archboot/plasma.sh
+++ usr/lib/archboot/plasma.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # created by Tobias Powalowski <tpowa@archlinux.org>
 
-_install_kde() {
+_install_plasma() {
     _PACKAGES="${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_STANDARD_BROWSER} ${_PLASMA_PACKAGES}"
     prepare_plasma >/dev/tty7 2>&1
 }
 
-_start_kde() {
+_start_plasma() {
     echo -e "Launching KDE/Plasma now, logging is done on \033[1m/dev/tty8\033[0m ..."
     echo "export DESKTOP_SESSION=plasma" > /root/.xinitrc
     echo "exec startplasma-x11" >> /root/.xinitrc
