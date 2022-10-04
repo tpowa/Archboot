5578cc3b41ea8180f96def802acca4a49a58d998
Tobias Powalowski <tpowa@archlinux.org>
2022-10-04 09:56:11 +0200

    move plasma install to plasma_prepare

--- usr/lib/archboot/plasma.sh
+++ usr/lib/archboot/plasma.sh
@@ -2,16 +2,8 @@
 # created by Tobias Powalowski <tpowa@archlinux.org>
 
 _install_kde() {
-    if ! [[ -e /usr/bin/startplasma-x11 ]]; then
-        echo -e "\033[1mStep 3/5:\033[0m Installing KDE/Plasma desktop now ..."
-        echo "          This will need some time ..."
-        _prepare_graphic "${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_STANDARD_BROWSER} ${_PLASMA_PACKAGES}" >/dev/tty7 2>&1
-        echo -e "\033[1mStep 4/5:\033[0m Configuring KDE desktop ..."
-        _configure_plasma >/dev/tty7 2>&1
-	else
-		echo -e "\033[1mStep 3/5:\033[0m Installing KDE/Plasma desktop already done ..."
-		echo -e "\033[1mStep 4/5:\033[0m Configuring KDE desktop already done ..."
-    fi
+    _PACKAGES="${_XORG_PACKAGE} ${_VNC_PACKAGE} ${_STANDARD_PACKAGES} ${_STANDARD_BROWSER} ${_PLASMA_PACKAGES}"
+    prepare_plasma >/dev/tty7 2>&1
 }
 
 _start_kde() {
