543abf67f746f5e55f757e1508bed1e4a39c1743
Tobias Powalowski <tpowa@archlinux.org>
2022-10-09 15:09:13 +0200

    add waiting for mount

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -334,6 +334,10 @@ _cleanup_cache() {
 # needed for programs which check disk space
 _home_root_mount() {
     if ! mountpoint /home >/dev/null 2>&1; then
+        echo "Waiting for mount being available ..."
+        while ! mount >/dev/null 2>&1; do
+            sleep 1
+        done
         echo "Mount tmpfs on /home ..."
         mount -t tmpfs tmpfs /home
     fi
