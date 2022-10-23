d2cb67fa335d267c083086c32c06fe7a67a7a65a
Tobias Powalowski <tpowa@archlinux.org>
2022-10-23 09:20:54 +0200

    revert cache move

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -240,8 +240,7 @@ _create_container() {
         # add the db too on reboot
         install -D -m644 /var/cache/pacman/pkg/archboot.db "${_W_DIR}"/var/cache/pacman/pkg/archboot.db
         if [[ "${_L_INSTALL_COMPLETE}" == "1" ]]; then
-            mv /var/cache/pacman/pkg/* "${_W_DIR}"/var/cache/pacman/pkg/
-            "archboot-${_RUNNING_ARCH}-create-container.sh" "${_W_DIR}" -cc --install-source=file://"${_W_DIR}"/var/cache/pacman/pkg >/dev/tty7 2>&1 || exit 1
+            "archboot-${_RUNNING_ARCH}-create-container.sh" "${_W_DIR}" -cc --install-source=file:///var/cache/pacman/pkg >/dev/tty7 2>&1 || exit 1
         fi
         # needed for checks
         cp "${_W_DIR}"/var/cache/pacman/pkg/archboot.db /var/cache/pacman/pkg/archboot.db
