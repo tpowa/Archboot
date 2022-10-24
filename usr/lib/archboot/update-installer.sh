13358f1677c072fcd84eb2472bc2bfd478a563cb
Tobias Powalowski <tpowa@archlinux.org>
2022-10-24 14:08:06 +0200

    speed up deletion

--- usr/lib/archboot/update-installer.sh
+++ usr/lib/archboot/update-installer.sh
@@ -283,9 +283,9 @@ _create_initramfs() {
     bsdtar --uid 0 --gid 0 --null -cnf - -T - |
     bsdtar --null -cf - --format=newc @- | zstd --rm -T0> /initrd.img &
     sleep 5
-    for i in  $(find . -mindepth 1 -type f | sort -z); do
+    for i in $(find . -mindepth 1 -type f | sort -z); do
         rm "${i}"
-        sleep 0.05
+        sleep 0.015
     done
     while pgrep -x zstd > /dev/null 2>&1; do
         _clean_kernel_cache
