ee1a5ffe950505b703a0fead523ab50b7701806a
Tobias Powalowski <tpowa@archlinux.org>
2022-10-25 07:25:55 +0200

    add download_graphical to aarch64 and riscv64

--- usr/bin/archboot-riscv64-create-container.sh
+++ usr/bin/archboot-riscv64-create-container.sh
@@ -21,7 +21,8 @@ if [[ "${_RUNNING_ARCH}" == "riscv64" ]]; then
     _umount_special "${1}" || exit 1
     _fix_groups "${1}"
     _clean_container "${1}"
-    _clean_archboot_cache
+    _download_graphical "${1}" || exit 1
+    _clean_cache "${1}"
     _generate_keyring "${1}" || exit 1
     _copy_mirrorlist_and_pacman_conf "${1}"
 fi
@@ -33,6 +34,7 @@ if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
     _install_archboot "${1}" "use_binfmt" || exit 1
     _fix_groups "${1}"
     _clean_mkinitcpio "${1}"
+    _download_graphical "${1}" || exit 1
     _clean_cache "${1}"
     _clean_container "${1}" 2>/dev/null
 fi
