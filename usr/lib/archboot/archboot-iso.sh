b1f227b0c031b8628102d52ea4620cd2bca6cf4e
Tobias Powalowski <tpowa@archlinux.org>
2022-11-22 15:36:02 +0100

    merge iso into one file

---
+++ usr/lib/archboot/archboot-iso.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# created by Tobias Powalowski <tpowa@archlinux.org>
+. /usr/lib/archboot/common.sh
+. /usr/lib/archboot/iso.sh
+[[ -z "${1}" ]] && _usage
+_parameters "$@"
+_root_check
+[[ "${_GENERATE}" == "1" ]] || _usage
+echo "${0}" | grep -qw aarch64 && _aarch64_check
+echo "${0}" | grep -qw riscv64 && _riscv64_check
+echo "${0}" | grep -qw x86_64 && _x86_64_check
+_config
+if echo "${0}" | grep -qw aarch64 || echo "${0}" | grep -qw x86_64; then
+    # running system = aarch64 or x86_64
+    echo "Starting ISO creation ..."
+    _prepare_kernel_initramfs_files || exit 1
+    _prepare_ucode || exit 1
+    if echo "${0}" | grep -qw aarch64; then
+        _prepare_fedora_shim_bootloaders_aarch64 || exit 1
+        _prepare_uefi_AA64 || exit 1
+    fi
+    if echo "${0}" | grep -qw x86_64; then
+        _prepare_fedora_shim_bootloaders_x86_64 || exit 1
+        _prepare_uefi_shell_tianocore || exit 1
+        _prepare_uefi_X64 || exit 1
+        _prepare_uefi_IA32 || exit 1
+    fi
+    _prepare_efitools_uefi || exit 1
+    _prepare_background || exit 1
+    _reproducibility
+    _prepare_uefi_image || exit 1
+    _reproducibility
+    _grub_mkrescue || exit 1
+    _reproducibility_iso  || exit 1
+else
+    # running system = riscv64
+    echo "Starting Image creation ..."
+    _prepare_kernel_initramfs_files_RISCV64 || exit 1
+    _prepare_extlinux_conf || exit 1
+    _reproducibility
+    _uboot || exit 1
+fi
+_create_cksum || exit 1
+_cleanup_iso || exit 1
+if echo "${0}" | grep -qw aarch64 || echo "${0}" | grep -qw x86_64; then
+    echo "Finished ISO creation."
+else
+    echo "Finished Image creation."
+fi
