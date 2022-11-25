5c201e4a116400ec349523e9abaf7ff32512c5c3
Tobias Powalowski <tpowa@archlinux.org>
2022-11-25 11:59:04 +0100

    reformat message

--- usr/lib/archboot/installer/configuration.sh
+++ usr/lib/archboot/installer/configuration.sh
@@ -54,7 +54,7 @@ set_password() {
             echo "${PASSWORD}" >> /tmp/.password
             PASSWORD=/tmp/.password
         else
-            DIALOG --msgbox "Error:\nPassword didn't match, please enter again." 5 50
+            DIALOG --msgbox "Error:\nPassword didn't match, please enter again." 5 50 8
             PASSWORD=""
             PASS=""
             PASS2=""
