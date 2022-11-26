93d9cad2d3115ad1dc7f42e671c2dcd11104b0ec
Tobias Powalowski <tpowa@archlinux.org>
2022-11-26 13:34:13 +0100

    only show Regions

--- usr/bin/archboot-tz.sh
+++ usr/bin/archboot-tz.sh
@@ -57,7 +57,7 @@ dotimezone () {
 SET_ZONE=""
 while ! [[ "${SET_ZONE}" = "1" ]]; do
     REGIONS=""
-    for i in $(timedatectl --no-pager list-timezones | cut -d '/' -f 1 | sort -u); do
+    for i in $(timedatectl --no-pager list-timezones | cut -d '/' -f 1 | grep -v "[A-Z]$" | sort -u); do
         REGIONS="${REGIONS} ${i} -"
     done
     #shellcheck disable=SC2086
