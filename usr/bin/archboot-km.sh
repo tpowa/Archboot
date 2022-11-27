fccc350c9ffd1f596faba05705140cdd5418e3f5
Tobias Powalowski <tpowa@archlinux.org>
2022-11-27 16:32:32 +0100

    remove not working keymaps

--- usr/bin/archboot-km.sh
+++ usr/bin/archboot-km.sh
@@ -50,7 +50,7 @@ dokeymap() {
     fi
     ANSWER=$(cat ${ANSWER})
     KEYMAPS=""
-    for i in $(${KEYMAP} | grep -w "${ANSWER}" | grep -v 'mac' | grep -v 'amiga'); do
+    for i in $(${KEYMAP} | grep -w "${ANSWER}" | grep -v 'mac' | grep -v 'amiga' | grep -v 'sun'); do
         KEYMAPS="${KEYMAPS} ${i} -"
     done
     #shellcheck disable=SC2086
