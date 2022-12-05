46c441c0fce5aebb2ce8978501583443f6ffa57f
Tobias Powalowski <tpowa@archlinux.org>
2022-12-05 21:35:31 +0100

    bigger menu

--- usr/bin/archboot-tz.sh
+++ usr/bin/archboot-tz.sh
@@ -58,7 +58,7 @@ SET_ZONE=""
 while ! [[ "${SET_ZONE}" = "1" ]]; do
     REGIONS="Africa - America - Asia - Australia - Europe -"
     #shellcheck disable=SC2086
-    DIALOG --menu "Please Select A Region:" 10 40 5 ${REGIONS} 2>${ANSWER}
+    DIALOG --menu "Please Select A Region:" 12 40 7 ${REGIONS} 2>${ANSWER}
     region=$(cat ${ANSWER})
     ZONES=""
     for i in $(timedatectl --no-pager list-timezones | grep -w "${region}" | cut -d '/' -f 2 | sort -u); do
