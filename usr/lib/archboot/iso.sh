f04a54140b65b10677ca86aec831301960c8a1fc
Tobias Powalowski <tpowa@archlinux.org>
2022-12-09 06:55:23 +0100

    fixup grub menus, and uefi IA shell v1

--- usr/lib/archboot/iso.sh
+++ usr/lib/archboot/iso.sh
@@ -127,7 +127,7 @@ _prepare_uefi_shell_tianocore() {
     ## Install Tianocore UDK/EDK2 ShellBinPkg UEFI IA32 "Full Shell" - For UEFI Spec. >=2.3 systems
     cp /usr/share/edk2-shell/ia32/Shell.efi "${_ISODIR}/EFI/TOOLS/SHELLIA32_V2.EFI"
     ## InstallTianocore UDK/EDK2 EdkShellBinPkg UEFI IA32 "Full Shell" - For UEFI Spec. <2.3 systems
-    cp /usr/share/edk2-shell/ia32/Shell_Full.efi "${_ISODIR}/EFI/TOOLS/SHELLIA_V1.EFI"
+    cp /usr/share/edk2-shell/ia32/Shell_Full.efi "${_ISODIR}/EFI/TOOLS/SHELLIA32_V1.EFI"
 }
 
 # build grubXXX with all modules: http://bugs.archlinux.org/task/71382
