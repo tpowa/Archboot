61cb21284a8882a9fd86e4fa29889e41b93cb3d9
Tobias Powalowski <tpowa@archlinux.org>
2023-01-02 22:00:27 +0100

    replace which with command -v

--- usr/lib/archboot/installer/autoprepare.sh
+++ usr/lib/archboot/installer/autoprepare.sh
@@ -54,14 +54,14 @@ autoprepare() {
     fi
     while [[ "${DEFAULTFS}" == "" ]]; do
         FSOPTS=""
-        [[ "$(which mkfs.btrfs 2>/dev/null)" ]] && FSOPTS="${FSOPTS} btrfs Btrfs"
-        [[ "$(which mkfs.ext4 2>/dev/null)" ]] && FSOPTS="${FSOPTS} ext4 Ext4"
-        [[ "$(which mkfs.ext3 2>/dev/null)" ]] && FSOPTS="${FSOPTS} ext3 Ext3"
-        [[ "$(which mkfs.ext2 2>/dev/null)" ]] && FSOPTS="${FSOPTS} ext2 Ext2"
-        [[ "$(which mkfs.xfs 2>/dev/null)" ]] && FSOPTS="${FSOPTS} xfs XFS"
-        [[ "$(which mkfs.f2fs 2>/dev/null)" ]] && FSOPTS="${FSOPTS} f2fs F2FS"
-        [[ "$(which mkfs.nilfs2 2>/dev/null)" ]] && FSOPTS="${FSOPTS} nilfs2 Nilfs2"
-        [[ "$(which mkfs.jfs 2>/dev/null)" ]] && FSOPTS="${FSOPTS} jfs JFS"
+        command -v mkfs.btrfs 2>/dev/null && FSOPTS="${FSOPTS} btrfs Btrfs"
+        command -v mkfs.ext4 2>/dev/null && FSOPTS="${FSOPTS} ext4 Ext4"
+        command -v mkfs.ext3 2>/dev/null && FSOPTS="${FSOPTS} ext3 Ext3"
+        command -v mkfs.ext2 2>/dev/null && FSOPTS="${FSOPTS} ext2 Ext2"
+        command -v mkfs.xfs 2>/dev/null && FSOPTS="${FSOPTS} xfs XFS"
+        command -v mkfs.f2fs 2>/dev/null && FSOPTS="${FSOPTS} f2fs F2FS"
+        command -v mkfs.nilfs2 2>/dev/null && FSOPTS="${FSOPTS} nilfs2 Nilfs2"
+        command -v mkfs.jfs 2>/dev/null && FSOPTS="${FSOPTS} jfs JFS"
         # create 1 MB bios_grub partition for grub BIOS GPT support
         if [[ "${GUIDPARAMETER}" == "1" ]]; then
             GUID_PART_SIZE="2"
