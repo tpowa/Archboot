3252f465965cfebe1ce0fa314ca858010e201be7
Tobias Powalowski <tpowa@archlinux.org>
2023-01-15 19:49:10 +0100

    overwrite signature

--- usr/lib/archboot/installer/blockdevices.sh
+++ usr/lib/archboot/installer/blockdevices.sh
@@ -704,7 +704,7 @@ _getavailablevg()
     for dev in $(vgs -o vg_name --noheading); do
         if ! vgs -o vg_free --noheading --units m "${dev}" | grep -q " 0m$"; then
             #shellcheck disable=SC2028
-            echo "${dev} $(vgs -o vg_free --noheading --units m ${dev})"'\\n'
+            echo "${dev} $(vgs -o vg_free --noheading --units m ${dev})"\n
         fi
     done
 }
@@ -802,7 +802,7 @@ _createlv()
                 if [[ -z "${_LV_SIZE}" ]]; then
                     _dialog --yesno "Would you like to create Logical Volume with no free space left?" 0 0 && _LV_ALL=1
                     if [[ -z "${_LV_ALL}" ]]; then
-                         _LV_SIZE=0
+                         _LV_SIZE="0"
                     fi
                 fi
                 if [[ "${_LV_SIZE}" == 0 ]]; then
@@ -820,14 +820,14 @@ _createlv()
         [[ -z "${_LV_ALL}" ]] && _dialog --defaultno --yesno "Would you like to create Logical Volume as a contiguous partition, that means that your space doesn't get partitioned over one or more disks nor over non-contiguous physical extents.\n(usefull for swap space etc.)?" 0 0 && _LV_CONTIGUOUS=1
         if [[ -n "${_LV_CONTIGUOUS}" ]]; then
             _CONTIGUOUS=yes
-            _LV_EXTRA="-C y"
+            _LV_EXTRA="-W y -C y"
         else
             _CONTIGUOUS=no
-            _LV_EXTRA=""
+            _LV_EXTRA="-W y"
         fi
         [[ -z "${_LV_SIZE}" ]] && _LV_SIZE="All free space left"
         # final step ask if everything is ok?
-        _dialog --yesno "Would you like to create Logical Volume ${_LVDEV} like this?\nVolume Group:\n${_LV}\nVolume Size:\n${_LV_SIZE}\nContiguous Volume:\n${_CONTIGUOUS}" 0 0 && _LVFINISH="DONE"
+        _dialog --yesno "Would you like to create Logical Volume ${_LVDEV} like this?\nVolume Group: ${_LV}\nVolume Size: ${_LV_SIZE}\n Contiguous Volume: ${_CONTIGUOUS}" 0 0 && _LVFINISH="DONE"
     done
     _umountall
     if [[ -n "${_LV_ALL}" ]]; then
