41eb863dc1c6af585dbed377656eb13caac24643
Tobias Powalowski <tpowa@archlinux.org>
2023-01-15 20:21:58 +0100

    fix check

--- usr/lib/archboot/installer/blockdevices.sh
+++ usr/lib/archboot/installer/blockdevices.sh
@@ -799,8 +799,10 @@ _createlv()
             _LV_ALL=""
             _dialog --inputbox "Enter the size (MB) of your Logical Volume,\nMinimum value is > 0.\n\nVolume space left: $(vgs -o vg_free --noheading --units m "${_LV}")B\n\nIf you enter no value, all free space left will be used." 10 65 "" 2>"${_ANSWER}" || return 1
                 _LV_SIZE=$(cat "${_ANSWER}")
-                [[ -z "${_LV_SIZE}" ]] && _LV_ALL=1
-                if [[ "${_LV_SIZE}" == 0 ]]; then
+                if [[ -z "${_LV_SIZE}" ]]; then
+                    _LV_ALL=1
+                    _LV_SIZE_SET=1
+                elif [[ "${_LV_SIZE}" == 0 ]]; then
                     _dialog --msgbox "ERROR: You have entered a invalid size, please enter again." 0 0
                 else
                     if [[ "${_LV_SIZE}" -ge "$(vgs -o vg_free --noheading --units m | sed -e 's#m##g')" ]]; then
