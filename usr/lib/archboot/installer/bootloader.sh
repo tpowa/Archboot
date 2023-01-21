a9db4e8cdfd5a3e7f323543bf053f249323d925d
Tobias Powalowski <tpowa@archlinux.org>
2023-01-21 14:54:34 +0100

    reorder menu

--- usr/lib/archboot/installer/bootloader.sh
+++ usr/lib/archboot/installer/bootloader.sh
@@ -404,12 +404,12 @@ _do_efistub_uefi() {
     else
         _dialog --menu "Select which UEFI Boot Manager to install, to provide a menu for the EFISTUB kernels?" 11 55 3 \
             "SYSTEMD-BOOT" "SYSTEMD-BOOT for ${_UEFI_ARCH} UEFI" \
-            "rEFInd" "rEFInd for ${_UEFI_ARCH} UEFI" \
-            "UKI" "Unified Kernel Image for ${_UEFI_ARCH} UEFI" 2>"${_ANSWER}"
+            "FIRMWARE" "Unified Kernel Image for ${_UEFI_ARCH} UEFI" \
+            "rEFInd" "rEFInd for ${_UEFI_ARCH} UEFI" 2>"${_ANSWER}"
         case $(cat "${_ANSWER}") in
             "SYSTEMD-BOOT") _do_systemd_boot_uefi ;;
+            "FIRMWARE") _do_uki_uefi;;
             "rEFInd") _do_refind_uefi ;;
-            "UKI") _do_uki_uefi;;
         esac
     fi
 }
@@ -522,6 +522,7 @@ _do_uki_uefi() {
         cp -f "${_DESTDIR}/${_UEFISYS_MP}/EFI/Linux/archlinux-linux.efi" "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/BOOT${_UEFI_ARCH}.EFI"
         _dialog --infobox "Unified Kernel Image has been setup successfully.\nContinuing in 5 seconds..." 4 70
         sleep 5
+        _S_BOOTLOADER=1
     else
         _dialog --msgbox "Error setting up Unified Kernel Image." 3 40
     fi
