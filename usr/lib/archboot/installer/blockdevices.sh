870dcf4397f77c40b6e558d4a74f313b2c8cad63
Tobias Powalowski <tpowa@archlinux.org>
2023-01-25 09:01:55 +0100

    remove device from PV

--- usr/lib/archboot/installer/blockdevices.sh
+++ usr/lib/archboot/installer/blockdevices.sh
@@ -630,7 +630,7 @@ _createpv()
         while [[ "${_DEV}" != "DONE" ]]; do
             _DEVNUMBER="$((_DEVNUMBER + 1))"
             # clean loop from used partition and options
-            _DEVS="${_DEVS//${_DEV}\ _/}"
+            _DEVS="$(echo "${_DEVS}" | sed -e "s#$(${_LSBLK} NAME,SIZE -d "${_DEV}")##g")"
             # add more devices
             #shellcheck disable=SC2086
             _dialog --menu "Select additional device number ${_DEVNUMBER} for physical volume:" 15 60 12 ${_DEVS} DONE _ 2>"${_ANSWER}" || return 1
