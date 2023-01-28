b90f8705b0db73476948c35475ea3aaf6e9480e0
Tobias Powalowski <tpowa@archlinux.org>
2023-01-28 22:32:52 +0100

    launch login.sh from /etc/profile to get everything sourced correct

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -67,12 +67,12 @@ _run_update_installer() {
     echo ""
     if [[ "${TTY}" == "tty1" ]]; then
         echo -e "\033[1m\033[91m10 seconds\033[0;25m time to hit \033[1m\033[92mCTRL-C\033[0m to \033[1m\033[91mstop\033[0m the process \033[1m\033[1mnow...\033[0m"
-        sleep 10 || _enter_shell
+        sleep 10
         echo ""
         if [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -lt 2571000 ]]; then
-            _run_latest || _enter_shell
+            _run_latest
         else
-            _run_latest_install || _enter_shell
+            _run_latest_install
         fi
     elif [[ "${TTY}" == "ttyS0" || "${TTY}" == "ttyAMA0" || "${TTY}" == "ttyUSB0" || "${TTY}" == "pts/0" ]]; then
         if [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2571000 ]]; then
