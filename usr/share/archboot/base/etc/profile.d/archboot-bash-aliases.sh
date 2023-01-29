1499de86cdfa324aca4935e1a6c283c7fe36b7c2
Tobias Powalowski <tpowa@archlinux.org>
2023-01-29 23:39:37 +0100

    add aliases for vi,vim and edit to nvim, fix color-bash-prompt name

--- usr/share/archboot/base/etc/profile.d/archboot-bash-aliases.sh
+++ usr/share/archboot/base/etc/profile.d/archboot-bash-aliases.sh
@@ -1,4 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # created by Tobias Powalowski <tpowa@archlinux.org>
+# color output
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+# set neovim default editor
+alias vi='nvim'
+alias vim='nvim'
+alias edit='nvim'
