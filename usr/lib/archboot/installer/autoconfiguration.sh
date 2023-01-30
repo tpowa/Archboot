a54270f55823f6c7e2bd750d5d39ecd187cfa77d
Tobias Powalowski <tpowa@archlinux.org>
2023-01-30 08:11:47 +0100

    rename archboot-bash to custom-bash

--- usr/lib/archboot/installer/autoconfiguration.sh
+++ usr/lib/archboot/installer/autoconfiguration.sh
@@ -234,10 +234,10 @@ _auto_bash(){
         _dialog --infobox "Enable custom bash prompt on installed system..." 3 70
         ! grep -qw 'custom-bash-prompt.sh' "${_DESTDIR}/etc/bash.bashrc" &&\
             echo ". /etc/profile.d/custom-bash-prompt.sh" >> "${_DESTDIR}/etc/bash.bashrc"
-        cp /etc/profile.d/archboot-bash-prompt.sh "${_DESTDIR}"/etc/profile.d/
+        cp /etc/profile.d/custom-bash-prompt.sh "${_DESTDIR}"/etc/profile.d/
         sleep 2
     fi
-    if [[ ! -f ${_DESTDIR}/etc/profile.d/archboot-bash-aliases.sh ]]; then
+    if [[ ! -f ${_DESTDIR}/etc/profile.d/custom-bash-aliases.sh ]]; then
         _dialog --infobox "Enable custom bash aliases on installed system..." 3 70
         cp /etc/profile.d/custom-bash-aliases.sh "${_DESTDIR}"/etc/profile.d/
         sleep 2
