d704a63865688c8f672fc5d67de33a94761de4eb
Tobias Powalowski <tpowa@archlinux.org>
2023-01-31 17:49:14 +0100

    add comments

--- usr/share/archboot/base/etc/profile.d/custom-bash-prompt.sh
+++ usr/share/archboot/base/etc/profile.d/custom-bash-prompt.sh
@@ -3,8 +3,10 @@
 # check on bash
 [[ -n "${BASH_VERSION:-}" ]] || return
 if  [[ "${UID}" == 0 ]]; then
+    # red for root user
     PS1='[\e[1;31m\u\e[m@\e[1;32m\h\e[m \W]\$ '
 else
+    # blue for normal user
     PS1='[\e[1;34m\u\e[m@\e[1;32m\h\e[m \W]\$ '
 fi
 
