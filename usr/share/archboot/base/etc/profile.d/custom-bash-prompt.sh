08959dfe7e7daa5e6154f446752a8b7a923d4bb9
Tobias Powalowski <tpowa@archlinux.org>
2023-01-31 17:24:12 +0100

    check on bash

--- usr/share/archboot/base/etc/profile.d/custom-bash-prompt.sh
+++ usr/share/archboot/base/etc/profile.d/custom-bash-prompt.sh
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # created by Tobias Powalowski <tpowa@archlinux.org>
-if  [[ "${UID}" == 0 ]]; then
-    PS1='[\e[1;31m\u\e[m@\e[1;32m\h\e[m \W]\$ '
-else
-    PS1='[\e[1;34m\u\e[m@\e[1;32m\h\e[m \W]\$ '
+if [[ "${0}" == "bash" ]]; then
+    if  [[ "${UID}" == 0 ]]; then
+        PS1='[\e[1;31m\u\e[m@\e[1;32m\h\e[m \W]\$ '
+    else
+        PS1='[\e[1;34m\u\e[m@\e[1;32m\h\e[m \W]\$ '
+    fi
 fi
