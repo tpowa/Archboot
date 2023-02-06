513ab154cdb83dc485ae4b72a4c9041c4cb5bca1
Tobias Powalowski <tpowa@archlinux.org>
2023-02-06 17:20:35 +0100

    remove jansson from environment

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -85,7 +85,7 @@ fi
 if ! [[ -e "/.clean-pacman-db" ]]; then
     touch /.clean-pacman-db
     _RM_PACMAN_DB="grub libxml2 icu gettext refind amd-ucode intel-ucode edk2-shell cdrtools \
-        libisoburn libburn libisofs mkinitcpio memtest linux-api-headers"
+        libisoburn libburn libisofs mkinitcpio memtest linux-api-headers jansson"
     for i in ${_RM_PACMAN_DB}; do
         rm -rf /var/lib/pacman/local/${i}* &>/dev/null
     done
