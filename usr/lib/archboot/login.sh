812eeba31cc6b972c94a1b46348324ec5be97541
Tobias Powalowski <tpowa@archlinux.org>
2023-02-06 19:36:06 +0100

    remove libwbclient

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -85,7 +85,8 @@ fi
 if ! [[ -e "/.clean-pacman-db" ]]; then
     touch /.clean-pacman-db
     _RM_PACMAN_DB="grub libxml2 icu gettext refind amd-ucode intel-ucode edk2-shell cdrtools \
-        libisoburn libburn libisofs mkinitcpio memtest linux-api-headers jansson libwbclient"
+        libisoburn libburn libisofs mkinitcpio memtest linux-api-headers jansson libwbclient \
+        libbsd libmd"
     for i in ${_RM_PACMAN_DB}; do
         rm -rf /var/lib/pacman/local/${i}* &>/dev/null
     done
