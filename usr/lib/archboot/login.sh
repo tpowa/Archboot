7bf63f17c7050c0028659755e47cd601fe83b2a7
Tobias Powalowski <tpowa@archlinux.org>
2023-02-06 14:05:19 +0100

    remove nss as depend

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -84,7 +84,8 @@ if ! [[ -e "/.vconsole-run" ]]; then
 fi
 if ! [[ -e "/.clean-pacman-db" ]]; then
     touch /.clean-pacman-db
-    _RM_PACMAN_DB="grub libxml2 icu gettext refind"
+    _RM_PACMAN_DB="grub libxml2 icu gettext refind amd-ucode intel-ucode edk2-shell cdrtools \
+        libisoburn mkinitcpio memtest linux-api-headers audit"
     for i in ${_RM_PACMAN_DB}; do
         rm -rf /var/lib/pacman/local/${i}* &>/dev/null
     done
