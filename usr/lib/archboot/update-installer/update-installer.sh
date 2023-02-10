27f72c5b5daf85ccbb95af4f778bb5251e349f40
Tobias Powalowski <tpowa@archlinux.org>
2023-02-10 20:30:54 +0100

    better calclulation

--- usr/lib/archboot/update-installer/update-installer.sh
+++ usr/lib/archboot/update-installer/update-installer.sh
@@ -322,7 +322,7 @@ _kexec() {
     # wait until enough memory is available!
     while true; do
         if [[ "$(($(stat -c %s /ramfs/initrd.img)*339/100000))" -lt "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" ]]; then
-            [[ "$(($(stat -c %s /ramfs/initrd.img)*339/100000))" -lt "$(grep -w MemAvailable /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" ]] && break
+            [[ "$(($(stat -c %s /ramfs/initrd.img)*200/100000))" -lt "$(grep -w MemAvailable /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" ]] && break
         else
             [[ "$(($(stat -c %s /ramfs/initrd.img)/1000))" -lt "$(grep -w MemAvailable /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" ]] && break
         fi
