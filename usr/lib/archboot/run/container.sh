00095f0e061f12029e44aef22c1ecc52611498b0
Tobias Powalowski <tpowa@archlinux.org>
2023-02-24 21:31:28 +0100

    fix locales

--- usr/lib/archboot/run/container.sh
+++ usr/lib/archboot/run/container.sh
@@ -18,9 +18,9 @@ if echo "${_BASENAME}" | grep -qw "${_RUNNING_ARCH}"; then
     _clean_mkinitcpio "${1}"
     _clean_cache "${1}"
     _install_archboot "${1}" || exit 1
+    _umount_special "${1}" || exit 1
     _enable_common_locales "${1}" || exit 1
     _clean_cache "${1}"
-    _umount_special "${1}" || exit 1
     _clean_container "${1}"
     _generate_keyring "${1}" || exit 1
     _copy_mirrorlist_and_pacman_conf "${1}"
