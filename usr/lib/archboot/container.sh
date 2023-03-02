29661bf2292ff10394d4d814d28d4eb39ae23d8e
Tobias Powalowski <tpowa@archlinux.org>
2023-03-02 11:10:58 +0100

    don't touch file permissions in initramfs creation

--- usr/lib/archboot/container.sh
+++ usr/lib/archboot/container.sh
@@ -197,6 +197,7 @@ _set_hostname() {
 
 # patch mkinitcpio to preserve permissions
 _fix_initramfs_permissions() {
+    echo "Fixing preserving permissions in mkinitcpio..."
     sed -i -e '#--uid 0 --gid 0 ##g' "${1}"/usr/bin/mkinitcpio
 }
 # vim: set ft=sh ts=4 sw=4 et:
