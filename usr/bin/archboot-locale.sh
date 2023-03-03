6b1bd00f5786a21b78594d2909f732e49a988748
Tobias Powalowski <tpowa@archlinux.org>
2023-03-03 07:15:47 +0100

    delete .locale

--- usr/bin/archboot-locale.sh
+++ usr/bin/archboot-locale.sh
@@ -17,6 +17,7 @@ _abort()
 {
     _dialog --yesno "Abort Arch Linux System Wide Locale Setting?" 5 60 || return 0
     [[ -e /tmp/.locale-running ]] && rm /tmp/.locale-running
+    [[ -e /tmp/.locale ]] && rm /tmp/.locale
     clear
     exit 1
 }
