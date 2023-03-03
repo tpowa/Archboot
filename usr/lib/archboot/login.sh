a170f68ca8756c7dfbc7ef400091e6016f8fb3f6
Tobias Powalowski <tpowa@archlinux.org>
2023-03-03 08:21:30 +0100

    avoid double listing of MOTD

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -156,10 +156,12 @@ if [[ -e /usr/bin/setup ]]; then
     if ! [[ -e /tmp/.locale ]]; then
         echo -e "Hit \e[1m\e[92mENTER\e[m for \e[1mfirst\e[m login setup."
         read -r
-        ! [[ -e /tmp/.locale ]] && archboot-locale.sh
+        if ! [[ -e /tmp/.locale ]]; then
+            archboot-locale.sh
+            cat /etc/motd
+            source /etc/locale.conf
+        fi
     fi
-    cat /etc/motd
-    source /etc/locale.conf
     _local_mode
     _enter_shell
     if ! [[ -e /tmp/.setup ]]; then
