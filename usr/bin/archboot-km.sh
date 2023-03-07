a3f648372dded44ea5d0485fa1b5cde56d72f659
Tobias Powalowski <tpowa@archlinux.org>
2023-03-07 17:40:00 +0100

    fix box size

--- usr/bin/archboot-km.sh
+++ usr/bin/archboot-km.sh
@@ -45,7 +45,7 @@ _set_vconsole() {
     fi
     _CANCEL=
     #shellcheck disable=SC2086
-    _dialog --title "Select Console Font" --menu "\n     Font Name          Region" 13 40 15 ${_FONTS} 2>${_ANSWER} || _CANCEL=1
+    _dialog --title "Select Console Font" --menu "\n     Font Name          Region" 11 40 13 ${_FONTS} 2>${_ANSWER} || _CANCEL=1
     _abort_dialog || return 1
     #shellcheck disable=SC2086
     _FONT=$(cat ${_ANSWER})
