78c90cdb778848732fe0e225325e7f8a7b192725
Tobias Powalowski <tpowa@archlinux.org>
2023-03-08 22:41:13 +0100

    simplify km

--- usr/bin/archboot-km.sh
+++ usr/bin/archboot-km.sh
@@ -14,22 +14,6 @@ _dialog() {
     return $?
 }
 
-_abort()
-{
-    _dialog --yesno "Abort Console Font And Keymap Setting?" 6 42 || return 0
-    [[ -e /tmp/.km ]] && rm -f /tmp/.km
-    [[ -e /tmp/.km-running ]] && rm /tmp/.km-running
-    clear
-    exit 1
-}
-
-_abort_dialog() {
-    if [[ "${_CANCEL}" = "1" ]]; then
-        _S_NEXTITEM="1"
-        return 1
-    fi
-}
-
 _do_vconsole() {
     _dialog --infobox "Setting console font ${_FONT} and keymap ${_KEYMAP}..." 3 80
     echo KEYMAP="${_KEYMAP}" > /etc/vconsole.conf
@@ -44,63 +28,30 @@ _set_vconsole() {
     else
         _FONTS="ter-v16n Worldwide latarcyrheb-sun16 Worldwide eurlatgr Europe"
     fi
-    _CANCEL=
     #shellcheck disable=SC2086
-    _dialog --menu "        Select Console Font:\n\n     Font Name          Region" 12 40 14 ${_FONTS} 2>${_ANSWER} || _CANCEL=1
-    _abort_dialog || return 1
+    _dialog --title "${_TITLE}" --menu "        Select Console Font:\n\n     Font Name          Region" 12 40 14 ${_FONTS} 2>${_ANSWER} || return 1
     #shellcheck disable=SC2086
     _FONT=$(cat ${_ANSWER})
     # get list of 2 sign locale
     #  ${KEYMAP} | grep -v '...' | grep "^[a-z]"
     _KEYMAPS="us English de German es Spanish fr French pt Portuguese OTHER More"
     _OTHER_KEYMAPS="be Belarusian bg Bulgarian br Brazil ca Canada cz Czech dk Dansk et Estonian fi Finnish gr Greek hu Hungarian it Italian lt Lithuanian lv Latvian mk Macedonian nl Dutch no Norwegian pl Polish ro Romanian ru Russian sk Slovak sr Serbian sv Swedish uk Ukrainian"
-    _CANCEL=""
     #shellcheck disable=SC2086
-    _dialog --menu "Select A Keymap Region:" 13 40 7 ${_KEYMAPS} 2>${_ANSWER} || _CANCEL="1"
+    _dialog --title "${_TITLE}" --menu "Select A Keymap Region:" 13 40 7 ${_KEYMAPS} 2>${_ANSWER} || return 1
     _KEYMAP=$(cat ${_ANSWER})
     if [[ "${_KEYMAP}" == "OTHER" ]]; then
         #shellcheck disable=SC2086
-        _dialog --menu "Select A Keymap Region:" 18 40 12 ${_OTHER_KEYMAPS} 2>${_ANSWER} || _CANCEL="1"
+        _dialog --title "${_TITLE}" --menu "Select A Keymap Region:" 18 40 12 ${_OTHER_KEYMAPS} 2>${_ANSWER} || return 1
         _KEYMAP=$(cat ${_ANSWER})
     fi
-    _abort_dialog || return 1
     _KEYMAPS=""
     for i in $(${_LIST_MAPS} | grep "^${_KEYMAP}" | grep -v '^carpalx' | grep -v 'defkey' | grep -v 'mac' | grep -v 'amiga' | grep -v 'sun' | grep -v 'atari'); do
         _KEYMAPS="${_KEYMAPS} ${i} -"
     done
-    _CANCEL=""
     #shellcheck disable=SC2086
-    _dialog --menu "Select A Keymap Layout:" 14 40 8 ${_KEYMAPS} 2>${_ANSWER} || _CANCEL="1"
-    _abort_dialog || return 1
+    _dialog --title "${_TITLE}" --menu "Select A Keymap Layout:" 14 40 8 ${_KEYMAPS} 2>${_ANSWER} || return 1
     #shellcheck disable=SC2086
     _KEYMAP=$(cat ${_ANSWER})
-    _S_NEXTITEM=2
-}
-
-_mainmenu() {
-    if [[ -n "${_S_NEXTITEM}" ]]; then
-        _DEFAULT="--default-item ${_S_NEXTITEM}"
-    else
-        _DEFAULT=""
-    fi
-    #shellcheck disable=SC2086
-    _dialog ${_DEFAULT} --backtitle "${_TITLE}" --title " MAIN MENU " \
-                --menu "Use the UP and DOWN arrows to navigate menus.\nUse TAB to switch between buttons and ENTER to select." 10 58 12 \
-        "1" "Set Console Font And Keymap" \
-        "2" "${EXIT}" 2>${_ANSWER}
-    #shellcheck disable=SC2086
-    case $(cat ${_ANSWER}) in
-        "1")
-            _set_vconsole || return 1
-            _do_vconsole
-            ;;
-        "2")
-            [[ -e /tmp/.km-running ]] && rm /tmp/.km-running
-            clear
-            exit 0 ;;
-        *)
-            _abort ;;
-    esac
 }
 
 if [[ -e /tmp/.km-running ]]; then
@@ -109,22 +60,14 @@ if [[ -e /tmp/.km-running ]]; then
     exit 1
 fi 
 : >/tmp/.km-running
-if [[ "${1}" = "--setup" ]]; then
-    if ! _set_vconsole; then
-        [[ -e /tmp/.km-running ]] && rm /tmp/.km-running
-        clear
-        exit 1
-    fi
+if ! _set_vconsole; then
+    [[ -e /tmp/.km ]] && rm /tmp/.km
     [[ -e /tmp/.km-running ]] && rm /tmp/.km-running
     clear
-    _do_vconsole
-    exit 0
-else
-    EXIT="Exit"
+    exit 1
 fi
-while true; do
-    _mainmenu
-done
+[[ -e /tmp/.km-running ]] && rm /tmp/.km-running
 clear
+_do_vconsole
 exit 0
 # vim: set ts=4 sw=4 et:
