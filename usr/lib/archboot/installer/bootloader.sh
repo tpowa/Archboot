74cafe0794de94733e3e6076a5e4f51483c53249
Tobias Powalowski <tpowa@archlinux.org>
2023-03-19 14:08:05 +0100

    fix directory

--- usr/lib/archboot/installer/bootloader.sh
+++ usr/lib/archboot/installer/bootloader.sh
@@ -519,6 +519,7 @@ Type=oneshot
 ExecStart="/usr/bin/bash -c source /etc/ukify.conf"
 CONFEOF
     ${_NSPAWN} systemctl enable ukify.path &>"${_NO_LOG}"
+    mkdir -p "${_DESTDIR}/${_UEFISYS_MP}/EFI/Linux"
     ${_NSPAWN} /usr/bin/bash -c source /etc/ukify.conf >${_LOG}
     sleep 5
     if [[ -e "${_DESTDIR}/${_UEFISYS_MP}/EFI/Linux/archlinux-linux.efi" ]]; then
