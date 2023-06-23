1e5e34949b7ea4e70a97f9e662ff69a2613694b5
Tobias Powalowski <tpowa@archlinux.org>
2023-06-23 14:54:33 +0200

    fix detect_disk

--- usr/lib/archboot/installer/partition.sh
+++ usr/lib/archboot/installer/partition.sh
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # created by Tobias Powalowski <tpowa@archlinux.org>
 _detect_disk() {
-    if [[ -z "${_DISK}" ]] || ! echo "${_DISK}" | grep -q '/dev/' || findmnt -vno SOURCE "${_DESTDIR}/boot"; then
-        if findmnt -vno SOURCE "${_DESTDIR}/boot" | grep -qw systemd-1; then
-            _DISK="$(${_LSBLK} PKNAME "$(findmnt -vno SOURCE "${_DESTDIR}/boot" | grep -vw systemd-1)")"
-        else
-            _DISK="$(${_LSBLK} PKNAME "$(findmnt -vno SOURCE "${_DESTDIR}/boot")")"
-        fi
-    fi
     if [[ -z "${_DISK}" ]]; then
+        if findmnt -vno SOURCE "${_DESTDIR}/boot"; then
+            if findmnt -vno SOURCE "${_DESTDIR}/boot" | grep -qw systemd-1; then
+                _DISK="$(${_LSBLK} PKNAME "$(findmnt -vno SOURCE "${_DESTDIR}/boot" | grep -vw systemd-1)")"
+            else
+                _DISK="$(${_LSBLK} PKNAME "$(findmnt -vno SOURCE "${_DESTDIR}/boot")")"
+            fi
+        fi
         _DISK="$(${_LSBLK} PKNAME "$(findmnt -vno SOURCE "${_DESTDIR}/")")"
     fi
 }
