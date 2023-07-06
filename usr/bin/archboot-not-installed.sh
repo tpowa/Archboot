1fb2e27f47348142699e50d82053be60d4dfab01
Tobias Powalowski <tpowa@archlinux.org>
2023-07-06 07:31:26 +0200

    fix uninstalled.txt

--- usr/bin/archboot-not-installed.sh
+++ usr/bin/archboot-not-installed.sh
@@ -12,12 +12,12 @@ for i in $(cat packages.txt); do
     rm -r /var/lib/pacman/local/${i}*
 	#shellcheck disable=SC2086
     if pacman -S ${i} --noconfirm &>>log.txt; then
-        echo "${i}" >> uninstalled.txt
+        echo "${i}" >> uninstalled.orig.txt
     else
 		#shellcheck disable=SC2086
         pacman -S ${i} --noconfirm --overwrite '*'
     fi
 done
 # remove false positives
-grep -v "linux-firmware-marvell pambase pacman-mirrorlist licenses" uninstalled.txt >uninstalled.txt
+grep -v "linux-firmware-marvell pambase pacman-mirrorlist licenses" uninstalled.orig.txt >uninstalled.txt
 # vim: set ft=sh ts=4 sw=4 et:
