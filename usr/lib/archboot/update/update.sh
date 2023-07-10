ee5877fd69e9d7b8e4f17c6cfcc876746f76cdd0
Tobias Powalowski <tpowa@archlinux.org>
2023-07-10 13:57:01 +0200

    start sway support

--- usr/lib/archboot/update/update.sh
+++ usr/lib/archboot/update/update.sh
@@ -674,7 +674,7 @@ _configure_sway() {
         echo "include=/usr/share/foot/themes/tango" >>/etc/xdg/foot/foot.ini
     echo "Autostarting setup..."
     grep -q 'exec foot' /etc/sway/config ||\
-        echo "exec foot" >> /etc/sway/config
+        echo "exec foot -- /usr/bin/setup" >> /etc/sway/config
     #_HIDE_MENU="avahi-discover bssh bvnc org.gnome.Extensions org.gnome.FileRoller org.gnome.gThumb org.gnome.gedit fluid vncviewer qvidcap qv4l2"
     #echo "Hiding ${_HIDE_MENU} menu entries..."
     #for i in ${_HIDE_MENU}; do
