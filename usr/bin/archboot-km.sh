9e12ae30197870a97480c6dd2f02d494265debb5
Tobias Powalowski <tpowa@archlinux.org>
2023-07-13 11:24:32 +0200

    add abort dialog to km script

--- usr/bin/archboot-km.sh
+++ usr/bin/archboot-km.sh
@@ -15,6 +15,18 @@ _dialog() {
     return $?
 }
 
+_abort()
+{
+    if _dialog --yesno "Abort Arch Linux Console Configuration?" 5 60; then
+        [[ -e /tmp/.km-running ]] && rm /tmp/.km-running
+        [[ -e /tmp/.km ]] && rm /tmp/.km
+        clear
+        exit 1
+    else
+        _CONTINUE=""
+    fi
+}
+
 _do_vconsole() {
     _dialog --infobox "Setting console font ${_FONT} and keymap ${_KEYMAP}..." 3 80
     echo KEYMAP="${_KEYMAP}" > /etc/vconsole.conf
@@ -32,30 +44,51 @@ _set_vconsole() {
     else
         _FONTS="ter-v16n Worldwide latarcyrheb-sun16 Worldwide eurlatgr Europe"
     fi
-    #shellcheck disable=SC2086
-    _dialog --menu "        Select Console Font:\n\n     Font Name          Region" 12 40 14 ${_FONTS} 2>${_ANSWER} || return 1
-    #shellcheck disable=SC2086
-    _FONT=$(cat ${_ANSWER})
-    # get list of 2 sign locale
-    #  ${KEYMAP} | grep -v '...' | grep "^[a-z]"
-    _KEYMAPS="us English de German es Spanish fr French pt Portuguese OTHER More"
-    _OTHER_KEYMAPS="be Belarusian bg Bulgarian br Brazil ca Canada cz Czech dk Dansk et Estonian fi Finnish gr Greek hu Hungarian it Italian lt Lithuanian lv Latvian mk Macedonian nl Dutch no Norwegian pl Polish ro Romanian ru Russian sk Slovak sr Serbian sv Swedish uk Ukrainian"
-    #shellcheck disable=SC2086
-    _dialog --menu "Select A Keymap Region:" 13 40 7 ${_KEYMAPS} 2>${_ANSWER} || return 1
-    _KEYMAP=$(cat ${_ANSWER})
-    if [[ "${_KEYMAP}" == "OTHER" ]]; then
+    _CONTINUE=""
+    while [[ -z "${_CONTINUE}" ]]; do
         #shellcheck disable=SC2086
-        _dialog --menu "Select A Keymap Region:" 18 40 12 ${_OTHER_KEYMAPS} 2>${_ANSWER} || return 1
-        _KEYMAP=$(cat ${_ANSWER})
-    fi
-    _KEYMAPS=""
-    for i in $(${_LIST_MAPS} | grep "^${_KEYMAP}" | grep -v '^carpalx' | grep -v 'defkey' | grep -v 'mac' | grep -v 'amiga' | grep -v 'sun' | grep -v 'atari'); do
-        _KEYMAPS="${_KEYMAPS} ${i} -"
+        if _dialog --menu "        Select Console Font:\n\n     Font Name          Region" 12 40 14 ${_FONTS} 2>${_ANSWER}; then
+            #shellcheck disable=SC2086
+            _FONT=$(cat ${_ANSWER})
+            _CONTINUE=1
+        else
+            _abort
+        fi
     done
-    #shellcheck disable=SC2086
-    _dialog --menu "Select A Keymap Layout:" 14 40 8 ${_KEYMAPS} 2>${_ANSWER} || return 1
-    #shellcheck disable=SC2086
-    _KEYMAP=$(cat ${_ANSWER})
+    _CONTINUE=""
+    while [[ -z "${_CONTINUE}" ]]; do
+        # get list of 2 sign locale
+        #  ${KEYMAP} | grep -v '...' | grep "^[a-z]"
+        _KEYMAPS="us English de German es Spanish fr French pt Portuguese OTHER More"
+        _OTHER_KEYMAPS="be Belarusian bg Bulgarian br Brazil ca Canada cz Czech dk Dansk et Estonian fi Finnish gr Greek hu Hungarian it Italian lt Lithuanian lv Latvian mk Macedonian nl Dutch no Norwegian pl Polish ro Romanian ru Russian sk Slovak sr Serbian sv Swedish uk Ukrainian"
+        #shellcheck disable=SC2086
+        if _dialog --menu "Select A Keymap Region:" 13 40 7 ${_KEYMAPS} 2>${_ANSWER}; then
+            _KEYMAP=$(cat ${_ANSWER})
+            if [[ "${_KEYMAP}" == "OTHER" ]]; then
+                #shellcheck disable=SC2086
+                _dialog --menu "Select A Keymap Region:" 18 40 12 ${_OTHER_KEYMAPS} 2>${_ANSWER} || return 1
+                _KEYMAP=$(cat ${_ANSWER})
+            fi
+            _CONTINUE=1
+        else
+            _abort
+        fi
+    done
+    _CONTINUE=""
+    while [[ -z "${_CONTINUE}" ]]; do
+        _KEYMAPS=""
+        for i in $(${_LIST_MAPS} | grep "^${_KEYMAP}" | grep -v '^carpalx' | grep -v 'defkey' | grep -v 'mac' | grep -v 'amiga' | grep -v 'sun' | grep -v 'atari'); do
+            _KEYMAPS="${_KEYMAPS} ${i} -"
+        done
+        #shellcheck disable=SC2086
+        if _dialog --menu "Select A Keymap Layout:" 14 40 8 ${_KEYMAPS} 2>${_ANSWER}; then
+            #shellcheck disable=SC2086
+            _KEYMAP=$(cat ${_ANSWER})
+            _CONTINUE=1
+        else
+            _abort
+        fi
+    done
 }
 
 if [[ -e /tmp/.km-running ]]; then
