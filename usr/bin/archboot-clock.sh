c7e9492c1ac71aea6f720b573a72a7670637f00a
Tobias Powalowski <tpowa@archlinux.org>
2023-07-14 15:42:43 +0200

    restore some commits

--- usr/bin/archboot-clock.sh
+++ usr/bin/archboot-clock.sh
@@ -55,25 +55,21 @@ _timezone () {
                 _abort
             fi
         done
-        _CONTINUE=""
-        while [[ -z "${_CONTINUE}" ]]; do
-            _ZONES=""
-            for i in $(timedatectl --no-pager list-timezones | grep -w "${_REGION}" | cut -d '/' -f 2 | sort -u); do
-                _ZONES="${_ZONES} ${i} -"
-            done
-            #shellcheck disable=SC2086
-            if _dialog --title " Timezone Menu " --menu "" 21 30 16 ${_ZONES} 2>${_ANSWER}; then
-                _SET_ZONE="1"
-                _ZONE=$(cat ${_ANSWER})
-                [[ "${_ZONE}" == "${_REGION}" ]] || _ZONE="${_REGION}/${_ZONE}"
-                _dialog --infobox "Setting Timezone to ${_ZONE}..." 3 50
-                timedatectl set-timezone "${_ZONE}"
-                sleep 3
-                _CONTINUE=1
-            else
-                _SET_ZONE=""
-            fi
+        _ZONES=""
+        for i in $(timedatectl --no-pager list-timezones | grep -w "${_REGION}" | cut -d '/' -f 2 | sort -u); do
+            _ZONES="${_ZONES} ${i} -"
         done
+        #shellcheck disable=SC2086
+        if _dialog --title " Timezone Menu " --menu "" 21 30 16 ${_ZONES} 2>${_ANSWER}; then
+            _SET_ZONE="1"
+            _ZONE=$(cat ${_ANSWER})
+            [[ "${_ZONE}" == "${_REGION}" ]] || _ZONE="${_REGION}/${_ZONE}"
+            _dialog --infobox "Setting Timezone to ${_ZONE}..." 3 50
+            timedatectl set-timezone "${_ZONE}"
+            sleep 3
+        else
+            _SET_ZONE=""
+        fi
     done
 }
 
