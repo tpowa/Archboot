d27c4c11c692284ddc7c43c7f6901f8a26c5db66
Tobias Powalowski <tpowa@archlinux.org>
2023-07-15 08:28:04 +0200

    add CANCEL label

--- usr/bin/archboot-localize.sh
+++ usr/bin/archboot-localize.sh
@@ -28,7 +28,7 @@ _localize_menu() {
     _OTHER_LOCALES="be_BY Belarusian bg_BG Bulgarian cs_CZ Czech da_DK Dansk fi_FI Finnish el_GR Greek hu_HU Hungarian it_IT Italian lt_LT Lithuanian lv_LV Latvian mk_MK Macedonian nl_NL Dutch nn_NO Norwegian pl_PL Polish ro_RO Romanian  ru_RU Russian sk_SK Slovak sr_RS Serbian sv_SE Swedish uk_UA Ukrainian"
     _CANCEL=""
     #shellcheck disable=SC2086
-    _dialog --title " Locale Menu " --menu "" 12 35 5 ${_LOCALES} 2>${_ANSWER} || _abort
+    _dialog --cancel-label "Exit" --title " Locale Menu " --menu "" 12 35 5 ${_LOCALES} 2>${_ANSWER} || _abort
     _LOCALE=$(cat ${_ANSWER})
     if [[ "${_LOCALE}" == "OTHER" ]]; then
         #shellcheck disable=SC2086
