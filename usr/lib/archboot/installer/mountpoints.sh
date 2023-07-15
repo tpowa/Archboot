56359c625b70a5e70c5c6a12e7ac2cad83846719
Tobias Powalowski <tpowa@archlinux.org>
2023-07-15 15:57:37 +0200

    allow btrfs subvolumes

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -276,8 +276,8 @@ _mountpoints() {
                     _btrfs_parts
                     _check_mkfs_values
                     echo "${_DEV}:${_FSTYPE}:${_MP}:${_DOMKFS}:${_LABEL_NAME}:${_FS_OPTIONS}:${_BTRFS_DEVS}:${_BTRFS_LEVEL}:${_BTRFS_SUBVOLUME}:${_BTRFS_COMPRESS}" >>/tmp/.parts
-                    # always remove swap paetition and root device
-                    [[ ! "${_FSTYPE}" == "btrfs" || -z "${_UEFISYSDEV_DONE}" ]] && _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}"
+                    # always remove swap partition and root device
+                    [[ ! "${_FSTYPE}" == "btrfs" || "${_FSTYPE}" == "swap" ]] && _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}"
                 fi
             fi
         done
