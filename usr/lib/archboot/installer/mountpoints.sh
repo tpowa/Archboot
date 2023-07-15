c8ebba3e7ac37e73d0ec5d5b16c0ca45fc71ba21
Tobias Powalowski <tpowa@archlinux.org>
2023-07-15 18:39:33 +0200

    allow btrfs subvolumes

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -278,10 +278,11 @@ _mountpoints() {
                     _check_mkfs_values
                     echo "${_DEV}:${_FSTYPE}:${_MP}:${_DOMKFS}:${_LABEL_NAME}:${_FS_OPTIONS}:${_BTRFS_DEVS}:${_BTRFS_LEVEL}:${_BTRFS_SUBVOLUME}:${_BTRFS_COMPRESS}" >>/tmp/.parts
                     # btrfs is a special case! not really elegant
+                    # remove root btrfs on ESP selection menu, readd it on top aftwerwards
                     if [[ ! "${_FSTYPE}" == "btrfs" ]]; then
                         _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}"
                         if [[ -n "${_UEFISYSDEV_DONE}" && -n ${_ROOT_BTRFS} ]]; then
-                            _DEVS="${_DEVS} ${_ROOT_BTRFS}"
+                            _DEVS="${_ROOT_BTRFS} ${_DEVS}"
                             _ROOT_BTRFS=""
                         fi
                     else
