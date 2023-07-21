07389be1614f75744293f8fb20b3791522e37a3f
Tobias Powalowski <tpowa@archlinux.org>
2023-07-21 11:09:30 +0200

    add title

--- usr/bin/archboot-pacsetup.sh
+++ usr/bin/archboot-pacsetup.sh
@@ -41,7 +41,7 @@ _select_mirror() {
 _enable_testing() {
     if ! grep -q "^\[.*testing\]" /etc/pacman.conf; then
         _DOTESTING=""
-        _dialog --defaultno --yesno "Do you want to enable [core-testing]\nand [extra-testing] repositories?\n\nOnly enable this if you need latest\navailable packages for testing purposes!" 9 50 && _DOTESTING=1
+        _dialog --title " [core-testing] And [extra-testing] " --defaultno --yesno "Do you want to enable [*-testing] repositories?\n\nOnly enable this if you need latest\navailable packages for testing purposes!" 9 50 && _DOTESTING=1
         if [[ -n "${_DOTESTING}" ]]; then
             sed -i -e '/^#\[core-testing\]/ { n ; s/^#// }' /etc/pacman.conf
             sed -i -e '/^#\[extra-testing\]/ { n ; s/^#// }' /etc/pacman.conf
