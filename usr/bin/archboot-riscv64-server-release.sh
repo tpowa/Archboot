6e4df221dda06de27bd370bbea7a2acbb8f121ed
Tobias Powalowski <tpowa@archlinux.org>
2023-07-28 17:03:58 +0200

    revert last change

--- usr/bin/archboot-riscv64-server-release.sh
+++ usr/bin/archboot-riscv64-server-release.sh
@@ -1,18 +1,1 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: GPL-2.0-only
-. /usr/lib/archboot/common.sh
-. /usr/lib/archboot/server.sh
-_root_check
-_container_check
-if echo "${_BASENAME}" | grep -qw riscv64 || echo "${_BASENAME}" | grep -qw aarch64; then
-    _update_pacman_chroot || exit 1
-fi
-_update_source
-if echo "${_BASENAME}" | grep -qw x86_64; then
-    _x86_64_pacman_use_default || exit 1
-fi
-_server_release || exit 1
-if echo "${_BASENAME}" | grep -qw x86_64; then
-    _x86_64_pacman_restore || exit 1
-fi
-# vim: set ft=sh ts=4 sw=4 et:
+../lib/archboot/run/server-release.sh
--- usr/bin/archboot-x86_64-release.sh
+++ usr/bin/archboot-x86_64-release.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: GPL-2.0-only
-# created by Tobias Powalowski <tpowa@archlinux.org>
-. /usr/lib/archboot/common.sh
-. /usr/lib/archboot/release.sh
-[[ -z "${1}" ]] && _usage
-_root_check
-_container_check
-echo "Starting release creation in ${1}..."
-_create_iso "$@" || exit 1
-echo "Finished release creation in ${1}."
-
-# vim: set ft=sh ts=4 sw=4 et:
+../lib/archboot/run/release.sh
