05b48f5f97c4687614953d8d0c6cf17c374f4074
Tobias Powalowski <tpowa@archlinux.org>
2023-07-29 08:36:17 +0200

    only show MOTD once on ssh login

--- usr/share/archboot/remote/usr/bin/remote-login.sh
+++ usr/share/archboot/remote/usr/bin/remote-login.sh
@@ -4,7 +4,7 @@
 
 # simulate login from tty on first screen session
 if ! screen -ls &>/dev/null; then
-    cat /etc/motd
+    [[ -z "${SSH_TTY}" ]] && cat /etc/motd
     echo -e "Hit \e[1m\e[92mENTER\e[m for \e[1mshell\e[m login."
     read -r
 fi
