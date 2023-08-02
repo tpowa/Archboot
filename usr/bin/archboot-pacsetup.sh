874e823e8f31a6a6e31a73e5e2447db6d45ba1ed
Tobias Powalowski <tpowa@archlinux.org>
2023-08-02 08:20:20 +0200

    add progressbar download

--- usr/bin/archboot-pacsetup.sh
+++ usr/bin/archboot-pacsetup.sh
@@ -5,7 +5,6 @@
 _TITLE="Archboot ${_RUNNING_ARCH} | Basic Setup | Pacman Configuration"
 
 _download_mirror() {
-    _COUNTRY="$(curl -s "http://ip-api.com/csv/?fields=countryCode")"
     sleep 1
     _progress "50" "${_DOWNLOAD}"
     ${_DLPROG} "https://www.archlinux.org/mirrorlist/?country=${_COUNTRY}&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" -O /tmp/pacman_mirrorlist.txt
@@ -17,6 +16,7 @@ _download_mirror() {
 _select_mirror() {
     # Download updated mirrorlist, if possible (only on x86_64)
     if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
+        _COUNTRY="$(curl -s "http://ip-api.com/csv/?fields=countryCode")"
         _DOWNLOAD="Downloading latest mirrorlist for Region ${_COUNTRY}..."
         _download_mirror | _dialog --title "Logging to ${_LOG}" --no-mouse --gauge "${_DOWNLOAD}" 6 70 0
         if grep -q '#Server = https:' /tmp/pacman_mirrorlist.txt; then
