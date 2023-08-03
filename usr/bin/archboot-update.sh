fe9cd5c9dcb34f0b1fa78d6ca557db7ad522986d
Tobias Powalowski <tpowa@archlinux.org>
2023-08-03 19:44:30 +0200

    add progressbar on script update

--- usr/bin/archboot-update.sh
+++ usr/bin/archboot-update.sh
@@ -43,7 +43,8 @@ if [[ -n "${_D_SCRIPTS}" ]]; then
     _network_check
     touch /.update
     _TITLE="Archboot $(uname -m) | Basic Setup | Download GIT  Archboot Scripts"
-    _download_latest | _dialog --title " Download GIT " --gauge "Downloading latest config..." 6 75 0
+    _download_latest | _dialog --title " Downloading GIT " --gauge "Downloading latest config..." 6 75 0
+    clear
 fi
 # Generate new environment and launch it with kexec
 if [[ -n "${_L_COMPLETE}" || -n "${_L_INSTALL_COMPLETE}" ]] && [[ -z "${_G_RELEASE}" ]]; then
