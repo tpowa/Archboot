f87a97c2f0ed522bb724993da4bde6070bb2271d
Tobias Powalowski <tpowa@archlinux.org>
2023-08-06 15:34:32 +0200

    add progressbar on grub

--- usr/lib/archboot/installer/bootloader.sh
+++ usr/lib/archboot/installer/bootloader.sh
@@ -871,7 +871,6 @@ _do_grub_bios() {
 }
 
 _grub_install_uefi() {
-    _GRUB_PREFIX_DIR="/boot/grub/"
     chroot "${_DESTDIR}" grub-install \
         --directory="/usr/lib/grub/${_GRUB_ARCH}-efi" \
         --target="${_GRUB_ARCH}-efi" \
@@ -888,7 +887,6 @@ _grub_install_uefi() {
 _grub_install_uefi_sb() {
     ### Hint: https://src.fedoraproject.org/rpms/grub2/blob/rawhide/f/grub.macros#_407
     # add -v for verbose
-    _GRUB_PREFIX_DIR="${_UEFISYS_MP}/EFI/BOOT/"
     if [[ "${_RUNNING_ARCH}" == "aarch64" ]]; then
         ${_NSPAWN} grub-mkstandalone -d /usr/lib/grub/"${_GRUB_ARCH}"-efi -O "${_GRUB_ARCH}"-efi --sbat=/usr/share/grub/sbat.csv --modules="all_video boot btrfs cat configfile cryptodisk echo efi_gop efifwsetup efinet ext2 f2fs fat font gcry_rijndael gcry_rsa gcry_serpent gcry_sha256 gcry_twofish gcry_whirlpool gfxmenu gfxterm gzio halt hfsplus http iso9660 loadenv loopback linux lvm lsefi lsefimmap luks luks2 mdraid09 mdraid1x minicmd net normal part_apple part_msdos part_gpt password_pbkdf2 pgp png reboot regexp search search_fs_uuid search_fs_file search_label serial sleep syslinuxcfg test tftp video xfs zstd chain tpm" --fonts="ter-u16n" --locales="en@quot" --themes="" -o "${_GRUB_PREFIX_DIR}/grub${_SPEC_UEFI_ARCH}.efi" "boot/grub/grub.cfg=/${_GRUB_PREFIX_DIR}/${_GRUB_CFG}"
     elif [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
@@ -925,6 +923,7 @@ _setup_grub_uefi() {
         sleep 2
     else
         ## Install GRUB
+        _GRUB_PREFIX_DIR="/boot/grub/"
         _progress "10" "Setting up GRUB(2) UEFI..."
         _chroot_mount
         touch /.archboot
@@ -939,6 +938,7 @@ _setup_grub_uefi() {
 
 _setup_grub_uefi_sb() {
     if [[ -n "${_UEFI_SECURE_BOOT}" ]]; then
+        _GRUB_PREFIX_DIR="${_UEFISYS_MP}/EFI/BOOT/"
         _progress "10" "Setting up GRUB(2) UEFI Secure Boot..."
         # generate GRUB with config embeded
         #remove existing, else weird things are happening
