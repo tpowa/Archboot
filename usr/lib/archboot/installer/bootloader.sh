7cb4fe5b729edc4ac4b2cf02eb0c98d6c565e00f
Tobias Powalowski <tpowa@archlinux.org>
2023-08-11 21:37:23 +0200

    fix config

--- usr/lib/archboot/installer/bootloader.sh
+++ usr/lib/archboot/installer/bootloader.sh
@@ -478,17 +478,13 @@ _do_limine_bios() {
     fi
     _dialog --no-mouse --infobox "Setting up LIMINE BIOS now..." 3 60
     _LIMINE_CONFIG="${_DESTDIR}/boot/limine.cfg"
-    _SUBDIR=""
-    if [[ "${_ROOTDEV}" == "${_BOOTDEV}" ]]; then
-        _SUBDIR=/boot
-    fi
     _KERNEL="${_SUBDIR}/${_VMLINUZ}"
     _INITRD="${_SUBDIR}/${_INITRAMFS}"
     if [[ -n "${_UCODE}" ]]; then
         _INITRD_UCODE="${_SUBDIR}/${_UCODE}"
     fi
     _do_limine_config
-    _PARENT_BOOTDEV="$(${_LSBLK} PKNAME "${_BOOTDEV}")"
+    _PARENT_BOOTDEV="$(${_LSBLK} PKNAME ${_BOOTDEV})"
     _chroot_mount
     cp ${_DESTDIR}/usr/share/limine/limine-bios.sys ${_DESTDIR}/boot/
     chroot limine bios-install "${_PARENT_BOOTDEV}"
