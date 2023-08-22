16a60db84472bbbcb618872968103675158d4e93
Tobias Powalowski <tpowa@archlinux.org>
2023-08-22 19:32:23 +0200

    add ./ to upload paths

--- usr/lib/archboot/server.sh
+++ usr/lib/archboot/server.sh
@@ -53,20 +53,20 @@ _server_upload() {
     # copy files to server
     echo "Uploading files to ${_SERVER}:${_ARCH}..."
     #shellcheck disable=SC2086
-    sudo -u "${_USER}" ssh "${_SERVER}" "[[ -d "${_ARCH}" ]] || mkdir -p ${_ARCH}"
+    sudo -u "${_USER}" ssh "${_SERVER}" "[[ -d "./${_ARCH}" ]] || mkdir -p ./${_ARCH}"
     sudo -u "${_USER}" scp -q -r "${_DIR}" "${_SERVER}":"${_ARCH}" || exit 1
     # move files on server, create symlink and removing ${_PURGE_DATE} old release
     sudo -u "${_USER}" ssh "${_SERVER}" <<EOF
-echo "Removing old ${1}/${_ARCH}/${_DIR} directory..."
-rm -r "${1}"/"${_ARCH}"/"${_DIR}"
+echo "Removing old ./${1}/${_ARCH}/${_DIR} directory..."
+rm -r "./${1}"/"${_ARCH}"/"${_DIR}"
 echo "Removing old purge date reached ${1}/${_ARCH}/$(date -d "$(date +) - ${_PURGE_DATE}" +%Y.%m) directory..."
-rm -r "${1}"/"${_ARCH}"/"$(date -d "$(date +) - ${_PURGE_DATE}" +%Y.%m)" 2>/dev/null
-echo "Moving ${_ARCH}/${_DIR} to ${1}/${_ARCH}..."
-mv "${_ARCH}/${_DIR}" "${1}"/"${_ARCH}"
-echo "Removing ${_ARCH} directory..."
-rm -r "${_ARCH}"
-cd "${1}"/"${_ARCH}"
-echo "Creating new latest symlink in ${1}/${_ARCH}..."
+rm -r "./${1}"/"${_ARCH}"/"$(date -d "$(date +) - ${_PURGE_DATE}" +%Y.%m)" 2>/dev/null
+echo "Moving ./${_ARCH}/${_DIR} to ./${1}/${_ARCH}..."
+mv "./${_ARCH}/${_DIR}" "./${1}"/"${_ARCH}"
+echo "Removing ./${_ARCH} directory..."
+rm -r "./${_ARCH}"
+cd "./${1}"/"${_ARCH}"
+echo "Creating new latest symlink in ./${1}/${_ARCH}..."
 rm latest
 ln -s "${_DIR}" latest
 EOF
