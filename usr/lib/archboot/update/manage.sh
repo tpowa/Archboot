41c3a265b922e1d5683b215f3be82694472395af
Tobias Powalowski <tpowa@archlinux.org>
2023-08-25 09:22:30 +0200

    remove kernel from / on local image

--- usr/lib/archboot/update/manage.sh
+++ usr/lib/archboot/update/manage.sh
@@ -212,8 +212,8 @@ _new_environment() {
         mv "${_W_DIR}"/tmp/* "/${_RAM}/"
         # cleanup mkinitcpio directories and files
         _progress "95" "Cleanup ${_RAM}..."
-        rm -rf /sysroot/{hooks,install,kernel,new_root,sysroot,mkinitcpio.*} &>"${_NO_LOG}"
-        rm -f /sysroot/{VERSION,config,buildconfig,init} &>"${_NO_LOG}"
+        rm -rf /${_RAM}/{hooks,install,kernel,new_root,sysroot,mkinitcpio.*} &>"${_NO_LOG}"
+        rm -f /${_RAM}/{VERSION,config,buildconfig,init,${_VMLINUZ}} &>"${_NO_LOG}"
         _progress "100" "Switching to rootfs ${_RAM}..."
         sleep 2
         # https://www.freedesktop.org/software/systemd/man/bootup.html
