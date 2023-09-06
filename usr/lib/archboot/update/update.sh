12c83cea39ec61cb45f079353eb93d83435a04ec
Tobias Powalowski <tpowa@archlinux.org>
2023-09-06 09:31:37 +0200

    remove double quote

--- usr/lib/archboot/update/update.sh
+++ usr/lib/archboot/update/update.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 # created by Tobias Powalowski <tpowa@archlinux.org>
-_D_SCRIPTS=""
-_L_COMPLETE=""
-_L_INSTALL_COMPLETE=""
-_G_RELEASE=""
+_D_SCRIPTS="
+_L_COMPLETE="
+_L_INSTALL_COMPLETE="
+_G_RELEASE="
 _CONFIG="/etc/archboot/${_RUNNING_ARCH}-update_installer.conf"
 _W_DIR="/archboot"
 _ROOTFS_DIR="${_W_DIR}/tmp"
@@ -35,20 +35,20 @@ usage () {
     echo -e "\e[1mManage \e[36mArchboot\e[m\e[1m - Arch Linux Environment:\e[m"
     echo -e "\e[1m-----------------------------------------\e[m"
     echo -e " \e[1m-help\e[m            This message."
-    if [[ ! -e ""${_LOCAL_DB}"" || -e "/usr/bin/setup" ]]; then
+    if [[ ! -e "${_LOCAL_DB}" || -e "/usr/bin/setup" ]]; then
         echo -e " \e[1m-update\e[m          Update scripts: setup, quickinst, network, clock and helpers."
     fi
     # latest image
-    if [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2000000 && ! -e "/.full_system" && ! -e ""${_LOCAL_DB}"" ]]; then
+    if [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2000000 && ! -e "/.full_system" && ! -e "${_LOCAL_DB}" ]]; then
         echo -e " \e[1m-full-system\e[m     Switch to full Arch Linux system."
     # local image
-    elif [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2571000 && ! -e "/.full_system" && -e ""${_LOCAL_DB}"" && -e "/usr/bin/setup" ]]; then
+    elif [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2571000 && ! -e "/.full_system" && -e "${_LOCAL_DB}" && -e "/usr/bin/setup" ]]; then
         echo -e " \e[1m-full-system\e[m     Switch to full Arch Linux system."
     fi
-    echo -e ""
+    echo -e "
     if [[ -e "/usr/bin/setup" ]]; then
         # works only on latest image
-        if ! [[ -e ""${_LOCAL_DB}"" ]]; then
+        if ! [[ -e "${_LOCAL_DB}" ]]; then
             if [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2400000 ]] ; then
                 _graphic_options
             fi
@@ -57,11 +57,11 @@ usage () {
                 echo -e " \e[1m-xfce\e[m            Launch Xfce desktop with VNC sharing enabled."
                 echo -e " \e[1m-custom-xorg\e[m     Install custom X environment."
                [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 2400000 ]] && echo -e " \e[1m-custom-wayland\e[m  Install custom Wayland environment."
-                echo ""
+                echo "
             fi
         fi
     fi
-    if ! [[ -e ""${_LOCAL_DB}"" ]] || [[ -e ""${_LOCAL_DB}"" && ! -e "/usr/bin/setup" ]]; then
+    if ! [[ -e "${_LOCAL_DB}" ]] || [[ -e "${_LOCAL_DB}" && ! -e "/usr/bin/setup" ]]; then
         if [[ "$(grep -w MemTotal /proc/meminfo | cut -d ':' -f2 | sed -e 's# ##g' -e 's#kB$##g')" -gt 1970000 ]]; then
             if ! [[ "${_RUNNING_ARCH}" == "riscv64" ]]; then
                 echo -e " \e[1m-latest\e[m          Launch latest archboot environment (using kexec)."
