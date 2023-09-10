0aabd769ac4bdba9a8401f12434e6cfebf01c4d4
Tobias Powalowski <tpowa@archlinux.org>
2023-09-10 22:21:03 +0200

    update sync

--- usr/share/archboot/server/sync.sh
+++ usr/share/archboot/server/sync.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 # created by Tobias Powalowski <tpowa@archlinux.org>
-if [[ -e ~/.rsync-running ]]; then
-    echo "ERROR: rsync is already running!"
-    exit 1
-else
-    : > .rsync-running
-    echo "Syncing files to U.S. mirror: archboot.org..."
-    rsync -a -q --delete --delete-delay pkg src iso archboot.org:public_html
-    rm .rsync-running
-fi
+_SYNC_SERVER="archboot.org archboot.net"
+for i in ${_SYNC_SERVER}; do
+    if [[ -e ~/.rsync-running ]]; then
+        echo "ERROR: rsync is already running!"
+        exit 1
+    else
+        : > .rsync-running
+        echo "Syncing files to mirror: ${i}..."
+        rsync -a -q --delete --delete-delay pkg src iso ${i}:public_html/
+        rm .rsync-running
+    fi
+done
 # vim: set ft=sh ts=4 sw=4 et:
