881e2f001fc0e781b68b93f03a1f404c9f2a6b59
Tobias Powalowski <tpowa@archlinux.org>
2023-09-15 09:22:06 +0200

    remove archboot.org

--- usr/share/archboot/server/sync.sh
+++ usr/share/archboot/server/sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 # created by Tobias Powalowski <tpowa@archlinux.org>
-_SYNC_SERVER="archboot.org archboot.net"
+_SYNC_SERVER="archboot.net"
 for i in ${_SYNC_SERVER}; do
     if [[ -e ~/.rsync-running ]]; then
         echo "ERROR: rsync is already running!"
