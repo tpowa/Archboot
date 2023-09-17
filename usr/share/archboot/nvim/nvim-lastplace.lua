ee852651f98660a5b684ea8ef1ad8f2e700b9158
Tobias Powalowski <tpowa@archlinux.org>
2023-09-17 21:51:54 +0200

    fix lastplace nvim plugin

--- usr/share/archboot/nvim/nvim-lastplace.lua
+++ usr/share/archboot/nvim/nvim-lastplace.lua
@@ -129,4 +129,8 @@ function lastplace.lastplace_ft(buffer)
 end
 
 return lastplace
-.setup{}
+.setup {
+    lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+    lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+    lastplace_open_folds = true
+}
