b2c61cca047ffb03dad5225b6ebf3b7ce46a1a39
Tobias Powalowski <tpowa@archlinux.org>
2023-09-22 21:50:44 +0200

    choose layout from locale

--- usr/bin/archboot-localize.sh
+++ usr/bin/archboot-localize.sh
@@ -37,7 +37,7 @@ _vconsole_keymap() {
     if ${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}" | grep "nodeadkeys"; then
         _KEYMAP="$(${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}" | grep "nodeadkeys")"
     elif ${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}" | grep "^${_DETECTED_KEYMAP}-latin1$"; then
-        _KEYMAP="$(${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}" | grep "latin1$")"
+        _KEYMAP="$(${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}" | grep "^${_DETECTED_KEYMAP}-latin1$")"
     elif ${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}$"; then
         _KEYMAP="$(${_LIST_MAPS} | grep "^${_DETECTED_KEYMAP}$")"
     fi
