83567e48913914dd6447fd9bc48e93c4510852ca
Tobias Powalowski <tpowa@archlinux.org>
2023-10-12 20:30:36 +0200

    big cpio rewrite

--- usr/bin/archboot-cpio.sh
+++ usr/bin/archboot-cpio.sh
@@ -2,84 +2,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 # archboot-cpio.sh - modular tool for building initramfs images
-# simplified, stripped down, optimized for size and speed
+# focused and optimized for size and speed
 # by Tobias Powalowski <tpowa@archlinux.org>
 
 shopt -s extglob
 
 . /usr/lib/archboot/common.sh
 . /usr/lib/archboot/cpio/cpio.sh
-# needed files/directories
-_CONFIG=""
-_CPIO=/usr/lib/archboot/cpio/
-# options and runtime data
-_GENERATE_IMAGE=""
-_TARGET_DIR=""
-declare -A _addedmodules _modpaths
-# Sanitize environment further
-# GREP_OPTIONS="--color=always" will break everything
-# CDPATH can affect cd and pushd
-# LIBMOUNT_* options can affect findmnt and other tools
-unset GREP_OPTIONS CDPATH "${!LIBMOUNT_@}"
 
-_usage() {
-    cat <<EOF
-ARCHBOOT CPIO
--------------
-Tool for creating an archboot initramfs image.
-
- -h               Display this message and exit
-
- -c <config>      Use <config> file
- -k <kernel>      Use specified <kernel>
-
- -g <path>        Generate cpio image and write to specified <path>
- -d <dir>         Generate image into <dir>
-
-usage: ${0##*/} <options>
-EOF
-    exit 0
-}
-
-_build_cpio() {
-    case "${_COMP}" in
-        cat)    echo "Creating uncompressed initcpio image: ${_OUT}"
-                unset _COMP_OPTS
-                ;;
-        *)      echo "Creating ${_COMP} compressed initcpio image: ${_OUT}"
-                ;;
-        xz)     _COMP_OPTS=('-T0' '--check=crc32' "${_COMP_OPTS[@]}")
-                ;;
-        lz4)    _COMP_OPTS=('-l' "${_COMP_OPTS[@]}")
-                ;;
-        zstd)   _COMP_OPTS=('-T0' "${_COMP_OPTS[@]}")
-                ;;
-    esac
-    # Reproducibility: set all timestamps to 0
-    find . -mindepth 1 -execdir touch -hcd "@0" "{}" +
-    # If this pipeline changes, |pipeprogs| below needs to be updated as well.
-    find . -mindepth 1 -printf '%P\0' |
-            sort -z |
-            LANG=C bsdtar --null -cnf - -T - |
-            LANG=C bsdtar --null -cf - --format=newc @- |
-            ${_COMP} "${_COMP_OPTS[@]}" > "${_OUT}" || _abort "initcpio image creation failed!"
-}
-
-preload_builtin_modules() {
-    local modname field value
-    # Prime the _addedmodules list with the builtins for this kernel.
-    # kmod>=27 and kernel >=5.2 required!
-    while IFS=.= read -rd '' modname field value; do
-        _addedmodules[${modname//-/_}]=2
-        case "$field" in
-            alias)
-                _addedmodules["${value//-/_}"]=2
-                ;;
-        esac
-    done <"$_d_kmoduledir/modules.builtin.modinfo"
-}
-
-if [[ -z "$1" ]]; then
+if [[ -z "${1}" ]]; then
     _usage
 fi
 _root_check
@@ -87,17 +18,17 @@ _root_check
 while :; do
     case "${1}" in
         -c) shift
-            ${_CONFIG}="${1}"
+            _CONFIG="${1}"
             ;;
         -k) shift
-            KERNEL="${1}"
+            _KERNEL="${1}"
             ;;
         -d) shift
-            ${_TARGET_DIR}="${1}"
+            _TARGET_DIR="${1}"
             ;;
         -g) shift
             [[ -d "${1}" ]] && _abort "Invalid image path -- ${1} is a directory!"
-            if ! ${_GENERATE_IMAGE}="$(readlink -f "$1")" || [[ ! -e "${_GENERATE_IMAGE%/*}" ]]; then
+            if ! _GENERATE_IMAGE="$(readlink -f "${1}")" || [[ ! -e "${_GENERATE_IMAGE%/*}" ]]; then
                 _abort "Unable to write to path!" "${1}"
             fi
             ;;
@@ -110,46 +41,35 @@ while :; do
 done
 
 #shellcheck disable="SC1090"
-! . "${_CONFIG}" 2>"${_NO_LOG}" && _abort "Failed to read configuration '%s'" "${_CONFIG}"
-if [[ -z "${KERNEL}" ]]; then
-    msg "Trying to autodetect ${_RUNNING_ARCH} kernel..."
-    [[ "${_RUNNING_ARCH}" == "x86_64" || "${_RUNNING_ARCH}" == "riscv64" ]] && KERNEL="/usr/lib/modules/*/vmlinuz"
-    [[ "${_RUNNING_ARCH}" == "aarch64" ]] && KERNEL="/boot/Image.gz"
+! . "${_CONFIG}" 2>"${_NO_LOG}" && _abort "Failed to read ${_CONFIG} configuration file"
+if [[ -z "${_KERNEL}" ]]; then
+    echo "Trying to autodetect ${_RUNNING_ARCH} kernel..."
+    [[ "${_RUNNING_ARCH}" == "x86_64" || "${_RUNNING_ARCH}" == "riscv64" ]] && _KERNEL="/usr/lib/modules/*/vmlinuz"
+    [[ "${_RUNNING_ARCH}" == "aarch64" ]] && _KERNEL="/boot/Image.gz"
 fi
 # allow * in config
 #shellcheck disable=SC2116,2086
-KERNEL="$(echo ${KERNEL})"
-msg "Using kernel: ${KERNEL}"
-if [[ ! -f "${KERNEL}" ]]; then
+_KERNEL="$(echo ${_KERNEL})"
+echo "Using kernel: ${_KERNEL}"
+if [[ ! -f "${_KERNEL}" ]]; then
     _abort "kernel image does not exist!"
 fi
-_KERNELVERSION="$(_kver "${KERNEL}")"
-_d_kmoduledir="/lib/modules/${_KERNELVERSION}"
-[[ -d "$_d_kmoduledir" ]] || _abort "'$_d_kmoduledir' is not a valid kernel module directory"
-_d_workdir="$(initialize_buildroot "${_KERNELVERSION}" "${_TARGET_DIR}")" || exit 1
-_ROOTFS="${_TARGET_DIR}:-$_d_workdir/root}"
-_hooks=("${HOOKS[@]}")
-if (( ${#_hooks[*]} == 0 )); then
-    _abort "Invalid config: No hooks found"
+_KERNELVERSION="$(_kver "${_KERNEL}")"
+_MODULE_DIR="/lib/modules/${_KERNELVERSION}"
+[[ -d "${_MODULE_DIR}" ]] || _abort "${_MODULE_DIR} is not a valid kernel module directory!"
+_BUILD_DIR="$(_init_rootfs "${_KERNELVERSION}" "${_TARGET_DIR}")" || exit 1
+_ROOTFS="${_TARGET_DIR}:-${_BUILD_DIR}/root}"
+if (( ${#_HOOKS[*]} == 0 )); then
+    _abort "No hooks found in config file!"
 fi
-if [[ -n "${_GENERATE_IMAGE}" ]]; then
-    # check for permissions. if the image doesn't already exist,
-    # then check the directory
-    if [[ ( -e ${_GENERATE_IMAGE} && ! -w ${_GENERATE_IMAGE} ) ||
-            ( ! -d ${_GENERATE_IMAGE%/*} || ! -w ${_GENERATE_IMAGE%/*} ) ]]; then
-        _abort "Unable to write to '%s'" "${_GENERATE_IMAGE}"
-    fi
-    msg "Starting build: '%s'" "${_KERNELVERSION}"
-elif [[ -n "${_TARGET_DIR}" ]]; then
-    msg "Starting build: '%s'" "${_KERNELVERSION}"
+if [[ -n "${_GENERATE_IMAGE}" || -n "${_TARGET_DIR}" ]]; then
+    echo "Starting build: ${_KERNELVERSION}"
 else
-    msg "Starting dry run: '%s'" "${_KERNELVERSION}"
+    echo "Starting dry run: ${_KERNELVERSION}"
 fi
-# set functrace and trap to catch errors in add_* functions
-declare -i _builderrors=0
-preload_builtin_modules
-map run_build_hook "${_hooks[@]}" || (( ++_builderrors ))
-install_modules "${!_modpaths[@]}"
+_builtin_modules
+_map _run_hook "${_HOOKS[@]}"
+_install_modules "${!_MOD_PATH[@]}"
 # this is simply a nice-to-have -- it doesn't matter if it fails.
 ldconfig -r "${_ROOTFS}" &>"${_NO_LOG}"
 # remove /var/cache/ldconfig/aux-cache for reproducibility
@@ -157,12 +77,11 @@ rm -f -- "${_ROOTFS}/var/cache/ldconfig/aux-cache"
 # Set umask to create initramfs images as 600
 umask 077
 if [[ -n "${_GENERATE_IMAGE}" ]]; then
-    _build_cpio "${_GENERATE_IMAGE}" "${COMPRESSION}" || exit 1
+    _create_cpio "${_GENERATE_IMAGE}" "${_COMP}" || exit 1
 elif [[ -n "${_TARGET_DIR}" ]]; then
     msg "Build complete."
 else
     msg "Dry run complete, use -g IMAGE to generate a real image"
 fi
-exit $(( !!_builderrors ))
 
 # vim: set ft=sh ts=4 sw=4 et:
--- usr/lib/archboot/cpio/cpio.sh
+++ usr/lib/archboot/cpio/cpio.sh
@@ -4,6 +4,31 @@
 # simplified, stripped down, optimized for size and speed
 # by Tobias Powalowski <tpowa@archlinux.org>
 
+_CONFIG=""
+_CPIO=/usr/lib/archboot/cpio/hooks/
+_GENERATE_IMAGE=""
+_TARGET_DIR=""
+declare -A _INCLUDED_MODS _MOD_PATH
+
+_usage() {
+    cat <<EOF
+ARCHBOOT CPIO
+-------------
+Tool for creating an archboot initramfs image.
+
+ -h               Display this message and exit
+
+ -c <config>      Use <config> file
+ -k <kernel>      Use specified <kernel>
+
+ -g <path>        Generate cpio image and write to specified <path>
+ -d <dir>         Generate image into <dir>
+
+usage: ${0##*/} <options>
+EOF
+    exit 0
+}
+
 _abort() {
     echo "Error:" "$@"
     if [[ -n "$_d_workdir" ]]; then
@@ -12,74 +37,91 @@ _abort() {
     exit 1
 }
 
-error() {
-    local mesg="$1"; shift
-    # shellcheck disable=SC2059
-    printf "$_color_red==> ERROR:$_color_none $_color_bold$mesg$_color_none\n" "$@" >&2
-    return 1
+_create_cpio() {
+    case "${_COMP}" in
+        cat)    echo "Creating uncompressed initcpio image: ${_OUT}"
+                unset _COMP_OPTS
+                ;;
+        *)      echo "Creating ${_COMP} compressed initcpio image: ${_OUT}"
+                ;;&
+        xz)     _COMP_OPTS=('-T0' '--check=crc32' "${_COMP_OPTS[@]}")
+                ;;
+        lz4)    _COMP_OPTS=('-l' "${_COMP_OPTS[@]}")
+                ;;
+        zstd)   _COMP_OPTS=('-T0' "${_COMP_OPTS[@]}")
+                ;;
+    esac
+    # Reproducibility: set all timestamps to 0
+    find . -mindepth 1 -execdir touch -hcd "@0" "{}" +
+    find . -mindepth 1 -printf '%P\0' | sort -z | LANG=C bsdtar --null -cnf - -T - |
+            LANG=C bsdtar --null -cf - --format=newc @- |
+            ${_COMP} "${_COMP_OPTS[@]}" > "${_OUT}" || _abort "initcpio image creation failed!"
 }
 
-map() {
-    local r=0
-    for _ in "${@:2}"; do
+_builtin_modules() {
+    # Prime the _INCLUDED_MODS list with the builtins for this kernel.
+    # kmod>=27 and kernel >=5.2 required!
+    while IFS=.= read -rd '' _MODULE _FIELD _VALUE; do
+        _INCLUDED_MODS[${_MODULE//-/_}]=2
+        case "$_FIELD" in
+            alias)  _INCLUDED_MODS["${_VALUE//-/_}"]=2
+                    ;;
+        esac
+    done <"${_MODULE_DIR}/modules.builtin.modinfo"
+}
+
+_map() {
+    for i in "${@:2}"; do
         # shellcheck disable=SC1105,SC2210,SC2035
-        "$1" "$_" || (( $# > 255 ? r=1 : ++r ))
+        "${1}" "${i}" || (( $# > 255 ? _R=1 : ++_R ))
     done
-    return "$r"
+    return "${_R}"
 }
 
-modprobe() {
-    # _optmoduleroot is assigned in mkinitcpio
-    # shellcheck disable=SC2154
-    command modprobe -d "$_optmoduleroot" -S "${_KERNELVERSION}" "$@"
-}
 
-all_modules() {
+_filter_modules() {
     # Add modules to the initcpio, filtered by grep.
     #   $@: filter arguments to grep
     #   -f FILTER: ERE to filter found modules
-    local -i count=0
-    local mod='' OPTIND='' OPTARG='' modfilter=()
-    while getopts ':f:' flag; do
-        [[ "$flag" = "f" ]] && modfilter+=("$OPTARG")
+    local -i _COUNT=0
+    local _MOD_INPUT="" OPTIND="" OPTARG="" _MOD_FILTER=()
+    while getopts ':f:' _FLAG; do
+        [[ "${_FLAG}" = "f" ]] && _MOD_FILTER+=("$OPTARG")
     done
     shift $(( OPTIND - 1 ))
-    # _d_kmoduledir is assigned in mkinitcpio
     # shellcheck disable=SC2154
     while read -r -d '' mod; do
-        (( ++count ))
-        for f in "${modfilter[@]}"; do
-            [[ "$mod" =~ $f ]] && continue 2
+        (( ++_COUNT ))
+        for f in "${_MOD_FILTER[@]}"; do
+            [[ "${_MOD_INPUT}" =~ $f ]] && continue 2
         done
-        mod="${mod##*/}"
-        mod="${mod%.ko*}"
-        printf '%s\n' "${mod//-/_}"
-    done < <(find "$_d_kmoduledir" -name '*.ko*' -print0 2>"${_NO_LOG}" | grep -EZz "$@")
-    (( count ))
+        _MOD_INPUT="${_MOD_INPUT##*/}" _MOD_INPUT="${_MOD_INPUT%.ko*}"
+        printf '%s\n' "${_MOD_INPUT//-/_}"
+    done < <(find "${_MODULE_DIR}" -name '*.ko*' -print0 2>"${_NO_LOG}" | grep -EZz "$@")
+    (( _COUNT ))
 }
 
-add_all_modules() {
+_all_modules() {
     # Add modules to the initcpio.
     #   $@: arguments to all_modules
-    local mod
-    local -a mods
-    mapfile -t mods < <(all_modules "$@")
-    map add_module "${mods[@]}"
-    return $(( !${#mods[*]} ))
+    local _MOD_INPUT
+    local -a _MODS
+    mapfile -t _MODS < <(_filter_modules "$@")
+    _map _module "${_MODS[@]}"
+    return $(( !${#_MODS[*]} ))
 }
 
-add_firmware() {
+_firmware() {
     # add a firmware file to the image.
     #   $1: firmware path fragment
-    local fw fwpath
-    local -a fwfile
-    local -i r=1
-    fwpath=/lib/firmware
-    for fw; do
+    local _FW _FW_DIR
+    local -a _FW_BIN
+    _FW_DIR=/lib/firmware
+    for _FW; do
         # shellcheck disable=SC2154,SC2153
-        if ! compgen -G "${BUILDROOT}${fwpath}/${fw}?(.*)" &>"${_NO_LOG}"; then
-            if read -r fwfile < <(compgen -G "${fwpath}/${fw}?(.*)"); then
-                map add_file "${fwfile[@]}"
+        if ! compgen -G "${_ROOTFS}${_FW_DIR}/${_FW}?(.*)" &>"${_NO_LOG}"; then
+            if read -r _FW_BIN < <(compgen -G "${_FW_DIR}/${_FW}?(.*)"); then
+                _map _file "${_FW_BIN[@]}"
                 break
             fi
         fi
@@ -87,224 +129,201 @@ add_firmware() {
     return 0
 }
 
-add_module() {
-    # Add a kernel module to the initcpio image. Dependencies will be
+_module() {
+    # Add a kernel module to the rootfs. Dependencies will be
     # discovered and added.
     #   $1: module name
-    local target='' module='' softdeps=() deps=() field='' value='' firmware=()
-    if [[ "$1" == *\? ]]; then
+    local _CHECK="" _MOD="" _SOFT=() _DEPS=() _FIELD="" _VALUE="" _FW=()
+    if [[ "${1}" == *\? ]]; then
         set -- "${1%?}"
     fi
-    target="${1%.ko*}" target="${target//-/_}"
+    _CHECK="${1%.ko*}" _CHECK="${_CHECK//-/_}"
     # skip expensive stuff if this module has already been added
-    (( _addedmodules["$target"] == 1 )) && return
-    while IFS=':= ' read -r -d '' field value; do
-        case "$field" in
+    (( _INCLUDED_MODS["${_CHECK}"] == 1 )) && return
+    while IFS=':= ' read -r -d '' _FIELD _VALUE; do
+        case "${_FIELD}" in
             filename)
                 # Only add modules with filenames that look like paths (e.g.
                 # it might be reported as "(builtin)"). We'll defer actually
                 # checking whether or not the file exists -- any errors can be
                 # handled during module install time.
-                if [[ "$value" == /* ]]; then
-                    module="${value##*/}" module="${module%.ko*}"
-                    _modpaths[".$value"]=1
-                    _addedmodules["${module//-/_}"]=1
+                if [[ "${_VALUE}" == /* ]]; then
+                    _MOD="${_VALUE##*/}" _MOD="${_MOD%.ko*}"
+                    _MOD_PATH[".${_VALUE}"]=1
+                    _INCLUDED_MODS["${_MOD//-/_}"]=1
                 fi
                 ;;
             depends)
-                IFS=',' read -r -a deps <<< "$value"
-                map add_module "${deps[@]}"
+                IFS=',' read -r -a _DEPS <<< "${_VALUE}"
+                _map _module "${_DEPS[@]}"
                 ;;
             firmware)
-                firmware+=("$value")
+                _FW+=("${_VALUE}")
                 ;;
             softdep)
-                read -ra softdeps <<<"$value"
-                for module in "${softdeps[@]}"; do
-                    [[ $module == *: ]] && continue
-                    add_module "$module?"
+                read -ra _SOFT <<<"${_VALUE}"
+                for i in "${_SOFT[@]}"; do
+                    [[ ${i} == *: ]] && continue
+                    _module "${i}?"
                 done
                 ;;
         esac
-    done < <(modinfo -b "$_optmoduleroot" -k "${_KERNELVERSION}" -0 "$target" 2>"${_NO_LOG}")
-    if (( ${#firmware[*]} )); then
-        add_firmware "${firmware[@]}"
+    done < <(modinfo -b "${_MODULE_DIR}" -k "${_KERNELVERSION}" -0 "${_CHECK}" 2>"${_NO_LOG}")
+    if (( ${#_FW[*]} )); then
+        _firmware "${_FW[@]}"
     fi
 }
 
-add_full_dir() {
-    # Add a directory and all its contents, recursively, to the initcpio image.
+_full_dir() {
+    # Add a directory and all its contents, recursively, to the rootfs.
     # No parsing is performed and the contents of the directory is added as is.
     #   $1: path to directory
-    if [[ -n $1 && -d $1 ]]; then
-        command tar -C /  --hard-dereference -cpf - ."$1" | tar -C "${BUILDROOT}" -xpf - || return 1
+    if [[ -n "${1}" && -d "${1}" ]]; then
+        command tar -C / --hard-dereference -cpf - ."${1}" | tar -C "${_ROOTFS}" -xpf - || return 1
     fi
 }
 
-add_dir() {
+_dir() {
     # add a directory (with parents) to $BUILDROOT
     #   $1: pathname on initcpio
     #   $2: mode (optional)
-    local mode="${2:-755}"
+    local _MODE="${2:-755}"
     # shellcheck disable=SC2153
-    if [[ -d "${BUILDROOT}${1}" ]]; then
+    if [[ -d "${_ROOTFS}${1}" ]]; then
         # ignore dir already exists
         return 0
     fi
-    command mkdir -p -m "${mode}" "${BUILDROOT}${1}" || return 1
+    command mkdir -p -m "${_MODE}" "${_ROOTFS}${1}" || return 1
 }
 
-add_symlink() {
-    # Add a symlink to the initcpio image. There is no checking done
+_symlink() {
+    # Add a symlink to the rootfs. There is no checking done
     # to ensure that the target of the symlink exists.
     #   $1: pathname of symlink on image
     #   $2: absolute path to target of symlink (optional, can be read from $1)
-    local name="$1" target="${2:-$1}" linkobject
+    local _LINK_NAME="${1}" _LINK_SOURCE="${2:-$1}" _LINK_DEST
     # find out the link target
-    if [[ "$name" == "$target" ]]; then
-        linkobject="$(find "$target" -prune -printf '%l')"
+    if [[ "${_LINK_NAME}" == "${_LINK_SOURCE}" ]]; then
+        _LINK_DEST="$(find "${_LINK_SOURCE}" -prune -printf '%l')"
         # use relative path if the target is a file in the same directory as the link
         # anything more would lead to the insanity of parsing each element in its path
-        if [[ "$linkobject" != *'/'* && ! -L "${name%/*}/${linkobject}" ]]; then
-            target="$linkobject"
+        if [[ "${_LINK_DEST}" != *'/'* && ! -L "${_LINK_NAME%/*}/${_LINK_DEST}" ]]; then
+            _LINK_SOURCE="${_LINK_DEST}"
         else
-            target="$(realpath -eq -- "$target")"
+            _LINK_SOURCE="$(realpath -eq -- "${_LINK_SOURCE}")"
         fi
-    elif [[ -L "$target" ]]; then
-        target="$(realpath -eq -- "$target")"
+    elif [[ -L "${_LINK_SOURCE}" ]]; then
+        _LINK_SOURCE="$(realpath -eq -- "${_LINK_SOURCE}")"
     fi
-    add_dir "${name%/*}"
-    ln -sfn "$target" "${BUILDROOT}${name}"
+    _dir "${_LINK_NAME%/*}"
+    command ln -sfn "${_LINK_SOURCE}" "${_ROOTFS}${_LINK_NAME}"
 }
 
-add_file() {
-    # Add a plain file to the initcpio image. No parsing is performed and only
+_file() {
+    # Add a plain file to the rootfs. No parsing is performed and only
     # the singular file is added.
     #   $1: path to file
     #   $2: destination on initcpio (optional, defaults to same as source)
     #   $3: mode
     # determine source and destination
-    local src="$1" dest="${2:-$1}" mode="$3" srcrealpath
-    if [[ ! -e "${BUILDROOT}${dest}" ]]; then
-        if [[ "$src" != "$dest" ]]; then
-            command tar --hard-dereference --transform="s|$src|$dest|" -C / -cpf - ."$src" | tar -C "${BUILDROOT}" -xpf - || return 1
+    local _SRC="${1}" _DEST="${2:-$1}" _MODE="${3}"
+    if [[ ! -e "${_ROOTFS}${_DEST}" ]]; then
+        if [[ "${_SRC}" != "${_DEST}" ]]; then
+            command tar --hard-dereference --transform="s|${_SRC}|${_DEST}|" -C / -cpf - ."${_SRC}" | tar -C "${_ROOTFS}" -xpf - || return 1
         else
-            command tar --hard-dereference -C / -cpf - ."$src" | tar -C "${BUILDROOT}" -xpf - || return 1
+            command tar --hard-dereference -C / -cpf - ."${_SRC}" | tar -C "${_DEST}" -xpf - || return 1
         fi
-        if [[ -L "$src" ]]; then
-            srcrealpath="$(realpath -- "$src")"
-            add_file  "$srcrealpath" "$srcrealpath" "$mode"
+        if [[ -L "${_SRC}" ]]; then
+            _LINK_SOURCE="$(realpath -- "${_SRC}")"
+            _file  "${_LINK_SOURCE}" "${_LINK_SOURCE}" "${_MODE}"
         else
-            if [[ -n $mode ]]; then
-                command chmod "$mode" "${BUILDROOT}${dest}"
+            if [[ -n "${_MODE}" ]]; then
+                command chmod "${_MODE}" "${_ROOTFS}${_DEST}"
             fi
         fi
     fi
 }
 
-add_binary() {
-    # Add a binary file to the initcpio image. library dependencies will
+_binary() {
+    # Add a binary file to the rootfs. library dependencies will
     # be discovered and added.
     #   $1: path to binary
-    #   $2: destination on initcpio (optional, defaults to same as source)
-    local line='' regex='' binary='' dest='' mode='' sodep=''
+    #   $2: destination on rootfs (optional, defaults to same as source)
     if [[ "${1:0:1}" != '/' ]]; then
-        binary="$(type -P "$1")"
+        _BIN="$(type -P "${1}")"
     else
-        binary="$1"
+        _BIN="${1}"
     fi
-    dest="${2:-$binary}"
-    add_file "$binary" "$dest"
+    _ROOTFS_BIN="${2:-${_BIN}}"
+    _file "${_BIN}" "${_ROOTFS_BIN}"
     # non-binaries
-    if ! lddout="$(ldd "$binary" 2>"${_NO_LOG}")"; then
+    if ! _LDD="$(ldd "${_BIN}" 2>"${_NO_LOG}")"; then
         return 0
     fi
-    # resolve sodeps
-    regex='^(|.+ )(/.+) \(0x[a-fA-F0-9]+\)'
-    while read -r line; do
-        if [[ "$line" =~ $regex ]]; then
-            sodep="${BASH_REMATCH[2]}"
+    # resolve libraries
+    _REGULAR_EXPRESSION='^(|.+ )(/.+) \(0x[a-fA-F0-9]+\)'
+    while read -r i; do
+        if [[ "${i}" =~ ${_REGULAR_EXPRESSION} ]]; then
+            _LIB="${BASH_REMATCH[2]}"
         fi
-        if [[ -f "$sodep" && ! -e "${BUILDROOT}${sodep}" ]]; then
-            add_file "$sodep" "$sodep"
+        if [[ -f "${_LIB}" && ! -e "${_ROOTFS}${_LIB}" ]]; then
+            _file "${_LIB}" "${_LIB}"
         fi
-    done <<< "$lddout"
+    done <<< "${_LDD}"
     return 0
 }
 
-initialize_buildroot() {
-    # creates a temporary directory for the buildroot and initialize it with a
+_init_rootfs() {
+    # creates a temporary directory for the rootfs and initialize it with a
     # basic set of necessary directories and symlinks
-    local workdir='' kernver="$1" arch buildroot
-    arch="$(uname -m)"
-    if ! workdir="$(mktemp -d --tmpdir mkinitcpio.XXXXXX)"; then
-        error 'Failed to create temporary working directory in %s' "${TMPDIR:-/tmp}"
-        return 1
-    fi
-    buildroot="${2:-$workdir/root}"
-    if [[ ! -w "${2:-$workdir}" ]]; then
-        error 'Unable to write to build root: %s' "$buildroot"
-        return 1
-    fi
+    _TMPDIR="$(mktemp -d --tmpdir mkinitcpio.XXXX)"
+    _ROOTFS="${2:-${_TMPDIR}/root}"
     # base directory structure
-    install -dm755 "$buildroot"/{new_root,proc,sys,dev,run,tmp,var,etc,usr/{local{,/bin,/sbin,/lib},lib,bin}}
-    ln -s "usr/lib" "$buildroot/lib"
-    ln -s "bin"     "$buildroot/usr/sbin"
-    ln -s "usr/bin" "$buildroot/bin"
-    ln -s "usr/bin" "$buildroot/sbin"
-    ln -s "/run"    "$buildroot/var/run"
-    case "$arch" in
-        x86_64)
-            ln -s "lib"     "$buildroot/usr/lib64"
-            ln -s "usr/lib" "$buildroot/lib64"
-            ;;
-    esac
+    install -dm755 "${_ROOTFS}"/{new_root,proc,sys,dev,run,tmp,var,etc,usr/{local{,/bin,/sbin,/lib},lib,bin}}
+    ln -s "usr/lib" "${_ROOTFS}/lib"
+    ln -s "bin" "${_ROOTFS}/usr/sbin"
+    ln -s "usr/bin" "${_ROOTFS}/bin"
+    ln -s "usr/bin" "${_ROOTFS}/sbin"
+    ln -s "/run" "${_ROOTFS}/var/run"
+    if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
+            ln -s "lib" "${_ROOTFS}/usr/lib64"
+            ln -s "usr/lib" "${_ROOTFS}/lib64"
+    fi
     # kernel module dir
-    [[ "$kernver" != 'none' ]] && install -dm755 "$buildroot/usr/lib/modules/$kernver/kernel"
+    [[ "${_KERNELVERSION}" != 'none' ]] && install -dm755 "${_ROOTFS}/usr/lib/modules/${_KERNELVERSION}/kernel"
     # mount tables
-    ln -s ../proc/self/mounts "$buildroot/etc/mtab"
-    : >"$buildroot/etc/fstab"
+    ln -s ../proc/self/mounts "${_ROOTFS}/etc/mtab"
+    : >"${_ROOTFS}/etc/fstab"
     # add a blank ld.so.conf to keep ldconfig happy
-    : >"$buildroot/etc/ld.so.conf"
-    printf '%s' "$workdir"
+    : >"${_ROOTFS}/etc/ld.so.conf"
 }
 
-run_build_hook() {
-    local hook="$1" script=''
-    # shellcheck disable=SC2034
-    local MODULES=() BINARIES=() FILES=() SCRIPT=''
+_run_hook() {
     # find script in install dirs
     # shellcheck disable=SC2154
-    if ! script="$(PATH="${_CPIO}" type -P "$hook")"; then
-        error "Hook '$hook' cannot be found"
+    if ! _HOOK_FILE="$(PATH="${_CPIO}" type -P "${1}")"; then
+        _abort "Hook ${1} cannot be found!"
         return 1
     fi
     # source
-    unset -f build
+    unset -f _run
     # shellcheck disable=SC1090
-    if ! . "$script"; then
-        error 'Failed to read %s' "$script"
-        return 1
-    fi
-    if ! declare -f build >"${_NO_LOG}"; then
-        error "Hook '%s' has no build function" "${script}"
+    . "${_HOOK_FILE}"
+    if ! declare -f _run >"${_NO_LOG}"; then
+        _abort "Hook ${_HOOK_FILE} has no run function!"
         return 1
     fi
     # run
-    msg2 "Running build hook: [%s]" "${script##*/}"
-    build
-    # if we made it this far, return successfully. Hooks can
-    # do their own error catching if it's severe enough, and
-    # we already capture errors from the add_* functions.
-    return 0
+    echo "Running hook:" "${_HOOK_FILE##*/}"
+    _run
 }
 
-install_modules() {
-    command tar --hard-dereference -C / -cpf - "$@" | tar -C "${BUILDROOT}" -xpf -
-    msg "Generating module dependencies"
-    map add_file "$_d_kmoduledir"/modules.{builtin,builtin.modinfo,order}
-    depmod -b "$BUILDROOT" "${_KERNELVERSION}"
+_install_modules() {
+    command tar --hard-dereference -C / -cpf - "$@" | tar -C "${_ROOTFS}" -xpf -
+    echo "Generating module dependencies..."
+    _map _file "${_MODULE_DIR}"/modules.{builtin,builtin.modinfo,order}
+    depmod -b "${_ROOTFS}" "${_KERNELVERSION}"
     # remove all non-binary module.* files (except devname for on-demand module loading
     # and builtin.modinfo for checking on builtin modules)
-    rm "${BUILDROOT}${_d_kmoduledir}"/modules.!(*.bin|*.modinfo|devname|softdep)
+    rm "${_ROOTFS}${_MODULE_DIR}"/modules.!(*.bin|*.modinfo|devname|softdep)
 }
