e2c933dd3e4001293a7b3d8f6d898e6ab768869a
Tobias Powalowski <tpowa@archlinux.org>
2023-10-16 19:24:14 +0200

    reorder packages

--- usr/lib/archboot/container.sh
+++ usr/lib/archboot/container.sh
@@ -146,13 +146,13 @@ _install_base_packages() {
         _MKINITCPIO=initramfs
     fi
     if [[ "${2}" == "use_binfmt" ]]; then
-        echo "Downloading  ${_MKINITCPIO} ${_KEYRING} ${_PACKAGES} to ${1}..."
+        echo "Downloading ${_KEYRING} ${_PACKAGES} ${_MKINITCPIO} to ${1}..."
         if grep -q 'archboot' /etc/hostname; then
             #shellcheck disable=SC2086
-            ${_PACMAN} -Syw ${_MKINITCPIO} ${_KEYRING} ${_PACKAGES} ${_PACMAN_DEFAULTS} ${_PACMAN_DB} &>"${_LOG}" || exit 1
+            ${_PACMAN} -Syw ${_KEYRING} ${_PACKAGES} ${_MKINITCPIO} ${_PACMAN_DEFAULTS} ${_PACMAN_DB} &>"${_LOG}" || exit 1
         else
             #shellcheck disable=SC2086
-            ${_PACMAN} -Syw ${_MKINITCPIO} ${_KEYRING} ${_PACKAGES} ${_PACMAN_DEFAULTS} ${_PACMAN_DB} &>"${_NO_LOG}" || exit 1
+            ${_PACMAN} -Syw ${_KEYRING} ${_PACKAGES} ${_MKINITCPIO} ${_PACMAN_DEFAULTS} ${_PACMAN_DB} &>"${_NO_LOG}" || exit 1
         fi
     fi
     echo "Installing ${_KEYRING} ${_PACKAGES} to ${1}..."
