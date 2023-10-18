79f9a97e31f048399982ded7e8f909483ba5feca
Tobias Powalowski <tpowa@archlinux.org>
2023-10-18 21:06:13 +0200

    update SPDX

--- usr/share/archboot/base/etc/profile.d/archboot-reset-root-password.sh
+++ usr/share/archboot/base/etc/profile.d/archboot-reset-root-password.sh
@@ -1,3 +1,3 @@
-# SPDX-License-Identifier: GPL-2.0-only
+# SPDX-License-Identifier: GPL-3.0-or-later
 # created by Tobias Powalowski <tpowa@archlinux.org>
 passwd -S root | grep -q 'L' && passwd -d root >/dev/null
