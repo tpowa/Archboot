f7c7a7087c1fbaedce2926605f01301219e42f83
Tobias Powalowski <tpowa@archlinux.org>
2023-10-19 13:48:35 +0200

    remove;\

--- usr/lib/archboot/release.sh
+++ usr/lib/archboot/release.sh
@@ -143,18 +143,18 @@ _create_iso() {
     touch boot/*
     # create Release.txt with included main archlinux packages
     echo "Generating Release.txt..."
-    (echo "ARCHBOOT - ARCH LINUX INSTALLATION / RESCUE SYSTEM";\
-    echo "(c) 2006 - 2023 | https://archboot.com";\
-    echo "Tobias Powalowski <tpowa@archlinux.org>";\
-    echo "";\
-    echo "Requirement: ${_ARCH} with 700M RAM and higher";\
-    echo "Archboot:$(${_NSPAWN} "${_W_DIR}" pacman -Qi "${_ARCHBOOT}" | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")";\
-    [[ "${_ARCH}" == "riscv64" ]] || echo "Grub: $(${_NSPAWN} "${_W_DIR}" pacman -Qi grub | grep Version | cut -d ":" -f3 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")";\
-    echo "Linux:$(${_NSPAWN} "${_W_DIR}" pacman -Qi linux | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")";\
-    echo "Pacman:$(${_NSPAWN} "${_W_DIR}" pacman -Qi pacman | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")";\
-    echo "Systemd:$(${_NSPAWN} "${_W_DIR}" pacman -Qi systemd | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")";\
-    echo "";\
-    echo "---Complete Package List---";\
+    (echo "ARCHBOOT - ARCH LINUX INSTALLATION / RESCUE SYSTEM"
+    echo "(c) 2006 - 2023 | https://archboot.com"
+    echo "Tobias Powalowski <tpowa@archlinux.org>"
+    echo ""
+    echo "Requirement: ${_ARCH} with 700M RAM and higher"
+    echo "Archboot:$(${_NSPAWN} "${_W_DIR}" pacman -Qi "${_ARCHBOOT}" | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")"
+    [[ "${_ARCH}" == "riscv64" ]] || echo "Grub: $(${_NSPAWN} "${_W_DIR}" pacman -Qi grub | grep Version | cut -d ":" -f3 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")"
+    echo "Linux:$(${_NSPAWN} "${_W_DIR}" pacman -Qi linux | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")"
+    echo "Pacman:$(${_NSPAWN} "${_W_DIR}" pacman -Qi pacman | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")"
+    echo "Systemd:$(${_NSPAWN} "${_W_DIR}" pacman -Qi systemd | grep Version | cut -d ":" -f2 | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g")"
+    echo ""
+    echo "---Complete Package List---"
     ${_NSPAWN} "${_W_DIR}" pacman -Q | sed -e "s/\r//g" -e "s/\x1b\[[0-9;]*m//g" -e "s/\x1b\[.*[0-9][h;l]//g") >>Release.txt
     # removing container
     echo "Removing container ${_W_DIR}..."
