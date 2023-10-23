0bc482807fc11352ece2dc65a55a76b8a16759c0
Tobias Powalowski <tpowa@archlinux.org>
2023-10-23 19:08:40 +0200

    remove rEFInd from MIX MODE selection

--- usr/lib/archboot/installer/bootloader.sh
+++ usr/lib/archboot/installer/bootloader.sh
@@ -204,14 +204,15 @@ _efistub_uefi() {
     _efistub_parameters
     _common_bootloader_checks
     if [[ -z ${_EFI_MIXED} ]]; then
-        _REFIND_ENTRY="rEFInd"
-        _REFIND_DESC="rEFInd for ${_UEFI_ARCH} UEFI"
-    fi
-    if [[ "${_RUNNING_ARCH}" == "x86_64" ]]; then
+    if [[ "${_RUNNING_ARCH}" == "x86_64" && -z ${_EFI_MIXED} ]]; then
         _dialog --title " EFISTUB " --menu "" 10 60 3 \
             "FIRMWARE" "Unified Kernel Image for ${_UEFI_ARCH} UEFI" \
             "LIMINE" "LIMINE for ${_UEFI_ARCH} UEFI" \
-            "${_REFIND_ENTRY}" "${_REFIND_DESC}" \
+            "rEFInd" "rEFInd for ${_UEFI_ARCH} UEFI" \
+            "SYSTEMD-BOOT" "SYSTEMD-BOOT for ${_UEFI_ARCH} UEFI" 2>"${_ANSWER}"
+    elif [[ "${_RUNNING_ARCH}" == "x86_64" && -n ${_EFI_MIXED} ]]; then
+            "FIRMWARE" "Unified Kernel Image for ${_UEFI_ARCH} UEFI" \
+            "LIMINE" "LIMINE for ${_UEFI_ARCH} UEFI" \
             "SYSTEMD-BOOT" "SYSTEMD-BOOT for ${_UEFI_ARCH} UEFI" 2>"${_ANSWER}"
     else
         _dialog --title " EFISTUB " --menu "" 8 60 3 \
