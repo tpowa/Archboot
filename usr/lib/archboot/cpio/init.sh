9fd98566c8faa5baa2362c08e914a0f6e83c3212
Tobias Powalowski <tpowa@archlinux.org>
2023-11-02 07:47:42 +0100

    switch to pure systemd bootup

--- usr/lib/archboot/cpio/init.sh
+++ usr/lib/archboot/cpio/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# SPDX-License-Identifier: GPL-2.0-only
+# SPDX-License-Identifier: GPL-3.0-or-later
 # created by Tobias Powalowski <tpowa@archlinux.org>
 LANG=C
 _TITLE="Archboot $(uname -m) | $(uname -r) | Basic Setup | Early Userspace"
@@ -7,11 +7,6 @@ _KEEP="Please keep the boot medium inserted."
 _dialog() {
     dialog --backtitle "${_TITLE}" "$@"
     return $?
-}
-_udev_trigger() {
-    udevadm trigger --action=add --type=subsystems
-    udevadm trigger --action=add --type=devices
-    udevadm settle
 }
 _progress() {
 cat <<EOF
@@ -32,7 +27,7 @@ _progress_wait() {
             _progress "${2}" "${3}"
         fi
         _COUNT="$((_COUNT+1))"
-        read -r -t "${4}"
+        sleep "${4}"
         ! [[ -e /.archboot ]] && break
     done
 }
@@ -78,13 +73,6 @@ _first_stage() {
     _progress_wait "0" "99" "${_KEEP} Loading files..." "0.5"
     _progress "100" "${_KEEP}"
 }
-_udev_stage() {
-    udevadm control -R
-    _udev_trigger
-    # shutdown udevd
-    udevadm control --exit
-    udevadm info --cleanup-db
-}
 _second_stage() {
     : >/.archboot
     _task cleanup &
@@ -97,38 +85,24 @@ _second_stage() {
     _task unmount &
     _progress_wait "98" "99" "${_KEEP} Unmounting archboot rootfs..." "1"
     _progress "100" "The boot medium can be safely removed now."
-    read -r -t 2
+    sleep -r -t 2
     # remove files and directories
     rm -r /sysroot/sysroot
     rm /sysroot/init
 }
-# mount kernel filesystems
-mount -t proc proc /proc -o nosuid,noexec,nodev
-mount -t sysfs sys /sys -o nosuid,noexec,nodev
-mount -t devtmpfs dev /dev -o mode=0755,nosuid
-mount -t tmpfs run /run -o nosuid,nodev,mode=0755
-if [ -e /sys/firmware/efi ]; then
-    mount -t efivarfs efivarfs /sys/firmware/efi/efivars -o nosuid,nodev,noexec
-fi
-echo archboot >/proc/sys/kernel/hostname
-# initialize udev
-kmod static-nodes --format=tmpfiles --output=/run/tmpfiles.d/kmod.conf
-systemd-tmpfiles --prefix=/dev --create --boot
-/usr/lib/systemd/systemd-udevd --daemon --resolve-names=never &>/dev/null
-_udev_trigger
 # not all devices trigger autoload!
 modprobe -q cdrom
 modprobe -q usb-storage
 modprobe -q zram
 modprobe -q zstd
-echo 1 > /proc/sys/kernel/sysrq
 echo "Initializing Console..."
 printf "\ec"
 # it needs one echo before, in order to reset the consolefont!
-setfont consolefont-16.psf.gz -C /dev/console
+setfont ter-v16n -C /dev/console
+loadkeys us
 echo "Searching 10 seconds for Archboot $(uname -m) rootfs..."
 _COUNT=0
-while true; do
+while ! [[ "${_COUNT}" == 10 ]]; do
     # dd / rufus
     mount UUID=1234-ABCD /mnt &>/dev/null && break
     # ventoy
@@ -140,9 +114,8 @@ while true; do
     if [[ -b /dev/sr0 ]]; then
         mount /dev/sr0 /cdrom &>/dev/null && break
     fi
-    read -r -t 1
+    sleep 1
     _COUNT=$((_COUNT+1))
-    [[ "${_COUNT}" == 10 ]] && break
 done
 if ! [[ -f "/mnt/boot/initrd-$(uname -m).img" ]] ; then
     if ! mount /cdrom/efi.img /mnt &>/dev/null; then
@@ -151,17 +124,14 @@ if ! [[ -f "/mnt/boot/initrd-$(uname -m).img" ]] ; then
         echo -e "\e[1mThis needs further debugging. Please contact the archboot author.\e[m"
         echo -e "\e[1mTobias Powalowski: tpowa@archlinux.org\e[m"
         echo ""
-        echo -e "\e[1;93mType 'exit' or 'reboot' for reboot.\e[m"
-        echo -e "\e[1;93mType 'poweroff' for poweroff.\e[m"
-        /bin/bash
-        echo b >/proc/sysrq-trigger
+        systemctl start emergency.service
     fi
 fi
 _first_stage | _dialog --title " Loading Kernel Modules " --gauge "${_KEEP} Loading files..." 6 75 0
 # avoid screen messup, don't run dialog on module loading!
 printf "\ec"
-# reinitialize available modules
-_udev_stage
+udevadm trigger --type=all --action=add --prioritized-subsystem=module,block,tpmrm,net,tty,input
+udevadm settle
 # autodetect screen size
 FB_SIZE="$(cut -d 'x' -f 1 "$(find /sys -wholename '*fb0/modes')" | sed -e 's#.*:##g')"
 if [[ "${FB_SIZE}" -gt '1900' ]]; then
@@ -172,12 +142,10 @@ fi
 # clear screen
 echo "Initializing Console..."
 printf "\ec"
-setfont consolefont-${SIZE}.psf.gz -C /dev/console
+setfont ter-v${SIZE}n -C /dev/console
 _second_stage | _dialog --title " Initializing System " --gauge "${_KEEP} Removing files..." 6 75 0
-echo 0 > /proc/sys/kernel/sysrq
 # set font size in vconsole.conf
 echo FONT=ter-v${SIZE}n >> /sysroot/etc/vconsole.conf
 printf "\ec"
 echo "Launching systemd $(udevadm --version)..."
-exec switch_root /sysroot /usr/bin/init "$@"
 # vim: set ft=sh ts=4 sw=4 et:
