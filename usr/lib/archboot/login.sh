128d64f9d9bbf6845edd4cd8e6f1a87db72eeee2
Tobias Powalowski <tpowa@archlinux.org>
2023-11-17 12:41:01 +0100

    fix timing

--- usr/lib/archboot/login.sh
+++ usr/lib/archboot/login.sh
@@ -36,19 +36,19 @@ if [[ "${TTY}" = "tty1" ]]; then
     : > /.archboot
     [[ -d /sysroot ]] || mkdir /sysroot
     (modprobe -q zram
-    modprobe -q zstd
-    echo "1" >/sys/block/zram0/reset
-    echo "zstd" >/sys/block/zram0/comp_algorithm
-    echo "5G" >/sys/block/zram0/disksize
-    mkfs.btrfs /dev/zram0 &>"${_NO_LOG}"
-    mount -o discard /dev/zram0 /sysroot &>"${_NO_LOG}"
-    rm /.archboot) &
-    _progress_wait "1" "5" "Creating btrfs on /dev/zram0..." "1"
+        modprobe -q zstd
+        echo "1" >/sys/block/zram0/reset
+        echo "zstd" >/sys/block/zram0/comp_algorithm
+        echo "5G" >/sys/block/zram0/disksize
+        mkfs.btrfs /dev/zram0 &>"${_NO_LOG}"
+        mount -o discard /dev/zram0 /sysroot &>"${_NO_LOG}"
+        rm /.archboot) &
+    _progress_wait "1" "30" "Creating btrfs on /dev/zram0..." "0.1"
     : > /.archboot
     (tar -C / --exclude="./dev/*" --exclude="./proc/*" --exclude="./sys/*" \
         --exclude="./run/*" --exclude="./mnt/*" --exclude="./tmp/*" --exclude="./sysroot/*" \
         -clpf - . | tar -C /sysroot -xlspf - &>"${_NO_LOG}"; rm /.archboot) &
-    _progress_wait "6" "99" "Copying archboot rootfs to /sysroot..." "0.125"
+    _progress_wait "31" "99" "Copying archboot rootfs to /sysroot..." "0.125"
     # cleanup directories and files
     rm -r /sysroot/sysroot &>"${_NO_LOG}"
     rm /sysroot/init &>"${_NO_LOG}"
