fa4669ec8c19bbc10573c65ee521ce925174573e
Tobias Powalowski <tpowa@archlinux.org>
2024-02-08 20:23:27 +0100

    revert f2fs grub on boot

--- usr/lib/archboot/installer/bootloader_grub.sh
+++ usr/lib/archboot/installer/bootloader_grub.sh
@@ -24,6 +24,7 @@ _grub_common_before() {
     _FAIL_COMPLEX=""
     _RAID_ON_LVM=""
     _common_bootloader_checks
+    _abort_f2fs_bootpart || return 1
     _abort_bcachefs_bootpart || return 1
     if [[ ! -d "${_DESTDIR}/usr/lib/grub" ]]; then
         _PACKAGES="grub"
