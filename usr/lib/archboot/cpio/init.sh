55092042b2325ad6a825290b0e8d71968fa59f0c
Tobias Powalowski <tpowa@archlinux.org>
2024-02-28 11:24:14 +0100

    remove double systemd

--- usr/lib/archboot/cpio/init.sh
+++ usr/lib/archboot/cpio/init.sh
@@ -142,5 +142,5 @@ _initrd_stage | _dialog --title " Initializing System " --gauge "${_KEEP} Search
 _clear
 _msg "The boot medium can be safely removed now."
 echo ""
-_msg "Launching systemd $(systemctl --version | head -n1)..."
+_msg "Launching $(systemctl --version | head -n1)..."
 # vim: set ft=sh ts=4 sw=4 et:
