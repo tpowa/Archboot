d18fc50e2555899d0c0b62ad2ac482a2b76a6a7c
Tobias Powalowski <tpowa@archlinux.org>
2024-03-28 15:49:15 +0100

    update lsws script

--- usr/share/archboot/server/lsws-autoindex.sh
+++ usr/share/archboot/server/lsws-autoindex.sh
@@ -5,12 +5,31 @@
 # copy header and footer for autoindex litespeed webserver
 # header is placed in plain text on top
 # readme is placed in plain text on bottom
+# main site
 for i in pkg release src; do
     for k in $(find public_html/${i}/ -type d); do
-        [[ "${i}" == "release" ]] && _TITLE="CDN Release Mirror"
+        [[ "${i}" == "release" ]] && _TITLE="Release Mirror"
         [[ "${i}" == "pkg" ]] && _TITLE="Package Repository"
         [[ "${i}" == "src" ]] && _TITLE="Sources"
         echo "Archboot - ${_TITLE} | (c) 2006 - $(date +%Y) Tobias Powalowski | Arch Linux Developer tpowa" > ${k}/HEADER.html
+    done
+done
+# mirrors
+cd public_html
+for k in $(find release/ -type d); do
+    _TITLE="Release Mirror"
+    [[ -d "~/${k}" ]] && echo "Archboot - ${_TITLE} | (c) 2006 - $(date +%Y) Tobias Powalowski | Arch Linux Developer tpowa" 2>/dev/null > ~/${k}/HEADER.html
+done
+# clean directory first
+for i in aarch64 riscv64 x86_64; do
+    rm ~/release/${i}/*
+done
+# keep 4 versions on mirrors
+for i in $(seq 0 3); do 
+    _SYMLINK=$(date -d "$(date +) - ${i} Months" +%Y.%m)
+    for k in aarch64 riscv64 x86_64; do
+        ln -s ~/public_html/release/${k}/${_SYMLINK} \
+              ~/release/${k}/${_SYMLINK}
     done
 done
 # vim: set ft=sh ts=4 sw=4 et:
