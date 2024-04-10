9a5224e81a7ef0eab4e46ef3f2fb5d49880c2040
Tobias Powalowski <tpowa@archlinux.org>
2024-04-10 12:04:26 +0200

    update status of bootloaders which support /efi and /boot as systemd-auto-gpt supports: grub and systemd-boot

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -548,6 +548,8 @@ _mkfs() {
     # /boot or /efi as ESP: c12a7328-f81f-11d2-ba4b-00a0c93ec93b
     # /boot as Extended Boot Loader Partition: bc13c2ff-59e6-4262-a352-b275fd6f7172
     # only as vfat supported by auto-generator!
+    ### limine and refind do not support this! STATUS on 10.04.2024
+    ### grub and systemd-boot work!
     # "${_GUID_VALUE}" == "c12a7328-f81f-11d2-ba4b-00a0c93ec93b" && "${5}" == "/efi"
     # "${_GUID_VALUE}" == "c12a7328-f81f-11d2-ba4b-00a0c93ec93b" && "${5}" == "/boot"
     # "${_GUID_VALUE}" == "bc13c2ff-59e6-4262-a352-b275fd6f7172" && "${5}" == "/boot" && "${2}" == "vfat"
@@ -558,9 +560,6 @@ _mkfs() {
         _GUID_VALUE="$(${_LSBLK} PARTTYPE "${1}")"
         if ! [[ "${_GUID_VALUE}" == "933ac7e1-2eb4-4f13-b844-0e14e2aef915" && "${5}" == "/home" ||\
                 "${_GUID_VALUE}" == "0657fd6d-a4ab-43c4-84e5-0933c84b4f4f" && "${5}" == "swap" ||\
-                "${_GUID_VALUE}" == "c12a7328-f81f-11d2-ba4b-00a0c93ec93b" && "${5}" == "/efi" ||\
-                "${_GUID_VALUE}" == "c12a7328-f81f-11d2-ba4b-00a0c93ec93b" && "${5}" == "/boot" ||\
-                "${_GUID_VALUE}" == "bc13c2ff-59e6-4262-a352-b275fd6f7172" && "${5}" == "/boot" && "${2}" == "vfat" ||\
                 "${5}" == "/" ]]; then
             echo -n "${_DEV} ${5} ${2} defaults 0 " >>/tmp/.fstab
             _check_filesystem_fstab "$@"
