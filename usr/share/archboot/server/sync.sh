df30f918786d9e57ad8a1fe32388d4f2e7a643f4
Tobias Powalowski <tpowa@archlinux.org>
2024-04-11 11:46:59 +0200

    update sync.sh script

--- usr/share/archboot/server/sync.sh
+++ usr/share/archboot/server/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rsync --delete --delete-before -L -k -a archboot.com:release ~/public_html/
-rsync --delete --delete-before --copy-dirlinks -a archboot.com:pkg ~/public_html/
+rsync --delete --delete-before -L -a archboot.com:release ~/public_html/
+rsync --delete --delete-before -L -a archboot.com:pkg ~/public_html/
 for i in aarch64 riscv64 x86_64; do
     cd ~/public_html/release/${i}/
     _DIR="$(ls -1rvd */ | head -n1)"
