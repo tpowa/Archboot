156bf51fa7c8d0829d80203ac592685a2eecebc7
Tobias Powalowski <tpowa@archlinux.org>
2024-04-11 21:08:52 +0200

    update lsws script

--- usr/share/archboot/server/lsws-autoindex.sh
+++ usr/share/archboot/server/lsws-autoindex.sh
@@ -7,32 +7,33 @@
 # readme is placed in plain text on bottom
 # main site
 for i in pkg release src; do
-    for k in $(find public_html/${i}/ -type d); do
+    #shellcheck disable=SC2044
+    for k in $(find public_html/"${i}"/ -type d); do
         [[ "${i}" == "release" ]] && _TITLE="Release Mirror"
         [[ "${i}" == "pkg" ]] && _TITLE="Package Repository"
         [[ "${i}" == "src" ]] && _TITLE="Sources"
-        echo "Archboot - ${_TITLE} | (c) 2006 - $(date +%Y) Tobias Powalowski | Arch Linux Developer tpowa" > ${k}/HEADER.html
+        echo "Archboot - ${_TITLE} | (c) 2006 - $(date +%Y) Tobias Powalowski | Arch Linux Developer tpowa" > "${k}"/HEADER.html
     done
 done
 # mirrors
 # clean and create directories
 rm ~/release/* 2>/dev/null
 for i in aarch64 riscv64 x86_64; do
-    [[ -d ~/release/${i} ]] || mkdir -p ~/release/${i}
-    rm ~/release/${i}/*
+    [[ -d ~/release/"${i}" ]] || mkdir -p ~/release/"${i}"
+    rm ~/release/"${i}"/*
 done
 # create html on mirrors
 for i in ./{,aarch64,riscv64,x86_64}; do
-    ln -s ~/public_html/release/${i}/HEADER.html \
-          ~/release/${i}/HEADER.html
+    ln -s ~/public_html/release/"${i}"/HEADER.html \
+          ~/release/"${i}"/HEADER.html
 done
 # keep 4 versions on mirrors
 for i in $(seq 0 3); do
     _SYMLINK=$(date -d "$(date +) - ${i} Months" +%Y.%m)
     for k in aarch64 riscv64 x86_64; do
-        if [[ -d ~/public_html/release/${k}/${_SYMLINK} ]]; then
-            ln -s ~/public_html/release/${k}/${_SYMLINK} \
-                  ~/release/${k}/${_SYMLINK}
+        if [[ -d ~/public_html/release/"${k}"/"${_SYMLINK}" ]]; then
+            ln -s ~/public_html/release/"${k}"/"${_SYMLINK}" \
+                  ~/release/"${k}"/"${_SYMLINK}"
         fi
     done
 done
