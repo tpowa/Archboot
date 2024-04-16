36fb2caca3f1d1bb17f150346a634a0e048c6ddb
Tobias Powalowski <tpowa@archlinux.org>
2024-04-16 18:46:05 +0200

    same layout on ssh and ttyd login

--- usr/share/archboot/remote/usr/bin/remote-login.sh
+++ usr/share/archboot/remote/usr/bin/remote-login.sh
@@ -8,8 +8,8 @@ if ! screen -ls &>/dev/null; then
     echo -e "\e[1mLogin on ttyd | $(uname -m) | $(uname -r) | $(date)\e[m"
     echo ""
     cat /etc/motd
-    echo -e "Hit \e[1m\e[92mENTER\e[m for \e[1mshell\e[m login."
     echo ""
+    echo -e "Hit \e[1m\e[92mENTER\e[m for \e[1mshell\e[m login."
     read -r
 fi
 # define /bin/bash, else /bin/sh is the screen default
