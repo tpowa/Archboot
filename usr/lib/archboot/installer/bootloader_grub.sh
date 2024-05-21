0161f612de9c337c457c7ddf7d5849d8633f4d1c
Tobias Powalowski <tpowa@archlinux.org>
2024-05-21 13:39:21 +0200

    fix bios_grub gpt check

--- usr/lib/archboot/installer/bootloader_grub.sh
+++ usr/lib/archboot/installer/bootloader_grub.sh
@@ -303,7 +303,7 @@ _grub_bios() {
     #shellcheck disable=SC2086
     _dialog --title " Grub Boot Device " --no-cancel --menu "" 14 55 7 ${_DEVS} 2>"${_ANSWER}" || return 1
     _BOOTDEV=$(cat "${_ANSWER}")
-    if [[ "$(${_LSBLK} PTTYPE "${_BOOTDEV}")" == "gpt" ]]; then
+    if [[ "$(${_LSBLK} PTTYPE -d "${_BOOTDEV}")" == "gpt" ]]; then
         _CHECK_BIOS_BOOT_GRUB=1
         _RUN_CFDISK=""
         _DISK="${_BOOTDEV}"
