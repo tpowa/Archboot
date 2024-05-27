53fb16e53272a4e90ae58df0be710f3a224d61ab
Tobias Powalowski <tpowa@archlinux.org>
2024-05-27 20:42:58 +0200

    bump logo

--- usr/share/archboot/uki/create-uki-bmp.sh
+++ usr/share/archboot/uki/create-uki-bmp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0-only
 # Created by Tobias Powalowski <tpowa@archlinux.org>
-convert ../grub/archboot-background.png -monochrome archboot-background-mono.bmp
-convert archboot-background-mono.bmp -fill '#0189FD' -opaque white  -colors 2 archboot-background.bmp
+magick ../grub/archboot-background.png -monochrome archboot-background-mono.bmp
+magick archboot-background-mono.bmp -fill '#0189FD' -opaque white  -colors 2 archboot-background.bmp
 rm archboot-background-mono.bmp
 
