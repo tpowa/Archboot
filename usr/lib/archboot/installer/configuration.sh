b5e9720464883479576d7e7087028d24e4c29718
Tobias Powalowski <tpowa@archlinux.org>
2024-06-02 18:19:04 +0200

    add modify users

--- usr/lib/archboot/installer/configuration.sh
+++ usr/lib/archboot/installer/configuration.sh
@@ -161,11 +161,11 @@ _user_management() {
             # add normal users
             while true; do
                 _USERS="root Superuser $(grep 'x:10[0-9][0-9]' ${_DESTDIR}/etc/passwd | cut -d : -f 1,5 | sed -e 's: :#:g' | sed -e 's#:# #g')"
-                _dialog --no-cancel --menu " User Selection Menu " 15 40 10 ${_USERS} "<" "Return To Previous Menu" 2>"${_ANSWER}" || return 1
+                _dialog --no-cancel --menu " User Selection Menu " 15 40 10 ${_USERS} "< Back" "Return To Previous Menu" 2>"${_ANSWER}" || return 1
                 _USER=$(cat "${_ANSWER}")
                 if [[ "${_USER}" = "root" ]]; then
                     _set_password Root root
-                elif [[ "${_USER}" = "<" ]]; then
+                elif [[ "${_USER}" = "< Back" ]]; then
                     break
                 else
                     _dialog --title " Modify User ${_USER} " --no-cancel --menu "" 10 45 4 \
