94565704232398b3c84c628f7371fe675acde397
Tobias Powalowski <tpowa@archlinux.org>
2024-06-04 07:40:10 +0200

    revert commit

--- usr/lib/archboot/installer/btrfs.sh
+++ usr/lib/archboot/installer/btrfs.sh
@@ -47,9 +47,7 @@ _find_btrfs_subvolume() {
         # existing btrfs subvolumes
         _mount_btrfs
         for i in $(btrfs subvolume list "${_BTRFSMP}" | cut -d ' ' -f 9 | grep -v 'var/lib/machines' | grep -v 'var/lib/portables'); do
-            # add echo without "" to kill hidden escapes from btrfs call
-            #shellcheck disable=SC2116,2086
-            echo ${i}
+            echo "${i}"
             [[ "${1}" ]] && echo "${1}"
         done
         _umount_btrfs
@@ -233,6 +231,7 @@ _choose_btrfs_subvolume () {
     _subvolumes_in_use
     # add echo to kill hidden escapes from btrfs call
     #shellcheck disable=SC2116,2086
+    _SUBVOLUMES="$(echo ${_SUBVOLUMES})"
     for i in ${_SUBVOLUME_IN_USE}; do
         _SUBVOLUMES="${_SUBVOLUMES//${i} _/}"
     done
@@ -243,7 +242,7 @@ _choose_btrfs_subvolume () {
         _btrfs_compress || return 1
     else
         if [[ -n "${_SUBVOLUMES_DETECTED}" ]]; then
-            _dialog --title " ERROR " --no-mouse --infobox "All subvolumes of the device are already in use.\nSwitching to create a new one now." 4 50
+            _dialog --title " ERROR " --no-mouse --infobox "All subvolumes of the device are already in use.\nSwitching to create a new one now." 4 50f
             sleep 5
             _prepare_btrfs_subvolume || return 1
         fi
