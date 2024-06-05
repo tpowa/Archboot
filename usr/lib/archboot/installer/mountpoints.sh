5c92e445b1bdd342636eab75b177b4aa8236bdc2
Tobias Powalowski <tpowa@archlinux.org>
2024-06-05 22:01:45 +0200

    start adding SWAP file support

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -79,7 +79,6 @@ _enter_mountpoint() {
                 _FS_OPTIONS="-U clear --size ${_SWAPFILE_SIZE}M --file"
             done
             if ! [[ "${_FSTYPE}" == "swap" ]]; then
-                _SKIP_FILESYSTEM=1
                 _DOMKFS=1
                 _LABEL_NAME="SWAP"
             fi
@@ -257,7 +256,7 @@ _mountpoints() {
                     _clear_fs_values
                     _check_btrfs_filesystem_creation
                     ! [[ "${_DEV}" == "> NONE" || "${_DEV}" == "> FILE" ]] && _FSTYPE="$(${_LSBLK} FSTYPE "${_DEV}" 2>"${_NO_LOG}")"
-                    if [[ -z "${_SWAP_DONE}" && "${_FSTYPE}" == "swap" ]] || [[ "${_DEV}" == "> NONE" ]]; then
+                    if [[ -z "${_SWAP_DONE}" ]] || [[ "${_DEV}" == "> NONE" ]]; then
                         _SKIP_FILESYSTEM=1
                     fi
                     # _CREATE_MOUNTPOINTS switch for create filesystem and only mounting filesystem
