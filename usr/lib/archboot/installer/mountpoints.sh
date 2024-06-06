68f811db6b3f8efd9f176c06636face5d0bec8f1
Tobias Powalowski <tpowa@archlinux.org>
2024-06-06 09:11:00 +0200

    enable error messages on filesystem mounting

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -416,7 +416,7 @@ _mountpoints() {
     _MAX_COUNT=$(wc -l < /tmp/.parts)
     _PROGRESS_COUNT=$((100/_MAX_COUNT))
     _COUNT=0
-    _run_mkfs | _dialog --title " Mountpoints " --no-mouse --gauge "Mountpoints..." 6 75 0
+    _run_mkfs | _dialog --title " Mountpoints " --no-mouse --gauge "Mountpoints..." 6 75 0 || return 1
     _printk on
     # bcachefs uses : array for raid devices, kill this one
      _ROOTDEV="$(mount | grep "${_DESTDIR} " | cut -d ' ' -f 1 | sed -e 's#:.*##g')"
