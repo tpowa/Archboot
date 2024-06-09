cf6338e7e4400cda4459e69339dc51bf92ceaea7
Tobias Powalowski <tpowa@archlinux.org>
2024-06-09 13:55:05 +0200

    replace sudo usage with su

--- usr/lib/archboot/bootloader.sh
+++ usr/lib/archboot/bootloader.sh
@@ -95,11 +95,11 @@ _upload_efi_files() {
     for i in *.efi; do
         #shellcheck disable=SC2086
         if [[ -f "${i}" ]]; then
-            su -u "{_USER}" -c gpg ${_GPG} "${i}" || exit 1
+            su -u "${_USER}" -c gpg ${_GPG} "${i}" || exit 1
         fi
     done
     #shellcheck disable=SC2086
-    su -u "{_USER}" -c ${_RSYNC} ./* "${_SERVER}:.${_ARCH_SERVERDIR}/" || exit 1
+    su -u "${_USER}" -c ${_RSYNC} ./* "${_SERVER}:.${_ARCH_SERVERDIR}/" || exit 1
     cd ..
 }
 
