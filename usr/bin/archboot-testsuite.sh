bbecab21f29f795d1089e862d29ee1f148cc7f85
Tobias Powalowski <tpowa@archlinux.org>
2024-06-13 19:25:26 +0200

    update message

--- usr/bin/archboot-testsuite.sh
+++ usr/bin/archboot-testsuite.sh
@@ -85,14 +85,16 @@ fi
 _result fw-error.txt
 # uninstall base again!
 pacman --noconfirm -Rdd base gettext &>>"${_LOG}"
-_run_test "none pacman tracked files in /usr/lib"
+echo -e "Starting none tracked files in \e[1m10\e[m seconds... \e[1;92mCTRL-C\e[m to stop now."
+read -t 10
+_run_test "none tracked files in /usr/lib... this takes a while"
 for i in $(find /usr/lib | grep -v -E '/modules|/udev'); do
     pacman -Qo ${i} &>${_NO_LOG} || echo ${i} >> pacman-error.log
 done
 _result pacman-error.log
 echo -e "Starting pacman database check in \e[1m10\e[m seconds... \e[1;92mCTRL-C\e[m to stop now."
 read -t 10
-_run_test "pacman database ... this takes a while"
+_run_test "pacman database... this takes a while"
 archboot-not-installed.sh &>>"${_LOG}"
 _result not-installed.txt
 [[ -n "${_TEST_FAIL}" ]] && exit 1
