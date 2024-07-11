648801730f889bbd0c628150c6000e738210e7cd
Tobias Powalowski <tpowa@archlinux.org>
2024-07-11 20:02:27 +0200

    add Wi-Fi iwctl to testsuite

--- usr/bin/archboot-testsuite.sh
+++ usr/bin/archboot-testsuite.sh
@@ -102,6 +102,14 @@ for i in bcachefs btrfs ext4 swap vfat xfs; do
     fi
 done
 _result filesytems-error.log
+_run_test "Wi-Fi iwctl..."
+archboot-hwsim.sh test
+iwctl station wlan1 scan
+sleep 5
+iwctl station wlan1 get-networks | rg -q test || echo "Wi-Fi iwctl get-networks error" >> iwd-error.log
+iwctl --passphrase=12345678 station wlan1 connect test || echo "Wi-Fi iwctl connect error" >> iwd-error.log
+iwctl station wlan1 disconnect || echo "Wi-Fi iwctl disconnect error" >> iwd-error.log
+_result iwd-error.log
 echo -e "Starting none tracked files in \e[1m10\e[m seconds... \e[1;92mCTRL-C\e[m to stop now."
 sleep 10
 _run_test "none tracked files in /usr/lib... this takes a while"
