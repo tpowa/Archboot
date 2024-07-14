1fd20a44bb8f4df3e3412f6f81db1c00ae55fafb
Tobias Powalowski <tpowa@archlinux.org>
2024-07-14 11:22:37 +0200

    add stats

--- usr/bin/archboot-testsuite.sh
+++ usr/bin/archboot-testsuite.sh
@@ -172,7 +172,7 @@ echo "\e[1mResult:\e[m"
 if [[ -z "${_TEST_FAIL}" ]]; then
     echo "\e[1;94m=> \e[1;92mAll tests finished successfully.\e[m"
 else
-    echo "\e[1;94m=> \e[1;91mAn error was detected. Please check the log files\e[m"
+    echo "\e[1;94m=> \e[1;91mAn error was detected. Please check the corresponding log files.\e[m"
     exit 1
 fi
 # vim: set ft=sh ts=4 sw=4 et:
