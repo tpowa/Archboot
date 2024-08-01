7b9eb9ad8f67151559f621412cfed96b6e3247bb
Tobias Powalowski <tpowa@archlinux.org>
2024-08-01 10:09:36 +0200

    update usage

--- usr/bin/archboot-hwsim.sh
+++ usr/bin/archboot-hwsim.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
-#
-#    archboot-hwsim.sh - setup a test SSID
-#    by Tobias Powalowski <tpowa@archlinux.org>
+# archboot-hwsim.sh - setup a test SSID
+# by Tobias Powalowski <tpowa@archlinux.org>
 # usage(exitvalue)
 # outputs a usage message and exits with value
 . /usr/lib/archboot/common.sh
 _usage()
 {
-    echo -e "\e[1mWelcome to \e[34marchboot's\e[m \e[1mHWSIM:\e[m"
-    echo -e "\e[1m---------------------------------------\e[m"
+    echo -e "\e[1m\e[36mArchboot\e[m \e[1m- HWSIM:\e[m"
+    echo -e "\e[1m-----------------\e[m"
     echo "Create a simulated wireless SSID for testing purposes"
     echo "with mac80211_hwsim module."
 	echo "- wlan0 will be setup as the AP. Don't use for scanning!"
 	echo "- wlan1 will be setup for STATION mode. Use this for scanning for your AP."
 	echo ""
-    echo -e "usage: \e[1m${_BASENAME} <SSID>\e[m"
+    echo -e "Usage: \e[1m${_BASENAME} <SSID>\e[m"
     exit 0
 }
 [[ -z "${1}" ]] && _usage
