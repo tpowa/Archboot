a6b34c1d1038f95dcdff188e8b38369cbff1399a
Tobias Powalowski <tpowa@archlinux.org>
2024-08-01 09:59:56 +0200

    add /var/lib/systemd and /var/run to exclude directories, update usage text

--- usr/bin/archboot-rsync-backup.sh
+++ usr/bin/archboot-rsync-backup.sh
@@ -7,16 +7,16 @@
 . /usr/lib/archboot/common.sh
 _usage()
 {
-    echo -e "\e[1mWelcome to \e[36mARCHBOOT\e[m\e[1m - RSYNC BACKUP:\e[m"
-    echo -e "\e[1m-----------------------------------\e[m"
-    echo -e "- Copy \e[1mbackupdir\e[m to \e[1mbackupdestination\e[m using rsync."
-    echo -e "- For system backup, start with \e[1mfull\e[m mounted system and then invoke this script"
-    echo -e "  with system's root directory as \e[1mbackupdir\e[m."
-    echo -e "- \e[1mexcluded\e[m directories are \e[1m/dev /tmp /proc /sys /run /mnt /media /lost+found\e[m"
-    echo -e "  \e[1mexcluded\e[m \e[1m/sysroot /var/run /var/lib/systemd\e[m"
-    echo -e "- \e[1m--numeric-ids\e[m option is invoked to \e[1mpreserve\e[m users"
+    echo -e "\e[1m\e[36mArchboot\e[m\e[1m - Rsync Backup:\e[m"
+    echo -e "\e[1m------------------------\e[m"
+    echo -e "- Copy \e[1m<backupdir>\e[m to \e[1m<backupdestination>\e[m using rsync."
+    echo -e "- For system backup, start with \e[1mfull\e[m mounted system and then"
+    echo -e "  invoke this script with system's root directory as \e[1mbackupdir\e[m."
+    echo -e "- \e[1mExcluded\e[m directories are: \e[1m/dev /lost+found /mnt /media /proc /run /sys\e[m"
+    echo -e "                            \e[1m/sysroot /tmp /var/lib/systemd /var/run\e[m"
+    echo -e "- \e[1mUsers\e[m are \e[1mpreserved\e[m as numeric-ids"
     echo ""
-    echo -e "usage: \e[1m${_BASENAME} <backupdir> <backupdestination>\e[m"
+    echo -e "Usage: \e[1m${_BASENAME} <backupdir> <backupdestination>\e[m"
     exit 0
 }
 ##################################################
@@ -26,6 +26,6 @@ fi
 _BACKUPDESTINATION="${2}"
 _BACKUPDIR="${1}"
 rsync -aAXv --numeric-ids \
---exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/sysroot/*"} \
+--exclude={"/dev/*","/lost+found","/media/*","/mnt/*","/proc/*","/run/*","/sys/*","/sysroot/*","/tmp/*","/var/lib/systemd/*","/var/run/*"} \
 "${_BACKUPDIR}" "${_BACKUPDESTINATION}"
 
