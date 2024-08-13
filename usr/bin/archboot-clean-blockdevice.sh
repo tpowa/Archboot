2a76bdb828922ba60b82239514b930a7a26397ce
Tobias Powalowski <tpowa@archlinux.org>
2024-08-13 15:20:00 +0200

    allow multiple devices

--- usr/bin/archboot-clean-blockdevice.sh
+++ usr/bin/archboot-clean-blockdevice.sh
@@ -9,9 +9,9 @@ _usage()
     echo -e "\e[1m----------------------------\e[m"
     echo -e "This script removes all filesystem signatures"
     echo -e "and partitiontable from the device."
-    echo -e "\e[1m\e[91mWARNING: ALL DATA WILL BE LOST ON THE DEVICE! \e[m"
+    echo -e "\e[1m\e[91mWARNING: ALL DATA WILL BE LOST ON THE DEVICE(S)! \e[m"
     echo ""
-    echo -e "Usage: \e[1m${_BASENAME} <device>\e[m"
+    echo -e "Usage: \e[1m${_BASENAME} <device(s)>\e[m"
     exit 0
 }
 ##################################################
@@ -21,11 +21,14 @@ if ! [[ ${UID} -eq 0 ]]; then
     echo "ERROR: Please run as root user!"
     exit 1
 fi
-echo -e "\e[1mCleaning blockdevice ${1}...\e[m"
-echo -e "\e[91mWARNING: 10 seconds for hitting CTRL+C to stop the process on ${1} now! \e[m"
-sleep 10
-echo -e "\e[1mSTEP 1/2:\e[m Cleaning filesystem signatures..."
-wipefs -f -a "${1}"
-echo -e "\e[1mSTEP 2/2:\e[m Cleaning partition table..."
-dd if=/dev/zero of="${1}" bs=1M count=10
-echo -e "\e[1mFinished.\e[m"
+#shellcheck disable=SC2068
+for i in $@; do
+    echo -e "\e[1mCleaning blockdevice ${i}...\e[m"
+    echo -e "\e[91mWARNING: 10 seconds for hitting CTRL+C to stop the process on ${i} now! \e[m"
+    sleep 10
+    echo -e "\e[1mSTEP 1/2:\e[m Cleaning filesystem signatures..."
+    wipefs -f -a "${i}"
+    echo -e "\e[1mSTEP 2/2:\e[m Cleaning partition table..."
+    dd if=/dev/zero of="${i}" bs=1M count=10
+    echo -e "\e[1mFinished ${i}.\e[m"
+done
