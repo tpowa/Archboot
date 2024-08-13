8fad4ba2afc72e02eb83e6a17dce7f74a7950b00
Tobias Powalowski <tpowa@archlinux.org>
2024-08-13 22:57:11 +0200

    add restore-usbstick bash completion

---
+++ usr/share/bash-completion/completions/restore-usbstick.sh
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# created by Tobias Powalowski <tpowa@archlinux.org>
+_restore_usbstick()
+{
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    compopt -o bashdefault -o default
+    COMPREPLY=( $(compgen -W "$(lsblk -pnro NAME,TRANS,TYPE | rg '(.*) disk$' -r '$1' | rg (.*) usb$' -r '$1')" -- $cur) )
+    return 0
+}
+complete -F _restore_usbstick restore-usbstick.sh
