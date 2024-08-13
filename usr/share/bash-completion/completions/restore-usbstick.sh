e484509280309f3e0dc02bc1139616dc7a02bb02
Tobias Powalowski <tpowa@archlinux.org>
2024-08-13 23:01:11 +0200

    fixed completion

--- usr/share/bash-completion/completions/restore-usbstick.sh
+++ usr/share/bash-completion/completions/restore-usbstick.sh
@@ -5,7 +5,7 @@ _restore_usbstick()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     compopt -o bashdefault -o default
-    COMPREPLY=( $(compgen -W "$(lsblk -pnro NAME,TRANS,TYPE | rg '(.*) disk$' -r '$1' | rg (.*) usb$' -r '$1')" -- $cur) )
+    COMPREPLY=( $(compgen -W "$(lsblk -pnro NAME,TRAN,TYPE | rg '(.*) disk$' -r '$1' | rg (.*) usb$' -r '$1')" -- $cur) )
     return 0
 }
 complete -F _restore_usbstick restore-usbstick.sh
