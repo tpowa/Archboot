0ef62e115fd50154fbe17c3e54e1cfd8cd6a9849
Tobias Powalowski <tpowa@archlinux.org>
2025-01-02 21:54:52 +0100

    update secureboot scripts to latest values

--- usr/bin/archboot-mkkeys.sh
+++ usr/bin/archboot-mkkeys.sh
@@ -2,32 +2,29 @@
 # Copyright (c) 2015 by Roderick W. Smith
 # Licensed under the terms of the GPL v3
 # replaced GUID with uuidgen Tobias Powalowski - <tpowa@archlinux.org>
+_GUID="$(uuidgen --random)"
 echo -n "Enter a Common Name to embed in the keys: "
-read -r NAME
-openssl req -new -x509 -newkey rsa:2048 -subj "/CN=$NAME PK/" -keyout PK.key \
+read -r _NAME
+echo "Creating keys..."
+openssl req -new -x509 -newkey rsa:4096 -subj "/CN=${_NAME} PK/" -keyout PK.key \
         -out PK.crt -days 3650 -nodes -sha256
-openssl req -new -x509 -newkey rsa:2048 -subj "/CN=$NAME KEK/" -keyout KEK.key \
+openssl req -new -x509 -newkey rsa:4096 -subj "/CN=${_NAME} KEK/" -keyout KEK.key \
         -out KEK.crt -days 3650 -nodes -sha256
-openssl req -new -x509 -newkey rsa:2048 -subj "/CN=$NAME DB/" -keyout DB.key \
+openssl req -new -x509 -newkey rsa:4096 -subj "/CN=${_NAME} DB/" -keyout DB.key \
         -out DB.crt -days 3650 -nodes -sha256
 openssl x509 -in PK.crt -out PK.cer -outform DER
 openssl x509 -in KEK.crt -out KEK.cer -outform DER
 openssl x509 -in DB.crt -out DB.cer -outform DER
-GUID="$(uuidgen)"
-echo "$GUID" > myGUID.txt
-cert-to-efi-sig-list -g "$GUID" PK.crt PK.esl
-cert-to-efi-sig-list -g "$GUID" KEK.crt KEK.esl
-cert-to-efi-sig-list -g "$GUID" DB.crt DB.esl
+echo "${_GUID}" > GUID.txt
+cert-to-efi-sig-list -g "${_GUID}" PK.crt PK.esl
+cert-to-efi-sig-list -g "${_GUID}" KEK.crt KEK.esl
+cert-to-efi-sig-list -g "${_GUID}" DB.crt DB.esl
 rm -f noPK.esl
 : > noPK.esl
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')" \
-                  -k PK.key -c PK.crt PK PK.esl PK.auth
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')" \
-                  -k PK.key -c PK.crt PK noPK.esl noPK.auth
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')" \
-                  -k PK.key -c PK.crt KEK KEK.esl KEK.auth
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')" \
-                  -k KEK.key -c KEK.crt db DB.esl DB.auth
+sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt PK PK.esl PK.auth
+sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt PK noPK.esl noPK.auth
+sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt KEK KEK.esl KEK.auth
+sign-efi-sig-list -g "${_GUID}" -k KEK.key -c KEK.crt db DB.esl DB.auth
 chmod 0600 ./*.key
 echo ""
 echo ""
--- usr/bin/archboot-secureboot-keys.sh
+++ usr/bin/archboot-secureboot-keys.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # created by Tobias Powalowski <tpowa@archlinux.org>
 . /usr/lib/archboot/common.sh
+_MS_GUID="77fa9abd-0359-4d32-bd60-28f4e78f784b"
+_MS_PATH="https://www.microsoft.com/pkiops/certs"
+_MS_CERTS="MicWinProPCA2011_2011-10-19 MicCorUEFCA2011_2011-06-27 'windows uefi ca 2023' 'microsoft uefi ca 2023'"
 _usage () {
     echo -e "\e[1m\e[36mArchboot\e[m\e[1m - Generate Secure Boot Keys, MOK Files\e[m"
     echo -e "\e[1m-----------------------------------------------\e[m"
@@ -16,12 +19,12 @@ _usage () {
 _DIR="${2}"
 while [ $# -gt 0 ]; do
     case ${1} in
-        -name=*|--name=*) NAME="$(echo "${1}" | rg -o '=(.*)' -r '$1')" ;;
+        -name=*|--name=*) _NAME="$(echo "${1}" | rg -o '=(.*)' -r '$1')" ;;
         -h|--h|-help|--help|?) _usage ;;
         esac
     shift
 done
-if [[ -z "${NAME}" ]]; then
+if [[ -z "${_NAME}" ]]; then
     echo "ERROR: no name specified"
     _usage
     #shellcheck disable=2317
@@ -29,9 +32,9 @@ if [[ -z "${NAME}" ]]; then
 fi
 _root_check
 # archboot
-[[ -e /usr/bin/mkkeys.sh ]] && MKKEYS="mkkeys.sh"
+[[ -e /usr/bin/mkkeys.sh ]] && _MKKEYS="mkkeys.sh"
 # normal system
-[[ -e /usr/bin/archboot-mkkeys.sh ]] && MKKEYS="archboot-mkkeys.sh"
+[[ -e /usr/bin/archboot-mkkeys.sh ]] && _MKKEYS="archboot-mkkeys.sh"
 if [[ -n "${_DIR}" ]]; then
     [[ ! -d "${_DIR}" ]] && mkdir -p "${_DIR}"
     cd "${_DIR}" || exit 1
@@ -42,19 +45,33 @@ if [[ -n "${_DIR}" ]]; then
     efi-readvar -v db -o BACKUP/old_db.esl
     efi-readvar -v dbx -o BACKUP/old_dbx.esl
     cd BACKUP || exit 1; mokutil --export; cd .. || exit 1
-    echo "Generating Keys in $_DIR"
+    echo "Generating Keys in ${_DIR}"
     # add mkkeys.sh
-    ${MKKEYS} <<EOF
-${NAME} 
+    ${_MKKEYS} <<EOF
+${_NAME}
 EOF
     # download MS Certificates, else EFI might get broken!
-    ${_DLPROG} -O https://www.microsoft.com/pkiops/certs/MicWinProPCA2011_2011-10-19.crt
-    ${_DLPROG} -O https://www.microsoft.com/pkiops/certs/MicCorUEFCA2011_2011-06-27.crt
-    sbsiglist --owner 77fa9abd-0359-4d32-bd60-28f4e78f784b --type x509 --output MS_Win_db.esl MicWinProPCA2011_2011-10-19.crt
-    sbsiglist --owner 77fa9abd-0359-4d32-bd60-28f4e78f784b --type x509 --output MS_UEFI_db.esl MicCorUEFCA2011_2011-06-27.crt
-    cat MS_Win_db.esl MS_UEFI_db.esl > MS_db.esl
-    sign-efi-sig-list -a -g 77fa9abd-0359-4d32-bd60-28f4e78f784b -k KEK.key -c KEK.crt db MS_db.esl add_MS_db.auth
-    openssl req -new -x509 -newkey rsa:2048 -keyout MOK.key -out MOK.crt -nodes -days 3650 -subj "/CN=${NAME}/"
+    echo "Downloading Microsoft Certificates..."
+    for i in ${_MS_CERTS}; do
+        ${_DLPROG} -O "${_MS_PATH}/${i}.crt" || exit 1
+    done
+    echo "Creating EFI Signature Lists from Microsoft's DER format db certificates..."
+    sbsiglist --owner ${_MS_GUID} --type x509 --output MS_Win_db.esl MicWinProPCA2011_2011-10-19.crt
+    sbsiglist --owner ${_MS_GUID} --type x509 --output MS_UEFI_db.esl MicCorUEFCA2011_2011-06-27.crt
+    sbsiglist --owner ${_MS_GUID} --type x509 --output MS_Win_db_2023.esl 'windows uefi ca 2023.crt'
+    sbsiglist --owner ${_MS_GUID} --type x509 --output MS_UEFI_db_2023.esl 'microsoft uefi ca 2023.crt'
+    cat MS_Win_db_2011.esl MS_Win_db_2023.esl MS_UEFI_db_2011.esl MS_UEFI_db_2023.esl > MS_db.esl
+    echo "Creating an EFI Signature List from Microsoft's DER format KEK certificates..."
+    sbsiglist --owner ${_MS_GUID} --type x509 --output MS_Win_KEK_2011.esl MicCorKEKCA2011_2011-06-24.crt
+    sbsiglist --owner ${_MS_GUID} --type x509 --output MS_Win_KEK_2023.esl 'microsoft corporation kek 2k ca 2023.crt'
+    cat MS_Win_KEK_2011.esl MS_Win_KEK_2023.esl > MS_Win_KEK.esl
+    echo "Signing a db variable update with your KEK..."
+    sign-efi-sig-list -a -g ${_MS_GUID} -k KEK.key -c KEK.crt db MS_db.esl add_MS_db.auth
+    echo "Signing a KEK variable update with your PK..."
+    sign-efi-sig-list -a -g ${_MS_GUID} -k PK.key -c PK.crt KEK MS_Win_KEK.esl add_MS_Win_KEK.auth
+    # generate new machine owner key
+    echo "Generating Machine Owner Key (MOK) ..."
+    openssl req -new -x509 -newkey rsa:2048 -keyout MOK.key -out MOK.crt -nodes -days 3650 -subj "/CN=${_NAME}/"
     openssl x509 -in MOK.crt -out MOK.cer -outform DER
     DIRS="DB KEK MOK PK noPK"
     for i in $DIRS; do
@@ -64,7 +81,7 @@ EOF
     mv DB db
     [[ ! -d "GUID" ]] && mkdir GUID
     [[ ! -d "MS" ]] && mkdir MS
-    mv myGUID.txt GUID
+    mv GUID.txt GUID
     mv ./*.crt ./*.auth ./*.esl MS
     cd ..
     chmod 700 "${_DIR}"
