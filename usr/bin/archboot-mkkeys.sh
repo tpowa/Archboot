cae9c8985d3fda7b6924089ccfc3a34cdc996584
Tobias Powalowski <tpowa@archlinux.org>
2025-01-06 08:50:28 +0100

    disalbe console output

--- usr/bin/archboot-mkkeys.sh
+++ usr/bin/archboot-mkkeys.sh
@@ -7,26 +7,25 @@ echo -n "Enter a Common Name to embed in the keys: "
 read -r _NAME
 echo "Creating keys..."
 openssl req -new -x509 -newkey rsa:4096 -subj "/CN=${_NAME} PK/" -keyout PK.key \
-        -out PK.crt -days 3650 -nodes -sha256
+        -out PK.crt -days 3650 -nodes -sha256 &>"${_NO_LOG}"
 openssl req -new -x509 -newkey rsa:4096 -subj "/CN=${_NAME} KEK/" -keyout KEK.key \
-        -out KEK.crt -days 3650 -nodes -sha256
+        -out KEK.crt -days 3650 -nodes -sha256 &>"${_NO_LOG}"
 openssl req -new -x509 -newkey rsa:4096 -subj "/CN=${_NAME} DB/" -keyout DB.key \
-        -out DB.crt -days 3650 -nodes -sha256
-openssl x509 -in PK.crt -out PK.cer -outform DER
-openssl x509 -in KEK.crt -out KEK.cer -outform DER
-openssl x509 -in DB.crt -out DB.cer -outform DER
+        -out DB.crt -days 3650 -nodes -sha256 &>"${_NO_LOG}"
+openssl x509 -in PK.crt -out PK.cer -outform DER &>"${_NO_LOG}"
+openssl x509 -in KEK.crt -out KEK.cer -outform DER &>"${_NO_LOG}"
+openssl x509 -in DB.crt -out DB.cer -outform DER &>"${_NO_LOG}"
 echo "${_GUID}" > GUID.txt
 cert-to-efi-sig-list -g "${_GUID}" PK.crt PK.esl
 cert-to-efi-sig-list -g "${_GUID}" KEK.crt KEK.esl
 cert-to-efi-sig-list -g "${_GUID}" DB.crt DB.esl
 rm -f noPK.esl
 : > noPK.esl
-sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt PK PK.esl PK.auth
-sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt PK noPK.esl noPK.auth
-sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt KEK KEK.esl KEK.auth
-sign-efi-sig-list -g "${_GUID}" -k KEK.key -c KEK.crt db DB.esl DB.auth
+sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt PK PK.esl PK.auth &>"${_NO_LOG}"
+sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt PK noPK.esl noPK.auth &>"${_NO_LOG}"
+sign-efi-sig-list -g "${_GUID}" -k PK.key -c PK.crt KEK KEK.esl KEK.auth &>"${_NO_LOG}"
+sign-efi-sig-list -g "${_GUID}" -k KEK.key -c KEK.crt db DB.esl DB.auth &>"${_NO_LOG}"
 chmod 0600 ./*.key
-echo ""
 echo ""
 echo "For use with KeyTool, copy the *.auth and *.esl files to a FAT USB"
 echo "flash drive or to your EFI System Partition (ESP)."
