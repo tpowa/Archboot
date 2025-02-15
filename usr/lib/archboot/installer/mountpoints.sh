4b256b02667f81dcc93f25debc205995ae27b649
Tobias Powalowski <tpowa@archlinux.org>
2025-02-15 13:29:38 +0100

    fix mountpoints

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -246,7 +246,7 @@ _mountpoints() {
         while [[ "${_DEV}" != "> DONE" ]]; do
             _MP_DONE=""
             while [[ -z "${_MP_DONE}" ]]; do
-                if ! echo "${_DEVS}" | rg -q /dev || ! echo "${_DEVS}" | rg -q '> DONE'; then
+                if ! echo "${_DEVS}" | rg -q /dev && ! echo "${_DEVS}" | rg -q '> DONE'; then
                      _dialog --title " ERROR " --no-mouse --infobox "All devices already in use, please start again." 3 70
                      sleep 5
                      return 1
