0d9832e53fb26c0950b38c19dc8836464e56446a
Tobias Powalowski <tpowa@archlinux.org>
2025-02-17 17:58:47 +0100

    bump to latest version

--- usr/lib/archboot/release.sh
+++ usr/lib/archboot/release.sh
@@ -27,7 +27,7 @@ _create_initrd_dir() {
 _create_fw_cpio() {
     [[ -d "${_W_DIR}/firmware" ]] || mkdir -p "${_W_DIR}/firmware"
     for i in "${_W_DIR}"/tmp/archboot-firmware/*; do
-        _create_cpio "${i}" "../../firmware/$(basename ${i}).img"
+        _create_cpio "${i}" "../../../firmware/$(basename ${i}).img"
     done
 }
 
