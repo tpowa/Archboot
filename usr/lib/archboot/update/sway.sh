23f8765960777cf85cb492d62c753284a01e3507
Tobias Powalowski <tpowa@archlinux.org>
2025-02-26 21:27:33 +0100

    fixed waybar

--- usr/lib/archboot/update/sway.sh
+++ usr/lib/archboot/update/sway.sh
@@ -91,8 +91,8 @@ EOF
         sd 'position top' 'a mode invisible' /etc/sway/config
         # diable not usable plugins
         echo "exec waybar" >> /etc/sway/config
-        for i in custom/media mpd idle_inhibitor pulseaudio; do
-            sd '"${i}",' '' /etc/xdg/waybar/config.jsonc
+        for i in custom/{media,power} mpd idle_inhibitor pulseaudio; do
+            sd "$i" '' /etc/xdg/waybar/config.jsonc
         done
     fi
     echo "Configuring wayvnc..."
