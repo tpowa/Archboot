011d2f87c727b415e41af7eb12b4907f2ae7ac75
Tobias Powalowski <tpowa@archlinux.org>
2025-03-07 19:13:12 +0100

    write refind to template

--- usr/lib/archboot/installer/bootloader_refind.sh
+++ usr/lib/archboot/installer/bootloader_refind.sh
@@ -14,6 +14,13 @@ _refind_uefi() {
     umount -q "${_DESTDIR}"/{boot,efi}
     chroot "${_DESTDIR}" refind-install &>"${_LOG}"
     _chroot_umount
+    # write to template
+    { echo "echo \"Setting up rEFInd now...\""
+    echo "_chroot_mount"
+    echo "umount -q \"${_DESTDIR}\"/{boot,efi}"
+    echo "chroot \"${_DESTDIR}\" refind-install &>\"${_LOG}\""
+    echo "_chroot_umount"
+    } >> "${_TEMPLATE}"
     _REFIND_CONFIG="${_DESTDIR}/${_UEFISYS_MP}/EFI/refind/refind.conf"
     cat << CONFEOF > "${_REFIND_CONFIG}"
 timeout 20
@@ -29,15 +36,26 @@ menuentry "Arch Linux" {
 }
 CONFEOF
     if [[ -e "${_DESTDIR}/${_UEFISYS_MP}/EFI/refind/refind_${_SPEC_UEFI_ARCH}.efi" ]]; then
-        [[ -d "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT" ]] || mkdir -p "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT"
+        if ! [[ -d "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT" ]]; then
+            mkdir -p "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT"
+            # write to template
+            echo "mkdir -p \"${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT\"" >> "${_TEMPLATE}"
+        fi
         rm -f "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/BOOT${_UEFI_ARCH}.EFI"
         rm -f "${_DESTDIR}"/boot/refind_linux.conf
         cp -f "${_DESTDIR}/${_UEFISYS_MP}/EFI/refind/refind_${_SPEC_UEFI_ARCH}.efi" "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/BOOT${_UEFI_ARCH}.EFI"
+        # write to template
+        { echo "rm -f \"${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/BOOT${_UEFI_ARCH}.EFI\""
+        echo "rm -f \"${_DESTDIR}\"/boot/refind_linux.conf"
+        echo "cp -f \"${_DESTDIR}/${_UEFISYS_MP}/EFI/refind/refind_${_SPEC_UEFI_ARCH}.efi\" \"${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/BOOT${_UEFI_ARCH}.EFI\""
+        } >> "${_TEMPLATE}"
         sleep 2
         _dialog --msgbox "You will now be put into the editor to edit:\nrefind.conf\n\nAfter you save your changes, exit the editor." 8 50
         _geteditor || return 1
         _editor "${_REFIND_CONFIG}"
         cp -f "${_REFIND_CONFIG}" "${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/"
+        # write to template
+        echo "cp -f \"${_REFIND_CONFIG}\" \"${_DESTDIR}/${_UEFISYS_MP}/EFI/BOOT/\"" >> "${_TEMPLATE}"
         _pacman_hook_refind
         _dialog --title " Success " --no-mouse --infobox "rEFInd has been setup successfully." 3 50
         sleep 3
