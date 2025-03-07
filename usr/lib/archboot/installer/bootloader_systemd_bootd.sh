6c6c38654195405a297e713d5ba15297a36e32e7
Tobias Powalowski <tpowa@archlinux.org>
2025-03-07 16:11:12 +0100

    write systemd_boot to template

--- usr/lib/archboot/installer/bootloader_systemd_bootd.sh
+++ usr/lib/archboot/installer/bootloader_systemd_bootd.sh
@@ -3,9 +3,19 @@
 # created by Tobias Powalowski <tpowa@archlinux.org>
 _systemd_boot_uefi() {
     _dialog --no-mouse --infobox "Setting up SYSTEMD-BOOT now..." 3 40
+    # write to template
+    echo "echo \"Setting up SYSTEMD-BOOT now...\"" >> "${_TEMPLATE}"
     # create directory structure, if it doesn't exist
-    [[ -d "${_DESTDIR}/boot/loader/entries" ]] || mkdir -p "${_DESTDIR}/boot/loader/entries"
-    [[ -d "${_DESTDIR}/${_UEFISYS_MP}/loader" ]] || mkdir -p "${_DESTDIR}/${_UEFISYS_MP}/loader"
+    if [[ ! -d "${_DESTDIR}/boot/loader/entries" ]]; then
+        mkdir -p "${_DESTDIR}/boot/loader/entries"
+        # write to template
+        echo "mkdir -p \"${_DESTDIR}/boot/loader/entries\"" >> "${_TEMPLATE}"
+    fi
+    if [[ ! -d "${_DESTDIR}/${_UEFISYS_MP}/loader" ]]; then
+        mkdir -p "${_DESTDIR}/${_UEFISYS_MP}/loader"
+        # write to template
+        echo "mkdir -p \"${_DESTDIR}/${_UEFISYS_MP}/loader\"" >> "${_TEMPLATE}"
+    fi
     _MAIN_CFG="boot/loader/entries/archlinux-core-main.conf"
     _LOADER_CFG="/${_UEFISYS_MP}/loader/loader.conf"
     cat << BOOTDEOF > "${_DESTDIR}/${_MAIN_CFG}"
@@ -23,6 +33,11 @@ BOOTDEOF
     # /boot XBOOTLDR in vfat format can be booted by systemd-boot
     chroot "${_DESTDIR}" bootctl install &>"${_LOG}"
     _chroot_umount
+    # write to template
+    { echo "_chroot_mount"
+    echo "chroot \"${_DESTDIR}\" bootctl install &>\"${_LOG}\""
+    echo "_chroot_umount"
+    } >> "${_TEMPLATE}"
     if [[ -e "${_DESTDIR}/${_UEFISYS_MP}/EFI/systemd/systemd-boot${_SPEC_UEFI_ARCH}.efi" ]]; then
         _dialog --msgbox "You will now be put into the editor to edit:\nloader.conf and menu entry files\n\nAfter you save your changes, exit the editor." 8 50
         _geteditor || return 1
