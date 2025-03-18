7e907f7955363741bd15687bd3ddf53e5fe5fb58
Tobias Powalowski <tpowa@archlinux.org>
2025-03-18 14:38:15 +0100

    shellcheck fixes

--- usr/share/archboot/server/create-ipxe-symlinks.sh
+++ usr/share/archboot/server/create-ipxe-symlinks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in $(find ../release/${1}/latest/ipxe/ ! -name '*.sig' ! -name '*.html' ! -name '*.txt' ! -name 'init-*' ! -name 'ipxe' ! -name 'Image'); do
-	ln -s ../${i} ${1}/$(basename ${i})
-	ln -s ../${i}.sig ${1}/$(basename ${i}).sig
+#shellcheck disable=SC2044
+for i in $(find ../release/"${1}"/latest/ipxe/ ! -name '*.sig' ! -name '*.html' ! -name '*.txt' ! -name 'init-*' ! -name 'ipxe' ! -name 'Image'); do
+	ln -s ../"${i}" "${1}"/"$(basename "${i}")"
+	ln -s ../"${i}".sig "${1}"/"$(basename "${i}")".sig
 done
