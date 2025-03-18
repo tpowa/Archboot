f84ed1d0a246170a8b1ba9716a20f9a2814d947c
Tobias Powalowski <tpowa@archlinux.org>
2025-03-18 13:00:12 +0100

    update homepage and add symlink script

---
+++ usr/share/archboot/server/create-ipxe-symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in $(find ../release/${1}/latest/ipxe/ ! -name '*.sig' ! -name '*.html' ! -name '*.txt' ! -name 'init-*' ! -name 'ipxe' ! -name 'Image'); do
+	ln -s ../${i} ${1}/$(basename ${i})
+	ln -s ../${i}.sig ${1}/$(basename ${i}).sig
+done
