85cb87964daa9c842f179378000b87ee09ba052c
Tobias Powalowski <tpowa@archlinux.org>
2025-03-20 11:08:32 +0100

    shellcheck fixes

--- usr/lib/archboot/server.sh
+++ usr/lib/archboot/server.sh
@@ -113,8 +113,8 @@ _server_release() {
     if [[ -d "${_CERT_DIR}" ]]; then
         mkdir ipxe
         for i in $(fd -t f -E '*.sig' -E '*.txt' -E 'archboot*' -E 'init-*'); do
-            ln -s "../${i}" "ipxe/$(basename ${i})"
-            archboot-ipxe-sign.sh ipxe/"$(basename ${i})"
+            ln -s "../${i}" ipxe/"$(basename "${i}")"
+            archboot-ipxe-sign.sh ipxe/"$(basename "${i}")"
         done
         chown -R "${_USER}:${_GROUP}" ipxe/
     fi
