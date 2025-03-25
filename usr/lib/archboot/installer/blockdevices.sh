7aa27462073aea60eed7aa13fcec493253ba31b0
Tobias Powalowski <tpowa@archlinux.org>
2025-03-25 12:45:06 +0100

    code cleanup

--- usr/lib/archboot/installer/blockdevices.sh
+++ usr/lib/archboot/installer/blockdevices.sh
@@ -262,15 +262,14 @@ _activate_special_devices()
 
 _set_device_name_scheme() {
     _NAME_SCHEME_PARAMETER=""
-    _NAME_SCHEME_LEVELS=""
+    _NAME_SCHEME_LEVELS=()
     ## util-linux root=PARTUUID=/root=PARTLABEL= support - https://git.kernel.org/?p=utils/util-linux/util-linux.git;a=commitdiff;h=fc387ee14c6b8672761ae5e67ff639b5cae8f27c;hp=21d1fa53f16560dacba33fffb14ffc05d275c926
     ## mkinitcpio's init root=PARTUUID= support - https://projects.archlinux.org/mkinitcpio.git/tree/init_functions#n185
     if [[ -n "${_UEFI_BOOT}" || -n "${_GUIDPARAMETER}" ]]; then
-        _NAME_SCHEME_LEVELS="${_NAME_SCHEME_LEVELS} PARTUUID PARTUUID=<partuuid> PARTLABEL PARTLABEL=<partlabel> SD_GPT_AUTO_GENERATOR none"
+        _NAME_SCHEME_LEVELS+=(PARTUUID PARTUUID\=\<partuuid\> PARTLABEL PARTLABEL\=\<partlabel\> SD_GPT_AUTO_GENERATOR none)
     fi
-    _NAME_SCHEME_LEVELS="${_NAME_SCHEME_LEVELS} FSUUID UUID=<uuid> FSLABEL LABEL=<label> KERNEL /dev/<kernelname>"
-    #shellcheck disable=SC2086
-    _dialog --no-cancel --title " Device Name Scheme " --menu "Use PARTUUID on GPT disks. Use FSUUID on MBR/MSDOS disks." 13 65 7 ${_NAME_SCHEME_LEVELS} 2>"${_ANSWER}" || return 1
+    _NAME_SCHEME_LEVELS+=(FSUUID UUID\=\<uuid\> FSLABEL LABEL\=\<label\> KERNEL /dev/\<kernelname\>)
+    _dialog --no-cancel --title " Device Name Scheme " --menu "Use PARTUUID on GPT disks. Use FSUUID on MBR/MSDOS disks." 13 65 7 "${_NAME_SCHEME_LEVELS[@]}" 2>"${_ANSWER}" || return 1
     _NAME_SCHEME_PARAMETER=$(cat "${_ANSWER}")
     _NAME_SCHEME_PARAMETER_RUN=1
 }
@@ -458,15 +457,17 @@ _createmd()
         # Remove all raid devices with children
         _dialog --no-mouse --infobox "Scanning blockdevices... This may need some time." 3 60
         _RAID_BLACKLIST="$(_raid_devices;_partitionable_raid_devices_partitions)"
-        #shellcheck disable=SC2119
-        _DEVS="$(_finddevices)"
+        _DEVS=()
+        for i in $(_finddevices); do
+            _DEVS+=("${i}")
+        done
         if [[ -n "${_RAID_BLACKLIST}" ]]; then
             for dev in ${_RAID_BLACKLIST}; do
-                _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${dev}" 2>"${_NO_LOG}")/}"
+                _DEVS=("${_DEVS[@]//$(${_LSBLK} NAME,SIZE -d "${dev}" 2>"${_NO_LOG}")/}")
             done
         fi
         # break if all devices are in use
-        if [[ -z "${_DEVS}" ]]; then
+        if [[ -z "${_DEVS[*]}" ]]; then
             _dialog --msgbox "All devices in use. No more devices left for new creation." 0 0
             return 1
         fi
@@ -480,41 +481,36 @@ _createmd()
                 _RAIDDEV=""
             fi
         done
-        _RAIDLEVELS="linear - raid0 - raid1 - raid4 - raid5 - raid6 - raid10 -"
-        #shellcheck disable=SC2086
-        _dialog --no-cancel --menu "Select the raid level you want to use:" 14 50 7 ${_RAIDLEVELS} 2>"${_ANSWER}" || return 1
+        _RAIDLEVELS=(linear - raid0 - raid1 - raid4 - raid5 - raid6 - raid10 -)
+        _dialog --no-cancel --menu "Select the raid level you want to use:" 14 50 7 "${_RAIDLEVELS[@]}" 2>"${_ANSWER}" || return 1
         _LEVEL=$(cat "${_ANSWER}")
         # raid5 and raid10 support parity parameter
         _PARITY=""
         if [[ "${_LEVEL}" == "raid5" || "${_LEVEL}" == "raid6" || "${_LEVEL}" == "raid10" ]]; then
-            _PARITYLEVELS="left-asymmetric - left-symmetric - right-asymmetric - right-symmetric -"
-            #shellcheck disable=SC2086
-            _dialog --no-cancel --menu "Select the parity layout you want to use (default is left-symmetric):" 21 50 13 ${_PARITYLEVELS} 2>"${_ANSWER}" || return 1
+            _PARITYLEVELS=(left-asymmetric - left-symmetric - right-asymmetric - right-symmetric -)
+            _dialog --no-cancel --menu "Select the parity layout you want to use (default is left-symmetric):" 21 50 13 "${_PARITYLEVELS[@]}" 2>"${_ANSWER}" || return 1
             _PARITY=$(cat "${_ANSWER}")
         fi
         # select the first device to use, no missing option available!
         _RAIDNUMBER=1
         _DEGRADED=""
-        #shellcheck disable=SC2086
-        _dialog --no-cancel --menu "Select device ${_RAIDNUMBER}:" 21 50 13 ${_DEVS} 2>"${_ANSWER}" || return 1
+        _dialog --no-cancel --menu "Select device ${_RAIDNUMBER}:" 21 50 13 "${_DEVS[@]}" 2>"${_ANSWER}" || return 1
         _DEV=$(cat "${_ANSWER}")
         echo "${_DEV}" >>/tmp/.raid
         while true; do
             _RAIDNUMBER=$((_RAIDNUMBER + 1))
             if [[ -n "${_DEV}" ]]; then
                 # clean loop from used partition and options
-                _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}"
+                _DEVS=("${_DEVS[@]//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}")
             fi
             # add more devices
             # raid0 doesn't support missing devices
             if [[ "${_LEVEL}" == "raid0" || "${_LEVEL}" == "linear" || -n "${_DEGRADED}" ]]; then
-                #shellcheck disable=SC2086
                 _dialog --no-cancel --menu "Select additional device ${_RAIDNUMBER}:" \
-                21 50 13 ${_DEVS} "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
+                21 50 13 "${_DEVS[@]}" "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
             else
-                #shellcheck disable=SC2086
                 _dialog --no-cancel --menu "Select additional device ${_RAIDNUMBER}:" \
-                21 50 13 ${_DEVS} "> MISSING" "Degraded Raid Device" "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
+                21 50 13 "${_DEVS[@]}" "> MISSING" "Degraded Raid Device" "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
             fi
             _DEV=$(cat "${_ANSWER}")
             [[ "${_DEV}" == "> DONE" ]] && break
@@ -533,28 +529,26 @@ _createmd()
             fi
         done
         # final step ask if everything is ok?
-        # shellcheck disable=SC2028
         _dialog --yesno "Would you like to create ${_RAIDDEV} like this?\n\nLEVEL:\n${_LEVEL}\n\nDEVICES:\n$(while read -r dev;do echo "${dev}\n"; done < /tmp/.raid)\nSPARES:\n$(while read -r dev;do echo "${dev}\n"; done < tmp/.raid-spare)" 0 0 && break
     done
     _umountall
-    _DEVS="$(echo -n "$(cat /tmp/.raid)")"
-    _SPARES="$(echo -n "$(cat /tmp/.raid-spare)")"
-    # combine both if spares are available, spares at the end!
-    [[ -n ${_SPARES} ]] && _DEVS="${_DEVS} ${_SPARES}"
+    mapfile -t _DEVS < <(cat /tmp/.raid)
+    mapfile -t _SPARES < <(cat /tmp/.raid-spare)
     # get number of devices
-    _RAID_DEVS="$(wc -l < /tmp/.raid)"
-    _SPARE_DEVS="$(wc -l < /tmp/.raid-spare)"
+    _RAID_DEVS="${#_DEVS[@]}"
+    _SPARE_DEVS="${#_SPARES[@]}"
+    # combine both if spares are available, spares at the end!
+    [[ -n ${_SPARES[*]} ]] && _DEVS=("${_DEVS[@]}" "${_SPARES[@]}")
     # generate options for mdadm
-    _RAIDOPTIONS="--force --run --level=${_LEVEL}"
-    ! [[ "${_RAID_DEVS}" == 0 ]] && _RAIDOPTIONS="${_RAIDOPTIONS} --raid-devices=${_RAID_DEVS}"
-    ! [[ "${_SPARE_DEVS}" == 0 ]] && _RAIDOPTIONS="${_RAIDOPTIONS} --spare-devices=${_SPARE_DEVS}"
-    [[ -n "${_PARITY}" ]] && _RAIDOPTIONS="${_RAIDOPTIONS} --layout=${_PARITY}"
-    #shellcheck disable=SC2086
-    if mdadm --create ${_RAIDDEV} ${_RAIDOPTIONS} ${_DEVS} &>"${_LOG}"; then
+    _RAIDOPTIONS=(--force --run --level="${_LEVEL}")
+    ! [[ "${_RAID_DEVS}" == 0 ]] && _RAIDOPTIONS+=(--raid-devices="${_RAID_DEVS}")
+    ! [[ "${_SPARE_DEVS}" == 0 ]] && _RAIDOPTIONS+=(--spare-devices="${_SPARE_DEVS}")
+    [[ -n "${_PARITY}" ]] && _RAIDOPTIONS+=(--layout="${_PARITY}")
+    if mdadm --create "${_RAIDDEV}" "${_RAIDOPTIONS[@]}" "${_DEVS[@]}" &>"${_LOG}"; then
         _dialog --no-mouse --infobox "${_RAIDDEV} created successfully." 3 50
         sleep 3
         { echo "### mdadm device"
-        echo "mdadm --create ${_RAIDDEV} ${_RAIDOPTIONS} ${_DEVS} &>\"\${_LOG}\""
+        echo "mdadm --create ${_RAIDDEV} ${_RAIDOPTIONS[*]} ${_DEVS[*]} &>\"\${_LOG}\""
         echo ""
         } >> "${_TEMPLATE}"
     else
@@ -592,32 +586,32 @@ _createpv()
         _LVM_BLACKLIST="$(for dev in $(${_LSBLK} NAME,TYPE | rg '(.*) lvm$' -r '$1' | sort -u); do
                     echo "${dev}"
                     done)"
-        #shellcheck disable=SC2119
-        _DEVS="$(_finddevices)"
+        _DEVS=()
+        for i in $(_finddevices); do
+            _DEVS+=("${i}")
+        done
         if [[ -n "${_LVM_BLACKLIST}" ]]; then
             for dev in ${_LVM_BLACKLIST}; do
-                _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${dev}" 2>"${_NO_LOG}")/}"
+                _DEVS=("${_DEVS[@]//$(${_LSBLK} NAME,SIZE -d "${dev}" 2>"${_NO_LOG}")/}")
             done
         fi
         # break if all devices are in use
-        if [[ -z "${_DEVS}" ]]; then
+        if [[ -z "${_DEVS[*]}" ]]; then
             _dialog --msgbox "No devices left for physical volume creation." 0 0
             return 1
         fi
         # select the first device to use
         _DEVNUMBER=1
-        #shellcheck disable=SC2086
-        _dialog --menu "Select device number ${_DEVNUMBER} for physical volume:" 15 50 12 ${_DEVS} 2>"${_ANSWER}" || return 1
+        _dialog --menu "Select device number ${_DEVNUMBER} for physical volume:" 15 50 12 "${_DEVS[@]}" 2>"${_ANSWER}" || return 1
         _DEV=$(cat "${_ANSWER}")
         echo "${_DEV}" >>/tmp/.pvs-create
         while [[ "${_DEV}" != "> DONE" ]]; do
             _DEVNUMBER="$((_DEVNUMBER + 1))"
             # clean loop from used partition and options
-            _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}"
+            _DEVS=("${_DEVS[@]//$(${_LSBLK} NAME,SIZE -d "${_DEV}" 2>"${_NO_LOG}")/}")
             # add more devices
-            #shellcheck disable=SC2086
             _dialog --no-cancel --menu "Select additional device number ${_DEVNUMBER} for physical volume:" 15 60 12 \
-                ${_DEVS} "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
+                "${_DEVS[@]}" "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
             _DEV=$(cat "${_ANSWER}")
             [[ "${_DEV}" == "> DONE" ]] && break
             echo "${_DEV}" >>/tmp/.pvs-create
@@ -626,10 +620,8 @@ _createpv()
         _dialog --yesno "Would you like to create physical volume on devices below?\n$(rg '(.*$)' -r '$1\n' /tmp/.pvs-create)" 0 0 && break
     done
     _DEV="$(echo -n "$(cat /tmp/.pvs-create)")"
-    #shellcheck disable=SC2028,SC2086
     _umountall
-    #shellcheck disable=SC2086
-    if pvcreate -y ${_DEV} &>"${_LOG}"; then
+    if pvcreate -y "${_DEV}" &>"${_LOG}"; then
         _dialog --no-mouse --infobox "Creating physical volume on ${_DEV} was successful." 3 75
         # write to template
         { echo "### pv device"
@@ -670,7 +662,6 @@ _findvg()
 {
     for dev in $(vgs -o vg_name --noheading); do
         if ! vgs -o vg_free --noheading --units m "${dev}" | rg -q ' 0m$'; then
-            #shellcheck disable=SC2028
             echo "${dev} $(vgs -o vg_free --noheading --units m "${dev}")"
         fi
     done
@@ -680,9 +671,12 @@ _createvg()
 {
     while true; do
         : >/tmp/.pvs
-        _PVS=$(_findpv)
+        _PVS=()
+        for i in $(_findpv); do
+            _PVS+=("${i}")
+        done
         # break if all devices are in use
-        if [[ -z "${_PVS}" ]]; then
+        if [[ -z "${_PVS[*]}" ]]; then
             _dialog --msgbox "No devices left for Volume Group creation." 0 0
             return 1
         fi
@@ -699,18 +693,16 @@ _createvg()
         # show all devices with sizes, which are not in use
         # select the first device to use, no missing option available!
         _PVNUMBER=1
-        #shellcheck disable=SC2086
-        _dialog --no-cancel --menu "Select Physical Volume ${_PVNUMBER} for ${_VGDEV}:" 13 50 10 ${_PVS} 2>"${_ANSWER}" || return 1
+        _dialog --no-cancel --menu "Select Physical Volume ${_PVNUMBER} for ${_VGDEV}:" 13 50 10 "${_PVS[@]}" 2>"${_ANSWER}" || return 1
         _PV=$(cat "${_ANSWER}")
         echo "${_PV}" >>/tmp/.pvs
-        while [[ "${_PVS}" != "> DONE" ]]; do
+        while [[ "${_PV}" != "> DONE" ]]; do
             _PVNUMBER=$((_PVNUMBER + 1))
             # clean loop from used partition and options
-            _PVS="${_PVS//$(${_LSBLK} NAME,SIZE -d "${_PV}" 2>"${_NO_LOG}")/}"
+            _PVS=("${_PVS[@]//$(${_LSBLK} NAME,SIZE -d "${_PV}" 2>"${_NO_LOG}")/}")
             # add more devices
-            #shellcheck disable=SC2086
             _dialog --no-cancel --menu "Select additional Physical Volume ${_PVNUMBER} for ${_VGDEV}:" 13 50 10 \
-                ${_PVS} "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
+                "${_PVS[@]}" "> DONE" "Proceed To Summary" 2>"${_ANSWER}" || return 1
             _PV=$(cat "${_ANSWER}")
             [[ "${_PV}" == "> DONE" ]] && break
             echo "${_PV}" >>/tmp/.pvs
@@ -718,14 +710,13 @@ _createvg()
         # final step ask if everything is ok?
         _dialog --yesno "Would you like to create Volume Group like this?\n\n${_VGDEV}\n\nPhysical Volumes:\n$(rg '(.*$)' -r '$1\n' /tmp/.pvs)" 0 0 && break
     done
-    _PV="$(echo -n "$(cat /tmp/.pvs)")"
+    mapfile -t _PV < <(cat /tmp/.pvs)
     _umountall
-    #shellcheck disable=SC2086
-    if vgcreate ${_VGDEV} ${_PV} &>"${_LOG}"; then
+    if vgcreate "${_VGDEV}" "${_PV[@]}" &>"${_LOG}"; then
         _dialog --no-mouse --infobox "Creating Volume Group ${_VGDEV} was successful." 3 60
         # write to template
         { echo "### vg device"
-        echo "vgcreate ${_VGDEV} ${_PV} &>\"\${_LOG}\""
+        echo "vgcreate ${_VGDEV} ${_PV[*]} &>\"\${_LOG}\""
         echo ""
         } >> "${_TEMPLATE}"
         sleep 3
@@ -738,15 +729,17 @@ _createvg()
 _createlv()
 {
     while true; do
-        _LVS=$(_findvg)
+        _LVS=()
+        for i in $(_findvg); do
+            _LVS+=("${i}")
+        done
         # break if all devices are in use
-        if [[ -z "${_LVS}" ]]; then
+        if [[ -z "${_LVS[*]}" ]]; then
             _dialog --msgbox "No Volume Groups with free space available for Logical Volume creation." 0 0
             return 1
         fi
         # show all devices with sizes, which are not 100% in use!
-        #shellcheck disable=SC2086
-        _dialog --menu "Select Volume Group:" 11 50 5 ${_LVS} 2>"${_ANSWER}" || return 1
+        _dialog --menu "Select Volume Group:" 11 50 5 "${_LVS[@]}" 2>"${_ANSWER}" || return 1
         _LV=$(cat "${_ANSWER}")
         # enter logical volume name
         _LVDEV=""
@@ -782,10 +775,10 @@ _createlv()
         fi
         if [[ -n "${_LV_CONTIGUOUS}" ]]; then
             _CONTIGUOUS=yes
-            _LV_EXTRA="-W y -C y -y"
+            _LV_EXTRA=(-W y -C y -y)
         else
             _CONTIGUOUS=no
-            _LV_EXTRA="-W y -y"
+            _LV_EXTRA=(-W y -y)
         fi
         [[ -z "${_LV_SIZE}" ]] && _LV_SIZE="All free space left"
         # final step ask if everything is ok?
@@ -793,12 +786,11 @@ _createlv()
     done
     _umountall
     if [[ -n "${_LV_ALL}" ]]; then
-        #shellcheck disable=SC2086
-        if lvcreate ${_LV_EXTRA} -l +100%FREE ${_LV} -n ${_LVDEV} &>"${_LOG}"; then
+        if lvcreate "${_LV_EXTRA[@]}" -l +100%FREE "${_LV}" -n "${_LVDEV}" &>"${_LOG}"; then
             _dialog --no-mouse --infobox "Creating Logical Volume ${_LVDEV} was successful." 3 60
             # write to template
             { echo "### lv device"
-            echo "lvcreate ${_LV_EXTRA} -l +100%FREE ${_LV} -n ${_LVDEV} &>\"\${_LOG}\""
+            echo "lvcreate ${_LV_EXTRA[*]} -l +100%FREE ${_LV} -n ${_LVDEV} &>\"\${_LOG}\""
             echo ""
             } >> "${_TEMPLATE}"
             sleep 3
@@ -807,12 +799,11 @@ _createlv()
             return 1
         fi
     else
-        #shellcheck disable=SC2086
-        if lvcreate ${_LV_EXTRA} -L ${_LV_SIZE} ${_LV} -n ${_LVDEV} &>"${_LOG}"; then
+        if lvcreate "${_LV_EXTRA[@]}" -L "${_LV_SIZE}" "${_LV}" -n "${_LVDEV}" &>"${_LOG}"; then
             _dialog --no-mouse --infobox "Creating Logical Volume ${_LVDEV} was successful." 3 60
             # write to template
             { echo "### lv device"
-            echo "lvcreate ${_LV_EXTRA} -L ${_LV_SIZE} ${_LV} -n ${_LVDEV} &>\"\${_LOG}\""
+            echo "lvcreate ${_LV_EXTRA[*]} -L ${_LV_SIZE} ${_LV} -n ${_LVDEV} &>\"\${_LOG}\""
             echo ""
             } >> "${_TEMPLATE}"
             sleep 3
@@ -900,21 +891,21 @@ _createluks()
         _LUKS_BLACKLIST="$(for dev in $(${_LSBLK} NAME,TYPE | rg '(.*) crypt$' -r '$1' | sort -u); do
                     echo "${dev}"
                     done)"
-        #shellcheck disable=SC2119
-         _DEVS="$(_finddevices)"
+        for i in $(_finddevices); do
+            _DEVS+=("${i}")
+        done
         if [[ -n "${_LUKS_BLACKLIST}" ]]; then
             for dev in ${_LUKS_BLACKLIST}; do
-                _DEVS="${_DEVS//$(${_LSBLK} NAME,SIZE -d "${dev}" 2>"${_NO_LOG}")/}"
+                _DEVS=("${_DEVS[@]//$(${_LSBLK} NAME,SIZE -d "${dev}" 2>"${_NO_LOG}")/}")
             done
         fi
         # break if all devices are in use
-        if [[ -z "${_DEVS}" ]]; then
+        if [[ -z "${_DEVS[*]}" ]]; then
             _dialog --msgbox "No devices left for luks encryption." 0 0
             return 1
         fi
         # show all devices with sizes
-        #shellcheck disable=SC2086
-        _dialog --menu "Select device for luks encryption:" 15 50 12 ${_DEVS} 2>"${_ANSWER}" || return 1
+        _dialog --menu "Select device for luks encryption:" 15 50 12 "${_DEVS[@]}" 2>"${_ANSWER}" || return 1
         _DEV=$(cat "${_ANSWER}")
         # enter luks name
         _enter_luks_name || return 1
