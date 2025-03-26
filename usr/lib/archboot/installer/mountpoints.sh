37bf61da52c3f14241ed7afbe3cbf89fbd9f5b56
Tobias Powalowski <tpowa@archlinux.org>
2025-03-26 10:44:45 +0100

    code cleanup

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -443,7 +443,7 @@ _mountpoints() {
         rg '/dev' /tmp/.parts >/tmp/.parts.tmp
         rg -v '/dev' /tmp/.parts >>/tmp/.parts.tmp
         mv /tmp/.parts.tmp /tmp/.parts
-        mapfile -t _MOUNTPOINTS < <(cat /tmp/.parts)
+        mapfile -t _MOUNTPOINTS < <(cat /tmp/.parts | sd '$' '\n')
         _dialog --title " Summary " --defaultno --yesno "Syntax\n------\nDEVICE|FSTYPE|MOUNTPOINT|FORMAT|LABEL|FSOPTIONS|FS_DETAILS\n\n${_MOUNTPOINTS[*]}" 0 0 && _DEVFINISH="DONE"
     done
     # disable swap and all mounted devices
