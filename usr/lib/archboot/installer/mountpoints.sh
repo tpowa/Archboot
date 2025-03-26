f36e956972abda243d5ba3f93ca63b180e237fe0
Tobias Powalowski <tpowa@archlinux.org>
2025-03-26 09:34:00 +0100

    add root device sorted

--- usr/lib/archboot/installer/mountpoints.sh
+++ usr/lib/archboot/installer/mountpoints.sh
@@ -426,7 +426,7 @@ _mountpoints() {
                          IFS=" " read -r -a _DEVS <<< "$(echo "${_DEVS[@]}" | sd "$(${_LSBLK} NAME,SIZE -d "${_DEV}")" "")"
                         if [[ -n "${_ESP_DONE}" && -z "${_XBOOTLDR}" && -n ${_ROOT_BTRFS} ]]; then
                             _DEVS=("${_ROOT_BTRFS}" "${_DEVS[@]}")
-                            mapfile -t _DEVS < <(printf '%s\n' "${_DEVS[@]}" | sort)
+                            mapfile -t _DEVS < <(${_LSBLK} NAME,SIZE "$(printf '%s\n' "${_DEVS[@]}" | rg '/dev' | sort)")
                             _ROOT_BTRFS=""
                         fi
                     else
