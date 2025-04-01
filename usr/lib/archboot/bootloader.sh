84f3cd2e4c402365a9a9a8569c07ee2d3523e9ee
Tobias Powalowski <tpowa@archlinux.org>
2025-04-01 21:53:24 +0200

    fixed wrong substitution

--- usr/lib/archboot/bootloader.sh
+++ usr/lib/archboot/bootloader.sh
@@ -22,7 +22,12 @@ _usage() {
 }
 
 _grub_mkstandalone() {
-    "${1:}" "${2:}" grub-mkstandalone -d "/usr/lib/grub/${_GRUB_ARCH}" -O "${_GRUB_ARCH}" \
+    if [[ -z ${1} ]]; then
+        _mkstandalone="grub-mkstandalone"
+    else
+        _mkstandalone="${1} ${2} grub-mkstandalone"
+    fi
+    ${_mkstandalone} -d "/usr/lib/grub/${_GRUB_ARCH}" -O "${_GRUB_ARCH}" \
     --sbat=/usr/share/grub/sbat.csv --fonts=ter-u16n --locales="" --themes="" \
     -o "grub-efi/${_GRUB_EFI}" "boot/grub/grub.cfg=${_GRUB_ISO}"
 }
