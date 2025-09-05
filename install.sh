006296558cee281acea63244e7f596a51f034c22
Tobias Powalowski <tpowa@archlinux.org>
2025-09-05 14:41:19 +0200

    fix symlinks

--- install.sh
+++ install.sh
@@ -13,15 +13,15 @@ fi
 echo "Installing files and symlinks to ${1}"
 cp -r etc usr "${1}"/
 for i in aarch64 riscv64 x86_64; do
-    symlink -s "${1}"/usr/bin/archboot-${i}-create-container.sh ../lib/archboot/run/container.sh
-    symlink -s "${1}"/usr/bin/archboot-${i}-create-repository.sh ../lib/archboot/run/repository.sh
-    symlink -s "${1}"/usr/bin/archboot-${i}-iso.sh ../lib/archboot/run/iso.sh
-    symlink -s "${1}"/usr/bin/archboot-${i}-release.sh ../lib/archboot/run/release.sh
-    symlink -s "${1}"/usr/bin/archboot-${i}-server-release.sh ../lib/archboot/run/server-release.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-create-container.sh ../lib/archboot/run/container.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-create-repository.sh ../lib/archboot/run/repository.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-iso.sh ../lib/archboot/run/iso.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-release.sh ../lib/archboot/run/release.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-server-release.sh ../lib/archboot/run/server-release.sh
 done
 for i in aarch64 riscv64; do
-    symlink -s "${1}"/usr/bin/archboot-${i}-pacman-container-tarball.sh ../lib/archboot/run/container-tarball.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-pacman-container-tarball.sh ../lib/archboot/run/container-tarball.sh
 done
 for i in aarch64 x86_64; do
-    symlink -s "${1}"/usr/bin/archboot-${i}-uki.sh ../lib/archboot/run/uki.sh
+    ln -s "${1}"/usr/bin/archboot-${i}-uki.sh ../lib/archboot/run/uki.sh
 done
