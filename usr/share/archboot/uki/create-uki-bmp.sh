9cb43d32d7a6800696a8ced2619c3d4fb449cbf9
Tobias Powalowski <tpowa@archlinux.org>
2025-10-02 22:47:23 +0200

    fix uki logo

--- usr/share/archboot/uki/create-uki-bmp.sh
+++ usr/share/archboot/uki/create-uki-bmp.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0-only
 # Created by Tobias Powalowski <tpowa@archlinux.org>
-magick ../grub/archboot-background.png -monochrome archboot-background-mono.bmp
-magick archboot-background-mono.bmp -fill '#0189FD' -opaque white  -colors 2 archboot-background.bmp
-rm archboot-background-mono.bmp
+magick ../grub/archboot-background.png -white-threshold 50% -monochrome archboot-background-mono.bmp
+magick archboot-background-mono.bmp -fill '#0189FD' -opaque black -colors 2 archboot-background-blue.bmp
+magick archboot-background-blue.bmp -fill black -opaque white -colors 2 archboot-background.bmp
+rm archboot-background-{mono,blue}.bmp
 
