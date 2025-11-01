dfc17fe28bdfad404b12135fdf747f1864909a1e
Tobias Powalowski <tpowa@archlinux.org>
2025-11-01 09:43:37 +0100

    update logo update routine

--- usr/share/archboot/uki/create-uki-bmp.sh
+++ usr/share/archboot/uki/create-uki-bmp.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# SPDX-License-Identifier: GPL-2.0-only
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Created by Tobias Powalowski <tpowa@archlinux.org>
 magick ../grub/archboot-background.png -white-threshold 50% -monochrome archboot-background-mono.bmp
-magick archboot-background-mono.bmp -fill '#0189FD' -opaque black -colors 2 archboot-background-blue.bmp
+magick archboot-background-mono.bmp -fill '#0189FD' -opaque white -colors 2 archboot-background-blue.bmp
 magick archboot-background-blue.bmp -fill black -opaque white -colors 2 archboot-background.bmp
 magick archboot-background.bmp -depth 16 -treedepth 16 ../grub/archboot-background.png
 rm archboot-background-{mono,blue}.bmp
