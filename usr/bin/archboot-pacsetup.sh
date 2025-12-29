a243d8b18a641d5917b70796ca3a638fc39a2ac0
Tobias Powalowski <tpowa@archlinux.org>
2025-12-29 21:15:25 +0100

    ensure mirrors get downloaded

--- usr/bin/archboot-pacsetup.sh
+++ usr/bin/archboot-pacsetup.sh
@@ -4,7 +4,9 @@
 . /usr/lib/archboot/common.sh
 _TITLE="archboot.com | ${_RUNNING_ARCH} | ${_RUNNING_KERNEL} | Basic Setup | Pacman Configuration"
 _task_download_mirror() {
-    ${_DLPROG} -o /tmp/pacman_mirrorlist.txt "https://archlinux.org/mirrorlist/?country=${_COUNTRY}&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on"
+    while true; do
+        ${_DLPROG} -o /tmp/pacman_mirrorlist.txt "https://archlinux.org/mirrorlist/?country=${_COUNTRY}&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" && break
+    done
     rm /.archboot
 }
 _download_mirror() {
