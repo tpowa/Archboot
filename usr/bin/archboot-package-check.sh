7713c7f04018330da8affc8d1492d593b5bf869f
Tobias Powalowski <tpowa@archlinux.org>
2026-01-23 20:05:11 +0100

    fix package check

--- usr/bin/archboot-package-check.sh
+++ usr/bin/archboot-package-check.sh
@@ -4,7 +4,7 @@
 . /usr/lib/archboot/common.sh
 _usage () {
     echo -e "\e[1m\e[36mArchboot\e[m\e[1m - Check On Not Installed Packages\e[m"
-    echo "------------------------------------"
+    echo "-------------------------------------------"
     echo "This will check on packages, which don't have any files in the environment"
     echo ""
     echo -e "Usage: \e[1m${_BASENAME} run\e[m"
@@ -23,5 +23,5 @@ sort -u /pkg-found.txt > /pkg-uniq.txt
 rm /pkg-found.txt
 pacman -Q >/pkg-install.txt
 sd ' ' '-' /pkg-install.txt
-diff -u /pkg-uniq.txt /pkg-install.txt | rg '\+' > /pkg-not-installed.txt
+diff -u /pkg-uniq.txt /pkg-install.txt | rg '\\+' > /pkg-not-installed.txt
 rm /pkg-uniq.txt /pkg-install.txt
